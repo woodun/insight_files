a360118caf3854022ad1f85ba127d218990f673d
woodun <hwang07@email.wm.edu>
2016-05-03 17:12:52 -0400

    debug

--- insight_template/setup_all_configs.sh
+++ insight_template/setup_all_configs.sh
@@ -8,12 +8,12 @@ for stor1_config in swl1 swl2 swl4 swl8 swl16
 do
 cd $configs_stor1
 cd $stor1_config
-sh setup_All.sh $stor1_config 1
+sh setup_ALL.sh $stor1_config 1
 done
 
 for stor2_config in lrr gto twolevel301 twolevel601 twolevel1201
 do
 cd $configs_stor2
 cd $stor2_config
-sh setup_All.sh $stor2_config 2
+sh setup_ALL.sh $stor2_config 2
 done

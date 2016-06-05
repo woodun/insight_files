cf51a201558f3cf074a2ca71e58bc41b4d7d1b84
woodun <hwang07@email.wm.edu>
2016-06-05 18:50:40 -0400

    debug

--- load_balance_scripts/launch_rodinia.sh
+++ load_balance_scripts/launch_rodinia.sh
@@ -2,8 +2,9 @@
 
 #13apps
 #not working:pf_naive
+#runnable but no reads: leukocyte
 
-for benchmark in backprop bfs hotspot heartwall cfd streamcluster nw pathfinder lud leukocyte srad_v1 srad_v2 pf_float
+for benchmark in backprop bfs hotspot heartwall cfd streamcluster nw pathfinder lud srad_v1 srad_v2 pf_float
 do
 sh launch_all_configs_per_app.sh $benchmark rodinia
 done

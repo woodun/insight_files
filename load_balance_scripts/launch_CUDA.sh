168a5d345bcd455f1dafce2568303561dd736c7a
woodun <hwang07@email.wm.edu>
2016-07-28 05:22:29 -0400

    debug

--- load_balance_scripts/launch_CUDA.sh
+++ load_balance_scripts/launch_CUDA.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#21apps, not ready:WP
+#21apps, not ready:WP, no global read misses:LIB, NQU
 
-for benchmark in CP LIB LPS NQU RAY STO SCP SLA CONS FWT TRA lulesh AES BFS MUM NN kmeans JPEG BFS2 KMN BlackScholes
+for benchmark in CP LPS RAY STO SCP SLA CONS FWT TRA lulesh AES BFS MUM NN kmeans JPEG BFS2 KMN BlackScholes
 do
 sh launch_all_configs_per_app.sh $benchmark CUDA
 done
--- load_balance_scripts/launch_CUDA_in02.sh
+++ load_balance_scripts/launch_CUDA_in02.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#21apps, not ready:WP
+#21apps, not ready:WP, no global read misses:LIB, NQU
 
-for benchmark in BlackScholes MUM FWT lulesh CP LIB LPS STO SCP SLA CONS TRA AES BFS NN kmeans BFS2 KMN
+for benchmark in BlackScholes MUM RAY JPEG FWT lulesh CP LPS STO SCP SLA CONS TRA AES BFS NN kmeans BFS2 KMN
 do
 sh launch_all_configs_per_app_in02.sh $benchmark CUDA
 done

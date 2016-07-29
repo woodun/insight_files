c37b383ddf770df17e33728801c359cca997802e
woodun <hwang07@email.wm.edu>
2016-07-29 18:49:23 -0400

    debug

--- load_balance_scripts/launch_CUDA.sh
+++ load_balance_scripts/launch_CUDA.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-#21apps, not ready:WP, no global read misses:LIB, NQU
+#21apps total, not ready:WP, no global read misses:LIB, NQU, too less misses: AES, CP, STO, bias always high: KMN, FWT, SCP.
+#bias doesn't change:CONS, LPS, SLA. bias high but changes:lulesh. less Misses: JPEG, RAY.
+#Good: TRA, BFS, MUM, NN, kmeans, BFS2, BlackScholes, lulesh?.
+#current:14
 
-for benchmark in CP LPS RAY STO SCP SLA CONS FWT TRA lulesh AES BFS MUM NN kmeans JPEG BFS2 KMN BlackScholes
+for benchmark in LPS RAY SLA CONS TRA BFS MUM NN kmeans JPEG BFS2 KMN BlackScholes lulesh
 do
 sh launch_all_configs_per_app.sh $benchmark CUDA
 done
--- load_balance_scripts/launch_CUDA_in02.sh
+++ load_balance_scripts/launch_CUDA_in02.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+#21apps total, not ready:WP, no global read misses:LIB, NQU, too less misses: AES, CP, STO, bias always high: KMN, FWT, SCP.
+#bias doesn't change:CONS, LPS, SLA. bias high but changes:lulesh. less Misses: JPEG, RAY.
+#Good: TRA, BFS, MUM, NN, kmeans, BFS2, BlackScholes, lulesh?.
+#current:14
 
-#21apps, not ready:WP, no global read misses:LIB, NQU
-
-for benchmark in BlackScholes MUM RAY JPEG FWT lulesh CP LPS STO SCP SLA CONS TRA AES BFS NN kmeans BFS2 KMN
+for benchmark in LPS RAY SLA CONS TRA BFS MUM NN kmeans JPEG BFS2 KMN BlackScholes lulesh
 do
 sh launch_all_configs_per_app_in02.sh $benchmark CUDA
 done

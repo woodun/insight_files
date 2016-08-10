b8dbb1360ea1ac2b8e25c203da7629e5e3f6f0ef
woodun <hwang07@email.wm.edu>
2016-08-10 18:57:46 -0400

    debug

--- load_balance_scripts/launch_CUDA.sh
+++ load_balance_scripts/launch_CUDA.sh
@@ -4,9 +4,10 @@
 #bias doesn't change:CONS, LPS, SLA. bias high but changes:lulesh. less Misses: JPEG, RAY.
 #Good: TRA, BFS, MUM, NN, kmeans, BFS2, BlackScholes, lulesh?.
 #bw<15% && ipc>400(not interesting): None.
-#16 for now
+#avg bias not change for schedulers: CONS BlackScholes LPS SLA
+#10 for now
 
-for benchmark in LPS RAY SLA CONS TRA BFS MUM NN kmeans JPEG BFS2 KMN BlackScholes lulesh
+for benchmark in RAY TRA BFS MUM NN kmeans JPEG BFS2 KMN lulesh
 do
 sh launch_all_configs_per_app.sh $benchmark CUDA
 done

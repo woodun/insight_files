50bbf6e39de2d001fcc5ace90372a5343d631bb3
woodun <hwang07@email.wm.edu>
2016-08-13 14:56:45 -0400

    debug

--- load_balance_scripts/configs_results_mkdir_in01.sh
+++ load_balance_scripts/configs_results_mkdir_in01.sh
@@ -4,7 +4,7 @@
 configs_results_stor1=/stor1/hwang07/stor1_results
 
 #modify the configs you want to collect result on machine in01
-for stor1_config in h0_t2_swl1 h0_t2_swl2 h0_t2_swl4 h0_t2_swl8 h0_t2_swl16 h0_t2_lrr h0_t2_gto h0_t2_tl201 h0_t2_tl401 h0_t2_tl801 h0_t3_swl1 h0_t3_swl2 h0_t3_swl4 h0_t3_swl8 h0_t3_swl16 h0_t3_lrr h0_t3_gto h0_t3_tl201 h0_t3_tl401 h0_t3_tl801
+for stor1_config in pc5_w0_t3_lrrswl1 pc5_w0_t3_lrrswl2 pc5_w0_t3_lrrswl4 pc5_w0_t3_lrrswl8 pc5_w0_t3_lrrswl16 pc5_w0_t3_swl1 pc5_w0_t3_swl2 pc5_w0_t3_swl4 pc5_w0_t3_swl8 pc5_w0_t3_swl16 pc5_w0_t3_lrr pc5_w0_t3_gto pc5_w0_t3_tl101 pc5_w0_t3_tl201 pc5_w0_t3_tl401 pc5_w0_t3_tl801 pc5_w0_t3_tl1601
 do
 cd $configs_results_stor1
 mkdir $stor1_config

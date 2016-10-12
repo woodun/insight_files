3e3725f54d561c587638d0f011ec07b89b0b1832
woodun <hwang07@email.wm.edu>
2016-10-12 19:27:38 -0400

    debug

--- load_balance_scripts/setup_in01_profile.sh
+++ load_balance_scripts/setup_in01_profile.sh
@@ -3,11 +3,10 @@
 #specify your config path in stor1
 configs_stor1=/stor1/hwang07/profile/
 
-#modify the configs you want to launch on machine in01
+#modify the configs you want to launch on machine in01, use this sh on machine in01
 for stor1_config in profile_lrrswl4 profile_lrrswl8 profile_lrrswl16 profile_RRswl4 profile_RRswl8 profile_RRswl16 profile_GTOswl4 profile_GTOswl8 profile_GTOswl16
 do
 cd $configs_stor1
 cd $stor1_config
-cd $2/$1/
-qsub pbs_$1.pbs
+sh setup_ALL.sh $stor1_config
 done

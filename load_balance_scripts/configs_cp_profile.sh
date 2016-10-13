015f15959b764bddf141f4cf1f613a3efd5d6299
woodun <hwang07@email.wm.edu>
2016-10-13 18:40:28 -0400

    debug

--- load_balance_scripts/configs_cp_profile.sh
+++ load_balance_scripts/configs_cp_profile.sh
@@ -8,7 +8,7 @@ configs_results_stor2=/stor1/hwang07/profile/
 template_folder=/stor1/hwang07/insight_files/insight_template_30min
 
 #modify the configs you want to launch on machine in01
-for stor1_config in profile_lrrswl4 profile_lrrswl8 profile_lrrswl16 profile_RRswl4 profile_RRswl8 profile_RRswl16 profile_GTOswl4 profile_GTOswl8 profile_GTOswl16
+for stor1_config in profile_lrrswl1 profile_lrrswl2 profile_lrrswl4 profile_lrrswl8 profile_lrrswl16 profile_lrrswl32 profile_RRswl1 profile_RRswl2 profile_RRswl4 profile_RRswl8 profile_RRswl16 profile_RRswl32 profile_GTOswl1 profile_GTOswl2 profile_GTOswl4 profile_GTOswl8 profile_GTOswl16 profile_GTOswl32
 do
 cd $configs_results_stor1
 cp -r $template_folder $stor1_config
--- load_balance_scripts/launch_all_configs_per_app_profile.sh
+++ load_balance_scripts/launch_all_configs_per_app_profile.sh
@@ -4,7 +4,7 @@
 configs_stor1=/stor1/hwang07/profile/
 
 #modify the configs you want to launch on machine in01
-for stor1_config in profile_lrrswl4 profile_lrrswl8 profile_lrrswl16 profile_RRswl4 profile_RRswl8 profile_RRswl16 profile_GTOswl4 profile_GTOswl8 profile_GTOswl16
+for stor1_config in profile_lrrswl1 profile_lrrswl2 profile_lrrswl4 profile_lrrswl8 profile_lrrswl16 profile_lrrswl32 profile_RRswl1 profile_RRswl2 profile_RRswl4 profile_RRswl8 profile_RRswl16 profile_RRswl32 profile_GTOswl1 profile_GTOswl2 profile_GTOswl4 profile_GTOswl8 profile_GTOswl16 profile_GTOswl32
 do
 cd $configs_stor1
 cd $stor1_config

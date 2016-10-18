9292c3d9fc11fed338dcf163ce10e48e73053c7a
woodun <hwang07@email.wm.edu>
2016-10-18 14:36:21 -0400

    debug

--- load_balance_scripts/launch_all_profile_in01.sh
+++ load_balance_scripts/launch_all_profile_in01.sh
@@ -1,31 +1,36 @@
 #too long: lbm, md, page view rank, page view count
 
+for benchmark in NN kmeans JPEG BFS2 KMN BlackScholes lulesh
+do
+sh launch_all_configs_per_app_profile_in01.sh $benchmark CUDA
+done
+###########################################################filtered
 #for benchmark in CP LPS RAY STO SCP SLA CONS FWT TRA AES BFS MUM NN kmeans JPEG BFS2 KMN BlackScholes lulesh
 #do
 #sh launch_all_configs_per_app_profile_in01.sh $benchmark CUDA
 #done
 
-for benchmark in SimilarityScore Kmeans MatrixMul InvertedIndex StringMatch WordCount
-do
-sh launch_all_configs_per_app_profile_in01.sh $benchmark Mars
-done
+#for benchmark in SimilarityScore Kmeans MatrixMul InvertedIndex StringMatch WordCount
+#do
+#sh launch_all_configs_per_app_profile_in01.sh $benchmark Mars
+#done
 
-for benchmark in spmv histo
-do
-sh launch_all_configs_per_app_profile_in01.sh $benchmark parboil
-done
+#for benchmark in spmv
+#sh launch_all_configs_per_app_profile_in01.sh $benchmark parboil
+#done
 
-for benchmark in heartwall cfd streamcluster nw pathfinder lud srad_v1 srad_v2 pf_float
-do
-sh launch_all_configs_per_app_profile_in01.sh $benchmark rodinia
-done
+#for benchmark in heartwall cfd streamcluster nw pathfinder lud srad_v1 srad_v2 pf_float
+#do
+#sh launch_all_configs_per_app_profile_in01.sh $benchmark rodinia
+#done
 
-for benchmark in QTC Reduction Scan Spmv Stencil2D Triad BFS
-do
-sh launch_all_configs_per_app_profile_in01.sh $benchmark shoc
-done
+#for benchmark in QTC Reduction Scan Spmv Stencil2D Triad BFS
+#do
+#sh launch_all_configs_per_app_profile_in01.sh $benchmark shoc
+#done
 
+########################lonestar############
 #for benchmark in bh mst sssp sp bfs
 #do
-#sh launch_all_configs_per_app.sh $benchmark lonestar
+#sh launch_all_configs_per_app_in01.sh $benchmark lonestar
 #done
--- load_balance_scripts/launch_all_profile_in02.sh
+++ load_balance_scripts/launch_all_profile_in02.sh
@@ -1,9 +1,14 @@
 #too long: lbm, md, page view rank, page view count
 
-for benchmark in CP LPS RAY STO SCP SLA CONS FWT TRA AES BFS MUM NN kmeans JPEG BFS2 KMN BlackScholes lulesh
+for benchmark in CP LPS RAY STO SCP SLA CONS FWT TRA AES BFS MUM
 do
 sh launch_all_configs_per_app_profile_in02.sh $benchmark CUDA
 done
+###########################################################filtered
+#for benchmark in CP LPS RAY STO SCP SLA CONS FWT TRA AES BFS MUM NN kmeans JPEG BFS2 KMN BlackScholes lulesh
+#do
+#sh launch_all_configs_per_app_profile_in02.sh $benchmark CUDA
+#done
 
 #for benchmark in SimilarityScore Kmeans MatrixMul InvertedIndex StringMatch WordCount
 #do
@@ -22,4 +27,10 @@ done
 #for benchmark in QTC Reduction Scan Spmv Stencil2D Triad BFS
 #do
 #sh launch_all_configs_per_app_profile_in02.sh $benchmark shoc
+#done
+
+########################lonestar############
+#for benchmark in bh mst sssp sp bfs
+#do
+#sh launch_all_configs_per_app_in02.sh $benchmark lonestar
 #done

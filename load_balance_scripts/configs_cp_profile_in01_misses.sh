c6fdbebf7826453756aeea2ac6fb3b6704d41174
woodun <hwang07@email.wm.edu>
2016-10-20 04:53:44 -0400

    debug

--- load_balance_scripts/configs_cp_profile_in01_misses.sh
+++ load_balance_scripts/configs_cp_profile_in01_misses.sh
@@ -4,7 +4,7 @@
 configs_results_stor1=/stor1/hwang07/profile/
 
 #use this template folder or copy it to your favored location
-template_folder=/stor1/hwang07/insight_files/insight_template
+template_folder=/stor1/hwang07/insight_files/insight_template_4hour
 
 #modify the configs you want to launch on machine in01
 for stor1_config in profile_misses_lrrswl1 profile_misses_lrrswl2 profile_misses_lrrswl4 profile_misses_lrrswl8 profile_misses_lrrswl16 profile_misses_lrrswl32 profile_misses_lrrswl48 profile_misses_RRswl1 profile_misses_RRswl2 profile_misses_RRswl4 profile_misses_RRswl8 profile_misses_RRswl16 profile_misses_RRswl32 profile_misses_RRswl48 profile_misses_GTOswl1 profile_misses_GTOswl2 profile_misses_GTOswl4 profile_misses_GTOswl8 profile_misses_GTOswl16 profile_misses_GTOswl32 profile_misses_GTOswl48

ba17cb8fcd3ddf9adc7036858fa49a14150bfb8d
woodun <hwang07@email.wm.edu>
2016-11-05 17:35:16 -0400

    debug

--- load_balance_scripts/launch_all_approx.sh
+++ load_balance_scripts/launch_all_approx.sh
@@ -2,42 +2,42 @@
 #9
 for benchmark in 2DCONV 3DCONV FDTD-2D GEMM GESUMMV GRAMSCHM MVT 2MM SYRK
 do
-sh launch_all_configs_per_app_profile_in02.sh $benchmark polybench
+sh launch_all_configs_approx.sh $benchmark polybench
 done
 
 ######################waiting
 #17
 #for benchmark in LPS RAY SCP SLA CONS FWT TRA BFS MUM NN kmeans BFS2 KMN BlackScholes CP STO JPEG
 #do
-#sh launch_all_configs_per_app_profile_in01.sh $benchmark CUDA
+#sh launch_all_configs_approx.sh $benchmark CUDA
 #done
 
 #6
 #for benchmark in Kmeans MatrixMul InvertedIndex PageViewCount PageViewRank WordCount
 #do
-#sh launch_all_configs_per_app_profile_in01.sh $benchmark Mars
+#sh launch_all_configs_approx.sh $benchmark Mars
 #done
 
 #2
 #for benchmark in lbm histo
 #do
-#sh launch_all_configs_per_app_profile_in02.sh $benchmark parboil
+#sh launch_all_configs_approx.sh $benchmark parboil
 #done
 
 #2
 #for benchmark in nw lud
 #do
-#sh launch_all_configs_per_app_profile_in02.sh $benchmark rodinia
+#sh launch_all_configs_approx.sh $benchmark rodinia
 #done
 
 #4
 #for benchmark in MD QTC Spmv BFS
 #do
-#sh launch_all_configs_per_app_profile_in02.sh $benchmark shoc
+#sh launch_all_configs_approx.sh $benchmark shoc
 #done
 
 #4
 #for benchmark in 3MM ATAX BICG SYR2K
 #do
-#sh launch_all_configs_per_app_profile_in02.sh $benchmark polybench
+#sh launch_all_configs_approx.sh $benchmark polybench
 #done

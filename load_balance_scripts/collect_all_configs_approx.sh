d19d65dc7d0eb594696ec68e091cd6436291945c
woodun <hwang07@email.wm.edu>
2016-11-06 13:40:00 -0500

    debug

--- load_balance_scripts/collect_all_configs_approx.sh
+++ load_balance_scripts/collect_all_configs_approx.sh
@@ -11,7 +11,7 @@ configs_stor2=/stor2/hwang07/profile/
 output=/stor1/hwang07/profile/collect/$2_$1.txt
 
 printf "%s" "###################################################Accuracy#######################################################################" >> $output
-printf "\n%s\n" "coverage:           0%       12.5%          25%          50%          75%         100%" >> $output
+printf "\n%s\n" "coverage:           0%        12.5%          25%          50%          75%         100%" >> $output
 printf "%s" "gtoswl48: " >> $output
 
 #modify the configs you want to launch on machine in01
@@ -35,7 +35,7 @@ grep ACT_percLossInQoR output_* | sed -e "s/ACT_percLossInQoR //g" | xargs print
 done
 
 printf "\n%s" "###################################################IPC#######################################################################" >> $output
-printf "\n%s\n" "coverage:           0%       12.5%          25%          50%          75%         100%" >> $output
+printf "\n%s\n" "coverage:       0%    12.5%      25%      50%      75%     100%" >> $output
 printf "%s" "gtoswl48: " >> $output
 
 #modify the configs you want to launch on machine in01
@@ -59,7 +59,7 @@ grep "gpu_tot_ipc =" output_* | tail -1 | sed -e "s/gpu_tot_ipc =//g" | xargs pr
 done
 
 printf "\n%s" "###################################################BW_UTIL#######################################################################" >> $output
-printf "\n%s\n" "coverage:           0%       12.5%          25%          50%          75%         100%" >> $output
+printf "\n%s\n" "coverage:       0%    12.5%      25%      50%      75%     100%" >> $output
 printf "%s" "gtoswl48: " >> $output
 
 #modify the configs you want to launch on machine in01
@@ -68,7 +68,7 @@ do
 cd $configs_stor1
 cd $stor1_config
 cd $2/$1/
-grep -w "bw_util=" output_* | tail -1 | sed -e "s/bw_util=//g" | xargs printf "%s " >> $output
+grep -w "bw_util=[^\s]*" output_* | tail -1 | sed -e "s/bw_util=//g" | xargs printf "%s " >> $output
 done
 
 printf "\n%s" "lrrswl48: " >> $output
@@ -79,5 +79,5 @@ do
 cd $configs_stor2
 cd $stor2_config
 cd $2/$1/
-grep -w "bw_util=" output_* | tail -1 | sed -e "s/bw_util=//g" | xargs printf "%s " >> $output
+grep -w "bw_util=[^\s]*" output_* | tail -1 | sed -e "s/bw_util=//g" | xargs printf "%s " >> $output
 done

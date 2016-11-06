de91401416d732bbdb5271ceffe0d39a4529a417
woodun <hwang07@email.wm.edu>
2016-11-06 13:21:01 -0500

    debug

--- load_balance_scripts/collect_all_configs_approx.sh
+++ load_balance_scripts/collect_all_configs_approx.sh
@@ -10,8 +10,8 @@ configs_stor2=/stor2/hwang07/profile/
 #specify your output file
 output=/stor1/hwang07/profile/collect/$2_$1.txt
 
-printf "%s" "coverage:           0%       12.5%          25%          50%          75%         100%\n" >> $output
-printf "%s\n" "accuracy:" >> $output
+printf "%s\n" "###################################################Accuracy#######################################################################" >> $output
+printf "%s\n" "coverage:           0%       12.5%          25%          50%          75%         100%" >> $output
 printf "%s" "gtoswl48: " >> $output
 
 #modify the configs you want to launch on machine in01
@@ -32,4 +32,52 @@ cd $configs_stor2
 cd $stor2_config
 cd $2/$1/
 grep ACT_percLossInQoR output_* | sed -e "s/ACT_percLossInQoR //g" | xargs printf "%s " >> $output
+done
+
+printf "\n%s\n" "###################################################IPC#######################################################################" >> $output
+printf "\n%s\n" "coverage:           0%       12.5%          25%          50%          75%         100%" >> $output
+printf "%s" "gtoswl48: " >> $output
+
+#modify the configs you want to launch on machine in01
+for stor1_config in exp_profile_gtoswl48 approximate_125coverage_gtoswl48 approximate_25coverage_gtoswl48 approximate_50coverage_gtoswl48 approximate_75coverage_gtoswl48 approximate_100coverage_gtoswl48 
+do
+cd $configs_stor1
+cd $stor1_config
+cd $2/$1/
+grep "gpu_tot_ipc =" output_* | sed -e "s/gpu_tot_ipc =//g" | xargs printf "%s " >> $output
+done
+
+printf "\n%s" "lrrswl48: " >> $output
+
+#modify the configs you want to launch on machine in02
+for stor2_config in exp_profile_lrrswl48 approximate_125coverage_lrrswl48 approximate_25coverage_lrrswl48 approximate_50coverage_lrrswl48 approximate_75coverage_lrrswl48 approximate_100coverage_lrrswl48
+do
+cd $configs_stor2
+cd $stor2_config
+cd $2/$1/
+grep "gpu_tot_ipc =" output_* | sed -e "s/gpu_tot_ipc =//g" | xargs printf "%s " >> $output
+done
+
+printf "\n%s\n" "###################################################BW_UTIL#######################################################################" >> $output
+printf "\n%s\n" "coverage:           0%       12.5%          25%          50%          75%         100%" >> $output
+printf "%s" "gtoswl48: " >> $output
+
+#modify the configs you want to launch on machine in01
+for stor1_config in exp_profile_gtoswl48 approximate_125coverage_gtoswl48 approximate_25coverage_gtoswl48 approximate_50coverage_gtoswl48 approximate_75coverage_gtoswl48 approximate_100coverage_gtoswl48 
+do
+cd $configs_stor1
+cd $stor1_config
+cd $2/$1/
+grep "bw_util=" output_* | sed -e "s/bw_util=//g" | xargs printf "%s " >> $output
+done
+
+printf "\n%s" "lrrswl48: " >> $output
+
+#modify the configs you want to launch on machine in02
+for stor2_config in exp_profile_lrrswl48 approximate_125coverage_lrrswl48 approximate_25coverage_lrrswl48 approximate_50coverage_lrrswl48 approximate_75coverage_lrrswl48 approximate_100coverage_lrrswl48
+do
+cd $configs_stor2
+cd $stor2_config
+cd $2/$1/
+grep "bw_util=" output_* | sed -e "s/bw_util=//g" | xargs printf "%s " >> $output
 done

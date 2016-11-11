4fdecd2ea5ce2eae0172809092bc5c12500f1412
woodun <hwang07@email.wm.edu>
2016-11-11 07:09:34 -0500

    debug

--- load_balance_scripts/collect_all_approx.sh
+++ load_balance_scripts/collect_all_approx.sh
@@ -1,24 +1,51 @@
 ######################launched
 #12
-for benchmark in 2DCONV 3DCONV FDTD-2D GEMM GESUMMV GRAMSCHM MVT 2MM SYRK 3MM ATAX BICG
+#for benchmark in 2DCONV 3DCONV FDTD-2D GEMM GESUMMV GRAMSCHM MVT 2MM SYRK 3MM ATAX BICG
+#do
+#sh launch_all_configs_approx.sh $benchmark polybench
+#done
+
+for benchmark in ATAX
 do
-sh collect_all_configs_approx.sh $benchmark polybench
+sh launch_all_configs_approx.sh $benchmark polybench
 done
 
-#2
-#for benchmark in Triad Scan
+#5
+#for benchmark in Triad Scan BFS Spmv
 #do
-#sh collect_all_configs_approx.sh $benchmark shoc
+#sh launch_all_configs_approx.sh $benchmark shoc
 #done
 
-#4
-#for benchmark in SLA TRA SCP BlackScholes
+#for benchmark in MD Stencil2D
 #do
-#sh collect_all_configs_approx.sh $benchmark CUDA
+#sh launch_all_configs_approx.sh $benchmark shoc
+#done
+
+#9
+#for benchmark in SLA TRA SCP BlackScholes LPS CONS FWT KMN JPEG
+#do
+#sh launch_all_configs_approx.sh $benchmark CUDA
+#done
+
+for benchmark in JPEG
+do
+sh launch_all_configs_approx.sh $benchmark CUDA
+done
+
+#1
+#for benchmark in histo
+#do
+#sh launch_all_configs_approx.sh $benchmark parboil
+#done
+
+#1
+#for benchmark in srad_v1 lud
+#do
+#sh launch_all_configs_approx.sh $benchmark rodinia
 #done
 ######################waiting
-#13
-#for benchmark in LPS RAY CONS FWT BFS MUM NN kmeans BFS2 KMN CP STO JPEG
+#8
+#for benchmark in RAY BFS MUM NN kmeans BFS2 CP STO
 #do
 #sh launch_all_configs_approx.sh $benchmark CUDA
 #done
@@ -29,20 +56,20 @@ done
 #sh launch_all_configs_approx.sh $benchmark Mars
 #done
 
-#2
-#for benchmark in lbm histo
+#1
+#for benchmark in lbm
 #do
 #sh launch_all_configs_approx.sh $benchmark parboil
 #done
 
-#2
-#for benchmark in nw lud
+#1
+#for benchmark in nw
 #do
 #sh launch_all_configs_approx.sh $benchmark rodinia
 #done
 
-#4
-#for benchmark in MD QTC Spmv BFS
+#1
+#for benchmark in QTC
 #do
 #sh launch_all_configs_approx.sh $benchmark shoc
 #done

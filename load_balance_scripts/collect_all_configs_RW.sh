7ddeadac12c5d17d5378aca4a63898f850c05ef4
woodun <hwang07@email.wm.edu>
2016-11-24 21:30:27 -0500

    debug

--- load_balance_scripts/collect_all_configs_RW.sh
+++ load_balance_scripts/collect_all_configs_RW.sh
@@ -10,44 +10,54 @@ configs_stor2=/stor2/hwang07/approx/
 #specify your output file
 output=/stor1/hwang07/RW.txt
 
+gpu_tot_ipc =
+gpu_stall_dramfull = 18843363
+gpu_stall_icnt2sh    = 1273357
+gpu_total_sim_rate=54196
+
 ###############################################################################Total_core_cache_stats_breakdown 0 coverage GTO48#########################
-printf "$2_$1:\n" >> $output
-printf "GTO48 No prediction:\n" >> $output
+printf "$2_$1:\r\n" >> $output
+printf "GTO48 No prediction:\r\n" >> $output
 for stor1_config in approx_0coverage_gtoswl48
 do
 cd $configs_stor1
 cd $stor1_config
 cd $2/$1/
-grep "L1D_total_cache_accesses =" output_* | tail -1 | xargs printf "   %s\n" >> $output
-grep "L1D_total_cache_misses =" output_* | tail -1 | xargs printf "   %s\n" >> $output
-grep "L1D_total_cache_miss_rate =" output_* | tail -1 | xargs printf "   %s\n" >> $output
-grep "L1D_total_cache_reservation_fails =" output_* | tail -1 | xargs printf "   %s\n" >> $output
+grep "gpu_tot_ipc" output_* | tail -1 | xargs printf "   %s\r\n" >> $output
+grep "gpu_stall_dramfull" output_* | tail -1 | xargs printf "   %s\r\n" >> $output
+grep "gpu_stall_icnt2sh" output_* | tail -1 | xargs printf "   %s\r\n" >> $output
+grep "gpu_total_sim_rate" output_* | tail -1 | xargs printf "   %s\r\n" >> $output
 
-grep "Total_core_cache_stats_breakdown\[GLOBAL_ACC_R\]\[HIT\] =" output_* | tail -1 | xargs printf "   %s\n" >> $output
-grep "Total_core_cache_stats_breakdown\[GLOBAL_ACC_R\]\[HIT_RESERVED\] =" output_* | tail -1 | xargs printf "   %s\n" >> $output
-grep "Total_core_cache_stats_breakdown\[GLOBAL_ACC_R\]\[MISS\] =" output_* | tail -1 | xargs printf "   %s\n" >> $output
-grep "Total_core_cache_stats_breakdown\[GLOBAL_ACC_R\]\[RESERVATION_FAIL\] =" output_* | tail -1 | xargs printf "   %s\n" >> $output
+grep "L1D_total_cache_accesses =" output_* | tail -1 | xargs printf "   %s\r\n" >> $output
+grep "L1D_total_cache_misses =" output_* | tail -1 | xargs printf "   %s\r\n" >> $output
+grep "L1D_total_cache_miss_rate =" output_* | tail -1 | xargs printf "   %s\r\n" >> $output
+grep "L1D_total_cache_reservation_fails =" output_* | tail -1 | xargs printf "   %s\r\n" >> $output
 
-grep "Total_core_cache_stats_breakdown\[GLOBAL_ACC_W\]\[MISS\] =" output_* | tail -1 | xargs printf "   %s\n" >> $output
-grep "Total_core_cache_stats_breakdown\[GLOBAL_ACC_W\]\[RESERVATION_FAIL\] =" output_* | tail -1 | xargs printf "   %s\n" >> $output
+grep "Total_core_cache_stats_breakdown\[GLOBAL_ACC_R\]\[HIT\] =" output_* | tail -1 | xargs printf "   %s\r\n" >> $output
+grep "Total_core_cache_stats_breakdown\[GLOBAL_ACC_R\]\[HIT_RESERVED\] =" output_* | tail -1 | xargs printf "   %s\r\n" >> $output
+grep "Total_core_cache_stats_breakdown\[GLOBAL_ACC_R\]\[MISS\] =" output_* | tail -1 | xargs printf "   %s\r\n" >> $output
+grep "Total_core_cache_stats_breakdown\[GLOBAL_ACC_R\]\[RESERVATION_FAIL\] =" output_* | tail -1 | xargs printf "   %s\r\n" >> $output
 
-grep "n_cmd=" output_* | tail -1 | xargs printf "   %s\n" >> $output
-grep "n_activity=" output_* | tail -1 | xargs printf "   %s\n" >> $output
+grep "Total_core_cache_stats_breakdown\[GLOBAL_ACC_W\]\[MISS\] =" output_* | tail -1 | xargs printf "   %s\r\n" >> $output
+grep "Total_core_cache_stats_breakdown\[GLOBAL_ACC_W\]\[RESERVATION_FAIL\] =" output_* | tail -1 | xargs printf "   %s\r\n" >> $output
 
-grep "L2_total_cache_accesses =" output_* | tail -1 | xargs printf "   %s\n" >> $output
-grep "L2_total_cache_misses =" output_* | tail -1 | xargs printf "   %s\n" >> $output
-grep "L2_total_cache_miss_rate =" output_* | tail -1 | xargs printf "   %s\n" >> $output
-grep "L2_total_cache_reservation_fails =" output_* | tail -1 | xargs printf "   %s\n" >> $output
+grep "n_cmd=" output_* | tail -1 | xargs printf "   %s\r\n" >> $output
+grep "n_activity=" output_* | tail -1 | xargs printf "   %s\r\n" >> $output
 
-grep "L2_cache_stats_breakdown\[GLOBAL_ACC_R\]\[HIT\] =" output_* | tail -1 | xargs printf "   %s\n" >> $output
-grep "L2_cache_stats_breakdown\[GLOBAL_ACC_R\]\[HIT_RESERVED\] =" output_* | tail -1 | xargs printf "   %s\n" >> $output
-grep "L2_cache_stats_breakdown\[GLOBAL_ACC_R\]\[MISS\] =" output_* | tail -1 | xargs printf "   %s\n" >> $output
-grep "L2_cache_stats_breakdown\[GLOBAL_ACC_R\]\[RESERVATION_FAIL\] =" output_* | tail -1 | xargs printf "   %s\n" >> $output
+grep "L2_total_cache_accesses =" output_* | tail -1 | xargs printf "   %s\r\n" >> $output
+grep "L2_total_cache_misses =" output_* | tail -1 | xargs printf "   %s\r\n" >> $output
+grep "L2_total_cache_miss_rate =" output_* | tail -1 | xargs printf "   %s\r\n" >> $output
+grep "L2_total_cache_reservation_fails =" output_* | tail -1 | xargs printf "   %s\r\n" >> $output
 
-grep "L2_cache_stats_breakdown\[GLOBAL_ACC_W\]\[HIT\] =" output_* | tail -1 | xargs printf "   %s\n" >> $output
-grep "L2_cache_stats_breakdown\[GLOBAL_ACC_W\]\[HIT_RESERVED\] =" output_* | tail -1 | xargs printf "   %s\n" >> $output
-grep "L2_cache_stats_breakdown\[GLOBAL_ACC_W\]\[MISS\] =" output_* | tail -1 | xargs printf "   %s\n" >> $output
-grep "L2_cache_stats_breakdown\[GLOBAL_ACC_W\]\[RESERVATION_FAIL\] =" output_* | tail -1 | xargs printf "   %s\n" >> $output
+grep "L2_cache_stats_breakdown\[GLOBAL_ACC_R\]\[HIT\] =" output_* | tail -1 | xargs printf "   %s\r\n" >> $output
+grep "L2_cache_stats_breakdown\[GLOBAL_ACC_R\]\[HIT_RESERVED\] =" output_* | tail -1 | xargs printf "   %s\r\n" >> $output
+grep "L2_cache_stats_breakdown\[GLOBAL_ACC_R\]\[MISS\] =" output_* | tail -1 | xargs printf "   %s\r\n" >> $output
+grep "L2_cache_stats_breakdown\[GLOBAL_ACC_R\]\[RESERVATION_FAIL\] =" output_* | tail -1 | xargs printf "   %s\r\n" >> $output
+
+grep "L2_cache_stats_breakdown\[GLOBAL_ACC_W\]\[HIT\] =" output_* | tail -1 | xargs printf "   %s\r\n" >> $output
+grep "L2_cache_stats_breakdown\[GLOBAL_ACC_W\]\[HIT_RESERVED\] =" output_* | tail -1 | xargs printf "   %s\r\n" >> $output
+grep "L2_cache_stats_breakdown\[GLOBAL_ACC_W\]\[MISS\] =" output_* | tail -1 | xargs printf "   %s\r\n" >> $output
+grep "L2_cache_stats_breakdown\[GLOBAL_ACC_W\]\[RESERVATION_FAIL\] =" output_* | tail -1 | xargs printf "   %s\r\n" >> $output
 done
 
 ###############################################################################Total_core_cache_stats_breakdown 50 coverage GTO48#########################
@@ -58,34 +68,39 @@ do
 cd $configs_stor2
 cd $stor1_config
 cd $2/$1/
-grep "L1D_total_cache_accesses =" output_* | tail -1 | xargs printf "   %s\n" >> $output
-grep "L1D_total_cache_misses =" output_* | tail -1 | xargs printf "   %s\n" >> $output
-grep "L1D_total_cache_miss_rate =" output_* | tail -1 | xargs printf "   %s\n" >> $output
-grep "L1D_total_cache_reservation_fails =" output_* | tail -1 | xargs printf "   %s\n" >> $output
+grep "gpu_tot_ipc" output_* | tail -1 | xargs printf "   %s\r\n" >> $output
+grep "gpu_stall_dramfull" output_* | tail -1 | xargs printf "   %s\r\n" >> $output
+grep "gpu_stall_icnt2sh" output_* | tail -1 | xargs printf "   %s\r\n" >> $output
+grep "gpu_total_sim_rate" output_* | tail -1 | xargs printf "   %s\r\n" >> $output
 
-grep "Total_core_cache_stats_breakdown\[GLOBAL_ACC_R\]\[HIT\] =" output_* | tail -1 | xargs printf "   %s\n" >> $output
-grep "Total_core_cache_stats_breakdown\[GLOBAL_ACC_R\]\[HIT_RESERVED\] =" output_* | tail -1 | xargs printf "   %s\n" >> $output
-grep "Total_core_cache_stats_breakdown\[GLOBAL_ACC_R\]\[MISS\] =" output_* | tail -1 | xargs printf "   %s\n" >> $output
-grep "Total_core_cache_stats_breakdown\[GLOBAL_ACC_R\]\[RESERVATION_FAIL\] =" output_* | tail -1 | xargs printf "   %s\n" >> $output
+grep "L1D_total_cache_accesses =" output_* | tail -1 | xargs printf "   %s\r\n" >> $output
+grep "L1D_total_cache_misses =" output_* | tail -1 | xargs printf "   %s\r\n" >> $output
+grep "L1D_total_cache_miss_rate =" output_* | tail -1 | xargs printf "   %s\r\n" >> $output
+grep "L1D_total_cache_reservation_fails =" output_* | tail -1 | xargs printf "   %s\r\n" >> $output
 
-grep "Total_core_cache_stats_breakdown\[GLOBAL_ACC_W\]\[MISS\] =" output_* | tail -1 | xargs printf "   %s\n" >> $output
-grep "Total_core_cache_stats_breakdown\[GLOBAL_ACC_W\]\[RESERVATION_FAIL\] =" output_* | tail -1 | xargs printf "   %s\n" >> $output
+grep "Total_core_cache_stats_breakdown\[GLOBAL_ACC_R\]\[HIT\] =" output_* | tail -1 | xargs printf "   %s\r\n" >> $output
+grep "Total_core_cache_stats_breakdown\[GLOBAL_ACC_R\]\[HIT_RESERVED\] =" output_* | tail -1 | xargs printf "   %s\r\n" >> $output
+grep "Total_core_cache_stats_breakdown\[GLOBAL_ACC_R\]\[MISS\] =" output_* | tail -1 | xargs printf "   %s\r\n" >> $output
+grep "Total_core_cache_stats_breakdown\[GLOBAL_ACC_R\]\[RESERVATION_FAIL\] =" output_* | tail -1 | xargs printf "   %s\r\n" >> $output
 
-grep "n_cmd=" output_* | tail -1 | xargs printf "   %s\n" >> $output
-grep "n_activity=" output_* | tail -1 | xargs printf "   %s\n" >> $output
+grep "Total_core_cache_stats_breakdown\[GLOBAL_ACC_W\]\[MISS\] =" output_* | tail -1 | xargs printf "   %s\r\n" >> $output
+grep "Total_core_cache_stats_breakdown\[GLOBAL_ACC_W\]\[RESERVATION_FAIL\] =" output_* | tail -1 | xargs printf "   %s\r\n" >> $output
 
-grep "L2_total_cache_accesses =" output_* | tail -1 | xargs printf "   %s\n" >> $output
-grep "L2_total_cache_misses =" output_* | tail -1 | xargs printf "   %s\n" >> $output
-grep "L2_total_cache_miss_rate =" output_* | tail -1 | xargs printf "   %s\n" >> $output
-grep "L2_total_cache_reservation_fails =" output_* | tail -1 | xargs printf "   %s\n" >> $output
+grep "n_cmd=" output_* | tail -1 | xargs printf "   %s\r\n" >> $output
+grep "n_activity=" output_* | tail -1 | xargs printf "   %s\r\n" >> $output
 
-grep "L2_cache_stats_breakdown\[GLOBAL_ACC_R\]\[HIT\] =" output_* | tail -1 | xargs printf "   %s\n" >> $output
-grep "L2_cache_stats_breakdown\[GLOBAL_ACC_R\]\[HIT_RESERVED\] =" output_* | tail -1 | xargs printf "   %s\n" >> $output
-grep "L2_cache_stats_breakdown\[GLOBAL_ACC_R\]\[MISS\] =" output_* | tail -1 | xargs printf "   %s\n" >> $output
-grep "L2_cache_stats_breakdown\[GLOBAL_ACC_R\]\[RESERVATION_FAIL\] =" output_* | tail -1 | xargs printf "   %s\n" >> $output
+grep "L2_total_cache_accesses =" output_* | tail -1 | xargs printf "   %s\r\n" >> $output
+grep "L2_total_cache_misses =" output_* | tail -1 | xargs printf "   %s\r\n" >> $output
+grep "L2_total_cache_miss_rate =" output_* | tail -1 | xargs printf "   %s\r\n" >> $output
+grep "L2_total_cache_reservation_fails =" output_* | tail -1 | xargs printf "   %s\r\n" >> $output
 
-grep "L2_cache_stats_breakdown\[GLOBAL_ACC_W\]\[HIT\] =" output_* | tail -1 | xargs printf "   %s\n" >> $output
-grep "L2_cache_stats_breakdown\[GLOBAL_ACC_W\]\[HIT_RESERVED\] =" output_* | tail -1 | xargs printf "   %s\n" >> $output
-grep "L2_cache_stats_breakdown\[GLOBAL_ACC_W\]\[MISS\] =" output_* | tail -1 | xargs printf "   %s\n" >> $output
-grep "L2_cache_stats_breakdown\[GLOBAL_ACC_W\]\[RESERVATION_FAIL\] =" output_* | tail -1 | xargs printf "   %s\n" >> $output
+grep "L2_cache_stats_breakdown\[GLOBAL_ACC_R\]\[HIT\] =" output_* | tail -1 | xargs printf "   %s\r\n" >> $output
+grep "L2_cache_stats_breakdown\[GLOBAL_ACC_R\]\[HIT_RESERVED\] =" output_* | tail -1 | xargs printf "   %s\r\n" >> $output
+grep "L2_cache_stats_breakdown\[GLOBAL_ACC_R\]\[MISS\] =" output_* | tail -1 | xargs printf "   %s\r\n" >> $output
+grep "L2_cache_stats_breakdown\[GLOBAL_ACC_R\]\[RESERVATION_FAIL\] =" output_* | tail -1 | xargs printf "   %s\r\n" >> $output
+
+grep "L2_cache_stats_breakdown\[GLOBAL_ACC_W\]\[HIT\] =" output_* | tail -1 | xargs printf "   %s\r\n" >> $output
+grep "L2_cache_stats_breakdown\[GLOBAL_ACC_W\]\[HIT_RESERVED\] =" output_* | tail -1 | xargs printf "   %s\r\n" >> $output
+grep "L2_cache_stats_breakdown\[GLOBAL_ACC_W\]\[MISS\] =" output_* | tail -1 | xargs printf "   %s\r\n" >> $output
+grep "L2_cache_stats_breakdown\[GLOBAL_ACC_W\]\[RESERVATION_FAIL\] =" output_* | tail -1 | xargs printf "   %s\r\n" >> $output
 done

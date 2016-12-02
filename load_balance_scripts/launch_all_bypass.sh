2907121b5024bc2853cfee3ab9e5520117e3ddd9
woodun <hwang07@email.wm.edu>
2016-12-01 19:09:36 -0500

    debug

--- load_balance_scripts/launch_all_bypass.sh
+++ load_balance_scripts/launch_all_bypass.sh
@@ -1,20 +1,20 @@
 ######################launched
 for benchmark in GESUMMV MVT 2MM SYRK ATAX BICG 3DCONV GEMM
 do
-sh launch_all_configs_approx.sh $benchmark polybench
+sh launch_all_configs_bypass.sh $benchmark polybench
 done
 
 for benchmark in Triad
 do
-sh launch_all_configs_approx.sh $benchmark shoc
+sh launch_all_configs_bypass.sh $benchmark shoc
 done
 
 #for benchmark in TRA SCP LPS CONS
 #do
-#sh launch_all_configs_approx.sh $benchmark CUDA
+#sh launch_all_configs_bypass.sh $benchmark CUDA
 #done
 
 for benchmark in histo
 do
-sh launch_all_configs_approx.sh $benchmark parboil
+sh launch_all_configs_bypass.sh $benchmark parboil
 done

b9b2f8ce187093c839ef9d71831891c79e087595
woodun <hwang07@email.wm.edu>
2016-12-04 20:51:02 -0500

    debug

--- load_balance_scripts/collect_all_configs_tsp.sh
+++ load_balance_scripts/collect_all_configs_tsp.sh
@@ -10,8 +10,121 @@ configs_stor2=/stor2/hwang07/tsp/
 #specify your output file
 output=/stor1/hwang07/collect_tsp/$2_$1.txt
 
+#############################################################################################predictable rate#####################
+#############################################################with cache 0
+for stor1_config in tsp_address_exp_gtoswl1 tsp_address_exp_gtoswl4 tsp_address_exp_gtoswl8 tsp_address_exp_gtoswl16 tsp_address_exp_gtoswl48
+do
+cd $configs_stor2
+cd $stor1_config
+cd $2/$1/
+grep -o ", predictable rate:[eE\-\+0-9\.]*" output_* | tail -1 | sed -e "s/, predictable rate://g" | xargs printf "   %s " >> $output
+done
+
+printf "\n" >> $output
+for stor2_config in tsp_address_exp_lrrswl1 tsp_address_exp_lrrswl4 tsp_address_exp_lrrswl8 tsp_address_exp_lrrswl16 tsp_address_exp_lrrswl48
+do
+cd $configs_stor2
+cd $stor2_config
+cd $2/$1/
+grep -o ", predictable rate:[eE\-\+0-9\.]*" output_* | tail -1 | sed -e "s/, predictable rate://g" | xargs printf "   %s " >> $output
+done
+
+printf "\n" >> $output
+for stor2_config in tsp_address_exp_RR1 tsp_address_exp_RR4 tsp_address_exp_RR8 tsp_address_exp_RR16 tsp_address_exp_RR48
+do
+cd $configs_stor2
+cd $stor2_config
+cd $2/$1/
+grep -o ", predictable rate:[eE\-\+0-9\.]*" output_* | tail -1 | sed -e "s/, predictable rate://g" | xargs printf "   %s " >> $output
+done
+
+#############################################################with cache 100
+printf "\n" >> $output
+for stor1_config in tsp_address_gtoswl1 tsp_address_gtoswl4 tsp_address_gtoswl8 tsp_address_gtoswl16 tsp_address_gtoswl48
+do
+cd $configs_stor1
+cd $stor1_config
+cd $2/$1/
+grep -o ", predictable rate:[eE\-\+0-9\.]*" output_* | tail -1 | sed -e "s/, predictable rate://g" | xargs printf "   %s " >> $output
+done
+
+printf "\n" >> $output
+for stor2_config in tsp_address_lrrswl1 tsp_address_lrrswl4 tsp_address_lrrswl8 tsp_address_lrrswl16 tsp_address_lrrswl48
+do
+cd $configs_stor1
+cd $stor2_config
+cd $2/$1/
+grep -o ", predictable rate:[eE\-\+0-9\.]*" output_* | tail -1 | sed -e "s/, predictable rate://g" | xargs printf "   %s " >> $output
+done
+
+printf "\n" >> $output
+for stor2_config in tsp_address_RR1 tsp_address_RR4 tsp_address_RR8 tsp_address_RR16 tsp_address_RR48
+do
+cd $configs_stor1
+cd $stor2_config
+cd $2/$1/
+grep -o ", predictable rate:[eE\-\+0-9\.]*" output_* | tail -1 | sed -e "s/, predictable rate://g" | xargs printf "   %s " >> $output
+done
+
+#############################################################no cache 0
+printf "\n" >> $output
+for stor1_config in nocache_tsp_address_exp_gtoswl1 nocache_tsp_address_exp_gtoswl4 nocache_tsp_address_exp_gtoswl8 nocache_tsp_address_exp_gtoswl16 nocache_tsp_address_exp_gtoswl48
+do
+cd $configs_stor2
+cd $stor1_config
+cd $2/$1/
+grep -o ", predictable rate:[eE\-\+0-9\.]*" output_* | tail -1 | sed -e "s/, predictable rate://g" | xargs printf "   %s " >> $output
+done
+
+printf "\n" >> $output
+for stor2_config in nocache_tsp_address_exp_lrrswl1 nocache_tsp_address_exp_lrrswl4 nocache_tsp_address_exp_lrrswl8 nocache_tsp_address_exp_lrrswl16 nocache_tsp_address_exp_lrrswl48
+do
+cd $configs_stor2
+cd $stor2_config
+cd $2/$1/
+grep -o ", predictable rate:[eE\-\+0-9\.]*" output_* | tail -1 | sed -e "s/, predictable rate://g" | xargs printf "   %s " >> $output
+done
+
+printf "\n" >> $output
+for stor2_config in nocache_tsp_address_exp_RR1 nocache_tsp_address_exp_RR4 nocache_tsp_address_exp_RR8 nocache_tsp_address_exp_RR16 nocache_tsp_address_exp_RR48
+do
+cd $configs_stor2
+cd $stor2_config
+cd $2/$1/
+grep -o ", predictable rate:[eE\-\+0-9\.]*" output_* | tail -1 | sed -e "s/, predictable rate://g" | xargs printf "   %s " >> $output
+done
+
+#############################################################no cache 100
+printf "\n" >> $output
+for stor1_config in nocache_tsp_address_gtoswl1 nocache_tsp_address_gtoswl4 nocache_tsp_address_gtoswl8 nocache_tsp_address_gtoswl16 nocache_tsp_address_gtoswl48
+do
+cd $configs_stor1
+cd $stor1_config
+cd $2/$1/
+grep -o ", predictable rate:[eE\-\+0-9\.]*" output_* | tail -1 | sed -e "s/, predictable rate://g" | xargs printf "   %s " >> $output
+done
+
+printf "\n" >> $output
+for stor2_config in nocache_tsp_address_lrrswl1 nocache_tsp_address_lrrswl4 nocache_tsp_address_lrrswl8 nocache_tsp_address_lrrswl16 nocache_tsp_address_lrrswl48
+do
+cd $configs_stor1
+cd $stor2_config
+cd $2/$1/
+grep -o ", predictable rate:[eE\-\+0-9\.]*" output_* | tail -1 | sed -e "s/, predictable rate://g" | xargs printf "   %s " >> $output
+done
+
+printf "\n" >> $output
+for stor2_config in nocache_tsp_address_RR1 nocache_tsp_address_RR4 nocache_tsp_address_RR8 nocache_tsp_address_RR16 nocache_tsp_address_RR48
+do
+cd $configs_stor1
+cd $stor2_config
+cd $2/$1/
+grep -o ", predictable rate:[eE\-\+0-9\.]*" output_* | tail -1 | sed -e "s/, predictable rate://g" | xargs printf "   %s " >> $output
+done
+
 #############################################################################################bw_util#####################
 #############################################################with cache 0
+printf "\n" >> $output
 for stor1_config in tsp_address_exp_gtoswl1 tsp_address_exp_gtoswl4 tsp_address_exp_gtoswl8 tsp_address_exp_gtoswl16 tsp_address_exp_gtoswl48
 do
 cd $configs_stor2

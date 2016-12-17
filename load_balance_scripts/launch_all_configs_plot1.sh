24fc0922d24244fa48babff8083d82d89740d4e9
woodun <hwang07@email.wm.edu>
2016-12-17 09:08:21 -0500

    debug

--- load_balance_scripts/launch_all_configs_plot1.sh
+++ load_balance_scripts/launch_all_configs_plot1.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
-#cp -r /stor1/hwang07/insight_files/insight_template /stor1/hwang07/plot3/plot3_gto48
-#cd plot3_gto48
-#sh setup_ALL.sh plot3_gto48
+#cp -r /stor1/hwang07/insight_files/insight_template /stor1/hwang07/plot1/tsp_address_exp_gtoswl48_alloff
+#cd tsp_address_exp_gtoswl48_alloff
+#sh setup_ALL.sh tsp_address_exp_gtoswl48_alloff
+#cp -r "/stor1/hwang07/approx/data/" data
 ###################################################################################
 
 #specify your config path in stor1
 #configs_stor1=/stor1/hwang07/tsp_address_exp_gtoswl48_profile/
 
-for configs_stor1 in /stor1/hwang07/plot3/plot3_gto48 /stor2/hwang07/plot3/plot3_lrr48 /stor2/hwang07/plot3/plot3_RR48
+for configs_stor1 in /stor1/hwang07/plot1/tsp_address_exp_gtoswl48_alloff
 do
 
 #FDTD-2D(good, ready)
@@ -16,7 +17,7 @@ do
 #SYR2K(good, ready)
 cd $configs_stor1
 cd polybench
-for benchmark in GESUMMV MVT 2MM 3MM SYRK ATAX BICG 2DCONV 3DCONV GEMM
+for benchmark in FDTD-2D GRAMSCHM SYR2K
 do
 cd $benchmark
 qsub pbs_$benchmark.pbs
@@ -24,11 +25,11 @@ cd ..
 done
 
 #Spmv(unfixable) MD(unfixable) Stencil2D(need metric, only has ACT_percLossInQoR NAN now) Reduction(need metric)
-#Scan(ERROR: Failed to execute:)
+#Scan(ready)
 #QTC(corrupted double-linked list:)
 cd $configs_stor1
 cd shoc
-for benchmark in Triad
+for benchmark in Scan Spmv MD Stencil2D Reduction
 do
 cd $benchmark
 qsub pbs_$benchmark.pbs
@@ -41,7 +42,7 @@ done
 #pf_float(Assertion `t != m_TextureRefToCudaArray.end()' failed.)
 cd $configs_stor1
 cd rodinia
-for benchmark in srad_v1
+for benchmark in srad_v1 srad_v2 nw
 do
 cd $benchmark
 qsub pbs_$benchmark.pbs
@@ -51,7 +52,7 @@ done
 #lbm(segfault, but have output, try to have metric) spmv(Cannot open output file, but it seems fine, see ipc and try metric)
 cd $configs_stor1
 cd parboil
-for benchmark in histo
+for benchmark in lbm spmv
 do
 cd $benchmark
 qsub pbs_$benchmark.pbs
@@ -64,7 +65,7 @@ done
 #NN(good, metric needed)
 cd $configs_stor1
 cd CUDA
-for benchmark in TRA SCP CONS FWT LPS KMN BlackScholes
+for benchmark in JPEG RAY kmeans SLA NN
 do
 cd $benchmark
 qsub pbs_$benchmark.pbs
@@ -72,15 +73,16 @@ cd ..
 done
 
 #PageViewCount(ERROR * access to memory 'global' is unaligned) 
-#MatrixMul(good)
+
 #Kmeans(Assertion `isspace_shared(smid, addr)' failed.)
 #InvertedIndex (addr_t generic_to_shared(unsigned int, addr_t): Assertion `isspace_shared(smid, addr)' failed.)
 #PageViewRank (Assertion `block_address == line_size_based_tag_func(addr+data_size_coales-1,segment_size)' failed.)
 #SimilarityScore (Assertion `isspace_shared(smid, addr)' failed.)
-#WordCount (cat: sample: No such file or directory)
+#WordCount (seems good)
+#MatrixMul(good)
 cd $configs_stor1
 cd Mars
-for benchmark in
+for benchmark in MatrixMul WordCount
 do
 cd $benchmark
 qsub pbs_$benchmark.pbs
--- load_balance_scripts/launch_all_configs_plot2_re.sh
+++ load_balance_scripts/launch_all_configs_plot2_re.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-#cp -r /stor1/hwang07/insight_files/insight_template /stor1/hwang07/plot3/plot3_gto48
-#cd plot3_gto48
-#sh setup_ALL.sh plot3_gto48
+#cp -r /stor1/hwang07/insight_files/insight_template /stor1/hwang07/plot2/plot2_125gto48
+#cd tsp_address_exp_gtoswl48_profile
+#sh setup_ALL.sh approx_125coverage_gtoswl48
 ###################################################################################
 
 #specify your config path in stor1
 #configs_stor1=/stor1/hwang07/tsp_address_exp_gtoswl48_profile/
 
-for configs_stor1 in /stor1/hwang07/plot3/plot3_gto48 /stor2/hwang07/plot3/plot3_lrr48 /stor2/hwang07/plot3/plot3_RR48
+for configs_stor1 in /stor1/hwang07/plot2/plot2_125gto48 /stor1/hwang07/plot2/plot2_25gto48 /stor1/hwang07/plot2/plot2_50gto48 /stor1/hwang07/plot2/plot2_75gto48
 do
 
 #FDTD-2D(good, ready)
@@ -16,19 +16,20 @@ do
 #SYR2K(good, ready)
 cd $configs_stor1
 cd polybench
-for benchmark in GESUMMV MVT 2MM 3MM SYRK ATAX BICG 2DCONV 3DCONV GEMM
+for benchmark in FDTD-2D GRAMSCHM SYR2K
 do
 cd $benchmark
 qsub pbs_$benchmark.pbs
 cd ..
 done
 
-#Spmv(unfixable) MD(unfixable) Stencil2D(need metric, only has ACT_percLossInQoR NAN now) Reduction(need metric)
-#Scan(ERROR: Failed to execute:)
+#Spmv(unfixable) MD(unfixable) Stencil2D(need metric, only has ACT_percLossInQoR NAN now, add noskip) Reduction(need metric, already has diff, should be easy)
+#Scan(ready)
 #QTC(corrupted double-linked list:)
+#Spmv MD Stencil2D Reduction
 cd $configs_stor1
 cd shoc
-for benchmark in Triad
+for benchmark in Scan
 do
 cd $benchmark
 qsub pbs_$benchmark.pbs
@@ -39,9 +40,10 @@ done
 #backprop lud hotspot(not interesting.)
 #nw(good. metric needed.)
 #pf_float(Assertion `t != m_TextureRefToCudaArray.end()' failed.)
+#srad_v2
 cd $configs_stor1
 cd rodinia
-for benchmark in srad_v1
+for benchmark in srad_v1 nw
 do
 cd $benchmark
 qsub pbs_$benchmark.pbs
@@ -49,9 +51,10 @@ cd ..
 done
 
 #lbm(segfault, but have output, try to have metric) spmv(Cannot open output file, but it seems fine, see ipc and try metric)
+#lbm spmv
 cd $configs_stor1
 cd parboil
-for benchmark in histo
+for benchmark in 
 do
 cd $benchmark
 qsub pbs_$benchmark.pbs
@@ -62,9 +65,10 @@ done
 #SLA(good, ready)
 #CP(no metric and not interesting)
 #NN(good, metric needed)
+#JPEG RAY kmeans
 cd $configs_stor1
 cd CUDA
-for benchmark in TRA SCP CONS FWT LPS KMN BlackScholes
+for benchmark in SLA NN
 do
 cd $benchmark
 qsub pbs_$benchmark.pbs
@@ -72,15 +76,16 @@ cd ..
 done
 
 #PageViewCount(ERROR * access to memory 'global' is unaligned) 
-#MatrixMul(good)
+
 #Kmeans(Assertion `isspace_shared(smid, addr)' failed.)
 #InvertedIndex (addr_t generic_to_shared(unsigned int, addr_t): Assertion `isspace_shared(smid, addr)' failed.)
 #PageViewRank (Assertion `block_address == line_size_based_tag_func(addr+data_size_coales-1,segment_size)' failed.)
 #SimilarityScore (Assertion `isspace_shared(smid, addr)' failed.)
-#WordCount (cat: sample: No such file or directory)
+#WordCount (seems good)
+#MatrixMul(good)
 cd $configs_stor1
 cd Mars
-for benchmark in
+for benchmark in MatrixMul WordCount
 do
 cd $benchmark
 qsub pbs_$benchmark.pbs

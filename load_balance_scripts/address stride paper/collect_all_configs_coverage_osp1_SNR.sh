35c2c395d84a51825cbd54b6de19f2bfdc498e71
woodun <hwang07@email.wm.edu>
2017-01-22 10:21:55 -0500

    debug

--- load_balance_scripts/address stride paper/collect_all_configs_coverage_osp1_SNR.sh
+++ load_balance_scripts/address stride paper/collect_all_configs_coverage_osp1_SNR.sh
@@ -8,28 +8,22 @@ output=/stor1/hwang07/SNR_collection/osp1
 for configs_stor1 in coverage25_osp_gto48 coverage50_osp_gto48 coverage75_osp_gto48 coverage100_osp_gto48
 do
 
-#13
+
 mkdir $output/$configs_stor1
-cd $source/$configs_stor1
-cd polybench
+
+#13
 for benchmark in GESUMMV MVT 2MM 3MM SYRK ATAX BICG 2DCONV 3DCONV GEMM FDTD-2D GRAMSCHM SYR2K
 do
-cd $benchmark
 cp $source/$configs_stor1/polybench/$benchmark/*_CPU.txt $output/$configs_stor1/
 cp $source/$configs_stor1/polybench/$benchmark/*_GPU.txt $output/$configs_stor1/
-cd ..
 done
 
 #figures: JPEG RAY srad_v1 histo
 #7
-cd $configs_stor1
-cd CUDA
 for benchmark in TRA SCP CONS FWT LPS BlackScholes SLA
 do
-cd $benchmark
 cp $source/$configs_stor1/CUDA/$benchmark/*_CPU.txt $output/$configs_stor1/
 cp $source/$configs_stor1/CUDA/$benchmark/*_GPU.txt $output/$configs_stor1/
-cd ..
 done
 
 done

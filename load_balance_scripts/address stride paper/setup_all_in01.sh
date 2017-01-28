441d8fa4b0408a91d3ca35ad665c4bf770a099eb
woodun <hwang07@email.wm.edu>
2017-01-28 10:08:45 -0500

    debug

--- load_balance_scripts/address stride paper/setup_all_in01.sh
+++ load_balance_scripts/address stride paper/setup_all_in01.sh
@@ -5,7 +5,7 @@ cd /home/hwang07/stor1/coverage_dynamic_nopc_S
 for dirs in `ls`
 do
 
-cd dirs
+cd $dirs
 #sh setup_ALL.sh dirs
 pwd
 cd ..
--- load_balance_scripts/address stride paper/setup_all_in02.sh
+++ load_balance_scripts/address stride paper/setup_all_in02.sh
@@ -5,7 +5,7 @@ cd /home/hwang07/stor2/coverage_dynamic_S
 for dirs in `ls`
 do
 
-cd dirs
+cd $dirs
 sh setup_ALL.sh dirs
 cd ..
 

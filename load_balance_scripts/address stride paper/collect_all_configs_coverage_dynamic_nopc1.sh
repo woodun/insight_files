4094a7863461a3cd7f8d67cdfe0fd05ecfacfc25
woodun <hwang07@email.wm.edu>
2017-01-28 23:13:39 -0500

    debug

---
+++ load_balance_scripts/address stride paper/collect_all_configs_coverage_dynamic_nopc1.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+#specify your output file
+output=/stor1/hwang07/paper_collection/coverage_dynamic_nopc1.txt
+
+for statistics in 'gpu_tot_ipc =' 'bwutil_global_read=' 'bwutil_global_write=' 'Total_core_cache_stats_breakdown\[GLOBAL_ACC_R\]\[MISS\] =' 'predictor coverage0:'
+do
+
+#specify your config path in stor1
+for configs_stor1 in /stor2/hwang07/coverage_dynamic_nopc_S/coverage50_dynamic_nopc_gto48 /stor2/hwang07/coverage_dynamic_nopc_S/coverage50_dynamic_nopc_lrr48 /stor2/hwang07/coverage_dynamic_nopc_S/coverage50_dynamic_nopc_RR48
+do
+
+#13
+cd $configs_stor1
+cd polybench
+for benchmark in GESUMMV MVT 2MM 3MM SYRK ATAX BICG 2DCONV 3DCONV GEMM FDTD-2D GRAMSCHM SYR2K
+do
+cd $benchmark
+grep -o "$statistics[ ]*[-eE\+0-9\.]*" output_* | tail -1 | sed -e "s/$statistics[ ]*//g" | xargs printf "0%s " >> $output
+cd ..
+done
+
+#figures: JPEG RAY srad_v1 histo
+#7
+cd $configs_stor1
+cd CUDA
+for benchmark in TRA SCP CONS FWT LPS BlackScholes SLA
+do
+cd $benchmark
+grep -o "$statistics[ ]*[-eE\+0-9\.]*" output_* | tail -1 | sed -e "s/$statistics[ ]*//g" | xargs printf "0%s " >> $output
+cd ..
+done
+
+printf "\r\n" >> $output
+done
+######################################################################################################################################
+
+done

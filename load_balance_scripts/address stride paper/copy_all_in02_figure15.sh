c7d822de2ccb72176a5064ee9e7c7c33ee1ed023
woodun <hwang07@email.wm.edu>
2017-01-29 17:35:44 -0500

    debug

--- load_balance_scripts/address stride paper/copy_all_in02_figure15.sh
+++ load_balance_scripts/address stride paper/copy_all_in02_figure15.sh
@@ -20,15 +20,19 @@ cd /stor2/hwang07/coverage_tsp_S/coverage44_tsp_gto48
 sh setup_ALL.sh coverage44_tsp_gto48
 #cp -r /stor1/hwang07/insight_files/insight_template /stor2/hwang07/coverage_tsp_S/coverage50_tsp_gto48
 
-cp -r /stor1/hwang07/insight_files/insight_template /stor2/hwang07/coverage_osp_nopc_S/coverage25_tsp_nopc_gto48
-cd /stor2/hwang07/coverage_osp_nopc_S/coverage25_tsp_nopc_gto48
-sh setup_ALL.sh coverage25_tsp_nopc_gto48
-cp -r /stor1/hwang07/insight_files/insight_template /stor2/hwang07/coverage_osp_nopc_S/coverage40_tsp_nopc_gto48
-cd /stor2/hwang07/coverage_osp_nopc_S/coverage40_tsp_nopc_gto48
-sh setup_ALL.sh coverage40_tsp_nopc_gto48
-cp -r /stor1/hwang07/insight_files/insight_template /stor2/hwang07/coverage_osp_nopc_S/coverage44_tsp_nopc_gto48
-cd /stor2/hwang07/coverage_osp_nopc_S/coverage44_tsp_nopc_gto48
-sh setup_ALL.sh coverage44_tsp_nopc_gto48
-cp -r /stor1/hwang07/insight_files/insight_template /stor2/hwang07/coverage_osp_nopc_S/coverage50_tsp_nopc_gto48
-cd /stor2/hwang07/coverage_osp_nopc_S/coverage50_tsp_nopc_gto48
-sh setup_ALL.sh coverage50_tsp_nopc_gto48
+rm -r /stor2/hwang07/coverage_osp_nopc_S/coverage25_tsp_nopc_gto48
+rm -r /stor2/hwang07/coverage_osp_nopc_S/coverage40_tsp_nopc_gto48
+rm -r /stor2/hwang07/coverage_osp_nopc_S/coverage44_tsp_nopc_gto48
+rm -r /stor2/hwang07/coverage_osp_nopc_S/coverage50_tsp_nopc_gto48
+cp -r /stor1/hwang07/insight_files/insight_template /stor2/hwang07/coverage_osp_nopc_S/coverage25_osp_nopc_gto48
+cd /stor2/hwang07/coverage_osp_nopc_S/coverage25_osp_nopc_gto48
+sh setup_ALL.sh coverage25_osp_nopc_gto48
+cp -r /stor1/hwang07/insight_files/insight_template /stor2/hwang07/coverage_osp_nopc_S/coverage40_osp_nopc_gto48
+cd /stor2/hwang07/coverage_osp_nopc_S/coverage40_osp_nopc_gto48
+sh setup_ALL.sh coverage40_osp_nopc_gto48
+cp -r /stor1/hwang07/insight_files/insight_template /stor2/hwang07/coverage_osp_nopc_S/coverage44_osp_nopc_gto48
+cd /stor2/hwang07/coverage_osp_nopc_S/coverage44_osp_nopc_gto48
+sh setup_ALL.sh coverage44_osp_nopc_gto48
+cp -r /stor1/hwang07/insight_files/insight_template /stor2/hwang07/coverage_osp_nopc_S/coverage50_osp_nopc_gto48
+cd /stor2/hwang07/coverage_osp_nopc_S/coverage50_osp_nopc_gto48
+sh setup_ALL.sh coverage50_osp_nopc_gto48
---
+++ load_balance_scripts/address stride paper/temp.sh
@@ -0,0 +1,16 @@
+rm -r /stor2/hwang07/coverage_osp_nopc_S/coverage25_tsp_nopc_gto48
+rm -r /stor2/hwang07/coverage_osp_nopc_S/coverage40_tsp_nopc_gto48
+rm -r /stor2/hwang07/coverage_osp_nopc_S/coverage44_tsp_nopc_gto48
+rm -r /stor2/hwang07/coverage_osp_nopc_S/coverage50_tsp_nopc_gto48
+cp -r /stor1/hwang07/insight_files/insight_template /stor2/hwang07/coverage_osp_nopc_S/coverage25_osp_nopc_gto48
+cd /stor2/hwang07/coverage_osp_nopc_S/coverage25_osp_nopc_gto48
+sh setup_ALL.sh coverage25_osp_nopc_gto48
+cp -r /stor1/hwang07/insight_files/insight_template /stor2/hwang07/coverage_osp_nopc_S/coverage40_osp_nopc_gto48
+cd /stor2/hwang07/coverage_osp_nopc_S/coverage40_osp_nopc_gto48
+sh setup_ALL.sh coverage40_osp_nopc_gto48
+cp -r /stor1/hwang07/insight_files/insight_template /stor2/hwang07/coverage_osp_nopc_S/coverage44_osp_nopc_gto48
+cd /stor2/hwang07/coverage_osp_nopc_S/coverage44_osp_nopc_gto48
+sh setup_ALL.sh coverage44_osp_nopc_gto48
+cp -r /stor1/hwang07/insight_files/insight_template /stor2/hwang07/coverage_osp_nopc_S/coverage50_osp_nopc_gto48
+cd /stor2/hwang07/coverage_osp_nopc_S/coverage50_osp_nopc_gto48
+sh setup_ALL.sh coverage50_osp_nopc_gto48

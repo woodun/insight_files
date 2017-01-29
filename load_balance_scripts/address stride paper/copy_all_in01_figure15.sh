34085da5a66cbcec80541e49d78fa718089edc79
woodun <hwang07@email.wm.edu>
2017-01-29 17:10:32 -0500

    debug

--- load_balance_scripts/address stride paper/copy_all_in01_figure15.sh
+++ load_balance_scripts/address stride paper/copy_all_in01_figure15.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+rm -r /stor1/hwang07/coverage_tsp_nopc_S/coverage25_tsp_nopc_gto48
+rm -r /stor1/hwang07/coverage_tsp_nopc_S/coverage40_tsp_nopc_gto48
+rm -r /stor1/hwang07/coverage_tsp_nopc_S/coverage44_tsp_nopc_gto48
+rm -r /stor1/hwang07/coverage_tsp_nopc_S/coverage50_tsp_nopc_gto48
 cp -r /stor1/hwang07/insight_files/insight_template /stor1/hwang07/coverage_tsp_nopc_S/coverage25_tsp_nopc_gto48
 cd /stor1/hwang07/coverage_tsp_nopc_S/coverage25_tsp_nopc_gto48
 sh setup_ALL.sh coverage25_tsp_nopc_gto48
@@ -13,6 +17,9 @@ cp -r /stor1/hwang07/insight_files/insight_template /stor1/hwang07/coverage_tsp_
 cd /stor1/hwang07/coverage_tsp_nopc_S/coverage50_tsp_nopc_gto48
 sh setup_ALL.sh coverage50_tsp_nopc_gto48
 
+rm -r /stor1/hwang07/warp_partition_osp_S/wp0_osp_coverage25_gto48
+rm -r /stor1/hwang07/warp_partition_osp_S/wp0_osp_coverage40_gto48
+rm -r /stor1/hwang07/warp_partition_osp_S/wp0_osp_coverage44_gto48
 cp -r /stor1/hwang07/insight_files/insight_template /stor1/hwang07/warp_partition_osp_S/wp0_osp_coverage25_gto48
 cd /stor1/hwang07/warp_partition_osp_S/wp0_osp_coverage25_gto48
 sh setup_ALL.sh wp0_osp_coverage25_gto48
@@ -24,6 +31,10 @@ cd /stor1/hwang07/warp_partition_osp_S/wp0_osp_coverage44_gto48
 sh setup_ALL.sh wp0_osp_coverage44_gto48
 #cp -r /stor1/hwang07/insight_files/insight_template /stor1/hwang07/warp_partition_osp_S/wp0_osp_coverage50_gto48
 
+rm -r /stor1/hwang07/warp_partition_tsp_S/wp0_tsp_coverage25_gto48
+rm -r /stor1/hwang07/warp_partition_tsp_S/wp0_tsp_coverage40_gto48
+rm -r /stor1/hwang07/warp_partition_tsp_S/wp0_tsp_coverage44_gto48
+rm -r /stor1/hwang07/warp_partition_tsp_S/wp0_tsp_coverage50_gto48
 cp -r /stor1/hwang07/insight_files/insight_template /stor1/hwang07/warp_partition_tsp_S/wp0_tsp_coverage25_gto48
 cd /stor1/hwang07/warp_partition_tsp_S/wp0_tsp_coverage25_gto48
 sh setup_ALL.sh wp0_tsp_coverage25_gto48

b97bbc10abb8243145fbb0ed012f4249ed7e9aab
woodun <hwang07@email.wm.edu>
2017-02-15 12:55:44 -0500

    debug

---
+++ load_balance_scripts/address stride paper/final_fig/all_off_fig4.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+#specify your output file
+output=/stor1/hwang07/paper_collection/all_off_fig4.txt
+
+for statistics in 'gpu_tot_ipc =' 'bwutil_global_read=' 'bwutil_global_write=' 'Total_core_cache_stats_breakdown\[GLOBAL_ACC_R\]\[MISS\] ='
+do
+
+#specify your config path in stor1
+for configs_stor1 in /stor2/hwang07/all_off_S/all_off_gto48
+do
+
+#13
+cd $configs_stor1
+cd polybench
+for benchmark in GESUMMV MVT SYRK ATAX BICG GRAMSCHM SYR2K
+do
+cd $benchmark
+grep -o "$statistics[ ]*[-eE\+0-9\.]*" output_* | tail -1 | sed -e "s/$statistics[ ]*//g" | xargs printf "0%s " >> $output
+cd ..
+done
+
+#figures: JPEG RAY srad_v1 histo
+#7
+cd $configs_stor1
+cd CUDA
+for benchmark in TRA LPS SLA
+do
+cd $benchmark
+grep -o "$statistics[ ]*[-eE\+0-9\.]*" output_* | tail -1 | sed -e "s/$statistics[ ]*//g" | xargs printf "0%s " >> $output
+cd ..
+done
+
+printf "\r\n" >> $output
+done
+######################################################################################################################################
+
+done

be19e699a3ab96cf9980b1b5b0ab71a193d5ed61
woodun <hwang07@email.wm.edu>
2017-03-19 18:17:17 -0400

    debug

--- load_balance_scripts/address stride paper/temp.sh
+++ load_balance_scripts/address stride paper/temp.sh
@@ -2,6 +2,8 @@ rm -r /stor2/hwang07/coverage_osp_nopc_S/coverage25_tsp_nopc_gto48
 rm -r /stor2/hwang07/coverage_osp_nopc_S/coverage40_tsp_nopc_gto48
 rm -r /stor2/hwang07/coverage_osp_nopc_S/coverage44_tsp_nopc_gto48
 rm -r /stor2/hwang07/coverage_osp_nopc_S/coverage50_tsp_nopc_gto48
+
+
 cp -r /stor1/hwang07/insight_files/insight_template /stor2/hwang07/coverage_osp_nopc_S/coverage25_osp_nopc_gto48
 cd /stor2/hwang07/coverage_osp_nopc_S/coverage25_osp_nopc_gto48
 sh setup_ALL.sh coverage25_osp_nopc_gto48
@@ -14,3 +16,8 @@ sh setup_ALL.sh coverage44_osp_nopc_gto48
 cp -r /stor1/hwang07/insight_files/insight_template /stor2/hwang07/coverage_osp_nopc_S/coverage50_osp_nopc_gto48
 cd /stor2/hwang07/coverage_osp_nopc_S/coverage50_osp_nopc_gto48
 sh setup_ALL.sh coverage50_osp_nopc_gto48
+
+
+cp -r /stor1/hwang07/insight_files/insight_template /stor1/hwang07/dynamic_noold_nopc_S/dynamic_noold_nopc_gto48
+cd dynamic_noold_nopc_gto48
+sh setup_ALL.sh dynamic_noold_nopc_gto48

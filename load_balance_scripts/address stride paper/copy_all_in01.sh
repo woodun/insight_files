0b25928c3bd6cc384ddeb80031a88955260f483a
woodun <hwang07@email.wm.edu>
2017-03-22 04:12:02 -0400

    debug

--- load_balance_scripts/address stride paper/copy_all_in01.sh
+++ load_balance_scripts/address stride paper/copy_all_in01.sh
@@ -1,11 +1,42 @@
 #!/bin/sh
 
-cp -r /stor1/hwang07/insight_files/insight_template /stor1/hwang07/coverage_dynamic_nopc_S/coverage75_dynamic_nopc_gto48
-cp -r /stor1/hwang07/insight_files/insight_template /stor1/hwang07/coverage_dynamic_nopc_S/coverage75_dynamic_nopc_lrr48
-cp -r /stor1/hwang07/insight_files/insight_template /stor1/hwang07/coverage_dynamic_nopc_S/coverage75_dynamic_nopc_RR48
-cp -r /stor1/hwang07/insight_files/insight_template /stor1/hwang07/coverage_dynamic_nopc_S/coverage50_dynamic_nopc_gto48
-cp -r /stor1/hwang07/insight_files/insight_template /stor1/hwang07/coverage_dynamic_nopc_S/coverage50_dynamic_nopc_lrr48
-cp -r /stor1/hwang07/insight_files/insight_template /stor1/hwang07/coverage_dynamic_nopc_S/coverage50_dynamic_nopc_RR48
-cp -r /stor1/hwang07/insight_files/insight_template /stor1/hwang07/coverage_dynamic_nopc_S/coverage25_dynamic_nopc_gto48
-cp -r /stor1/hwang07/insight_files/insight_template /stor1/hwang07/coverage_dynamic_nopc_S/coverage25_dynamic_nopc_lrr48
-cp -r /stor1/hwang07/insight_files/insight_template /stor1/hwang07/coverage_dynamic_nopc_S/coverage25_dynamic_nopc_RR48
+cp -r /stor1/hwang07/insight_files/insight_template /stor1/hwang07/dynamic_tsp_nopc_S/dynamic_tsp_nopc_gto48
+cd /stor1/hwang07/dynamic_tsp_nopc_S/dynamic_tsp_nopc_gto48
+sh setup_ALL.sh dynamic_tsp_nopc_gto48
+
+cp -r /stor1/hwang07/insight_files/insight_template /stor1/hwang07/dynamic_nolong_nopc_S/dynamic_nolong_nopc_gto48
+cd /stor1/hwang07/dynamic_nolong_nopc_S/dynamic_nolong_nopc_gto48
+sh setup_ALL.sh dynamic_nolong_nopc_gto48
+
+#######################################
+cp -r /stor1/hwang07/insight_files/insight_template /stor1/hwang07/dynamic_noold_nopc_S/dynamic_noold_nopc_size8_gto48
+cd /stor1/hwang07/dynamic_noold_nopc_S/dynamic_noold_nopc_size8_gto48
+sh setup_ALL.sh dynamic_noold_nopc_size8_gto48
+
+cp -r /stor1/hwang07/insight_files/insight_template /stor1/hwang07/dynamic_noold_nopc_S/dynamic_noold_nopc_size7_gto48
+cd /stor1/hwang07/dynamic_noold_nopc_S/dynamic_noold_nopc_size7_gto48
+sh setup_ALL.sh dynamic_noold_nopc_size7_gto48
+
+cp -r /stor1/hwang07/insight_files/insight_template /stor1/hwang07/dynamic_noold_nopc_S/dynamic_noold_nopc_size6_gto48
+cd /stor1/hwang07/dynamic_noold_nopc_S/dynamic_noold_nopc_size6_gto48
+sh setup_ALL.sh dynamic_noold_nopc_size6_gto48
+
+cp -r /stor1/hwang07/insight_files/insight_template /stor1/hwang07/dynamic_noold_nopc_S/dynamic_noold_nopc_size5_gto48
+cd /stor1/hwang07/dynamic_noold_nopc_S/dynamic_noold_nopc_size5_gto48
+sh setup_ALL.sh dynamic_noold_nopc_size5_gto48
+
+cp -r /stor1/hwang07/insight_files/insight_template /stor1/hwang07/dynamic_noold_nopc_S/dynamic_noold_nopc_size4_gto48
+cd /stor1/hwang07/dynamic_noold_nopc_S/dynamic_noold_nopc_size4_gto48
+sh setup_ALL.sh dynamic_noold_nopc_size4_gto48
+
+cp -r /stor1/hwang07/insight_files/insight_template /stor1/hwang07/dynamic_noold_nopc_S/dynamic_noold_nopc_size3_gto48
+cd /stor1/hwang07/dynamic_noold_nopc_S/dynamic_noold_nopc_size3_gto48
+sh setup_ALL.sh dynamic_noold_nopc_size3_gto48
+
+cp -r /stor1/hwang07/insight_files/insight_template /stor1/hwang07/dynamic_noold_nopc_S/dynamic_noold_nopc_size2_gto48
+cd /stor1/hwang07/dynamic_noold_nopc_S/dynamic_noold_nopc_size2_gto48
+sh setup_ALL.sh dynamic_noold_nopc_size2_gto48
+
+cp -r /stor1/hwang07/insight_files/insight_template /stor1/hwang07/dynamic_noold_nopc_S/dynamic_noold_nopc_size1_gto48
+cd /stor1/hwang07/dynamic_noold_nopc_S/dynamic_noold_nopc_size1_gto48
+sh setup_ALL.sh dynamic_noold_nopc_size1_gto48
---
+++ load_balance_scripts/address stride paper/launch_all_configs_dynamic_noold_nopc_allsizes.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+###################################################################################
+mother_dir=/stor1/hwang07/dynamic_noold_nopc_S
+
+#specify your config path in stor1
+for configs_stor1 in dynamic_noold_nopc_size8_gto48 dynamic_noold_nopc_size7_gto48 dynamic_noold_nopc_size6_gto48 dynamic_noold_nopc_size5_gto48 dynamic_noold_nopc_size4_gto48 dynamic_noold_nopc_size3_gto48 dynamic_noold_nopc_size2_gto48 dynamic_noold_nopc_size1_gto48
+do
+
+#13
+cd $mother_dir
+cd $configs_stor1
+cd polybench
+for benchmark in GESUMMV MVT 2MM 3MM SYRK ATAX BICG 2DCONV 3DCONV GEMM FDTD-2D GRAMSCHM SYR2K
+do
+cd $benchmark
+qsub pbs_$benchmark.pbs
+cd ..
+done
+
+#figures: JPEG RAY srad_v1 histo
+#7
+cd $mother_dir
+cd $configs_stor1
+cd CUDA
+for benchmark in TRA SCP CONS FWT LPS BlackScholes SLA
+do
+cd $benchmark
+qsub pbs_$benchmark.pbs
+cd ..
+done
+
+done

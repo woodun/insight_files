94add0cbbdd126190516c6de10bbead528d70525
woodun <hwang07@email.wm.edu>
2017-03-27 01:15:31 -0400

    debug

--- load_balance_scripts/address_stride_paper/new_figure15/calculate_SRE_setup_distribute.sh
+++ load_balance_scripts/address_stride_paper/new_figure15/calculate_SRE_setup_distribute.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 ########################################
-source_dir=/stor1/hwang07/insight_files/load_balance_scripts/address stride paper/new_figure15
+source_dir=/stor1/hwang07/insight_files/load_balance_scripts/address_stride_paper/new_figure15
 
 #cd $1
 cd /stor1/hwang07/coverage_dynamic_nopc_S/

905383faa1d97c06782710aa4675989f2da3d816
woodun <hwang07@email.wm.edu>
2017-03-30 21:18:08 -0400

    debug

---
+++ load_balance_scripts/address_stride_paper/new_figure15/collect_rate_in01.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+sh collect_rate_asap_osp.sh
+sh collect_rate_wp0_osp.sh
+sh collect_rate_wp2_osp.sh
+sh collect_rate_wp4_osp.sh
+sh collect_rate_wp6_osp.sh
---
+++ load_balance_scripts/address_stride_paper/new_figure15/collect_rate_in02.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+sh collect_rate_asap_tsp.sh
+sh collect_rate_wp0_tsp.sh
+sh collect_rate_wp2_tsp.sh
+sh collect_rate_wp4_tsp.sh
+sh collect_rate_wp6_tsp.sh

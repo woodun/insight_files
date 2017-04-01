1fc04797ae1d485f8f2a8b8c8a49b005dedeb6b2
woodun <hwang07@email.wm.edu>
2017-04-01 03:25:25 -0400

    debug

---
+++ load_balance_scripts/address_stride_paper/new_figure15/accurate/collect_HE_wp2_tsp.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+#specify your output file
+output=/stor1/hwang07/paper_collection/final_wp2_tsp_HE.txt
+mother_dir=/stor2/hwang07/final_accurate_tsp/
+
+for statistics in $1
+do
+
+#specify your config path in stor1
+for configs_stor1 in wp2_tsp_coverage10_gto48 wp2_tsp_coverage20_gto48 wp2_tsp_coverage30_gto48 wp2_tsp_coverage40_gto48 wp2_tsp_coverage50_gto48
+do
+
+#13
+cd $mother_dir
+cd $configs_stor1
+cd polybench
+for benchmark in GESUMMV MVT SYRK ATAX BICG SYR2K
+do
+cd $benchmark
+grep -o "$statistics[ ]*[-eE\+0-9\.]*" output_* | tail -1 | sed -e "s/$statistics[ ]*\(-$\)*//g" | xargs printf "0%s " >> $output
+cd ..
+done
+
+#figures: JPEG RAY srad_v1 histo
+#7
+cd $mother_dir
+cd $configs_stor1
+cd CUDA
+for benchmark in TRA LPS SLA
+do
+cd $benchmark
+grep -o "$statistics[ ]*[-eE\+0-9\.]*" output_* | tail -1 | sed -e "s/$statistics[ ]*\(-$\)*//g" | xargs printf "0%s " >> $output
+cd ..
+done
+
+printf "\r\n" >> $output
+done
+######################################################################################################################################
+
+done

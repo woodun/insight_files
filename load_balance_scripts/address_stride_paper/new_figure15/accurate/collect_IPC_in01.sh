ad10c0f75d6a43fbece60ef4468d8cedf189c1ef
woodun <hwang07@email.wm.edu>
2017-04-01 17:27:51 -0400

    debug

--- load_balance_scripts/address_stride_paper/new_figure15/accurate/collect_IPC_in01.sh
+++ load_balance_scripts/address_stride_paper/new_figure15/accurate/collect_IPC_in01.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
-token="gpu_tot_ipc ="
-
-sh collect_IPC_asap_osp.sh '$token' 
-sh collect_IPC_wp0_osp.sh '$token'
-sh collect_IPC_wp2_osp.sh '$token' 
-sh collect_IPC_wp4_osp.sh '$token' 
-sh collect_IPC_wp6_osp.sh '$token' 
+sh collect_IPC_asap_osp.sh 'gpu_tot_ipc =' final_asap_osp_IPC.txt
+sh collect_IPC_wp0_osp.sh 'gpu_tot_ipc ='
+sh collect_IPC_wp2_osp.sh 'gpu_tot_ipc =' 
+sh collect_IPC_wp4_osp.sh 'gpu_tot_ipc =' 
+sh collect_IPC_wp6_osp.sh 'gpu_tot_ipc =' 

a820e47270e646b51b4df0c250c280fd8a056963
woodun <hwang07@email.wm.edu>
2017-04-01 03:30:26 -0400

    DEBUG

--- load_balance_scripts/address_stride_paper/new_figure15/accurate/collect_HE_in01.sh
+++ load_balance_scripts/address_stride_paper/new_figure15/accurate/collect_HE_in01.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-sh collect_RE_asap_osp.sh 'average_avg_bias:'
-sh collect_RE_wp0_osp.sh 'average_avg_bias:'
-sh collect_RE_wp2_osp.sh 'average_avg_bias:'
-sh collect_RE_wp4_osp.sh 'average_avg_bias:'
-sh collect_RE_wp6_osp.sh 'average_avg_bias:'
+sh collect_HE_asap_osp.sh 'average_avg_bias:'
+sh collect_HE_wp0_osp.sh 'average_avg_bias:'
+sh collect_HE_wp2_osp.sh 'average_avg_bias:'
+sh collect_HE_wp4_osp.sh 'average_avg_bias:'
+sh collect_HE_wp6_osp.sh 'average_avg_bias:'
--- load_balance_scripts/address_stride_paper/new_figure15/accurate/collect_HE_in02.sh
+++ load_balance_scripts/address_stride_paper/new_figure15/accurate/collect_HE_in02.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-sh collect_RE_asap_tsp.sh 'average_avg_bias:'
-sh collect_RE_wp0_tsp.sh 'average_avg_bias:'
-sh collect_RE_wp2_tsp.sh 'average_avg_bias:'
-sh collect_RE_wp4_tsp.sh 'average_avg_bias:'
-sh collect_RE_wp6_tsp.sh 'average_avg_bias:'
+sh collect_HE_asap_tsp.sh 'average_avg_bias:'
+sh collect_HE_wp0_tsp.sh 'average_avg_bias:'
+sh collect_HE_wp2_tsp.sh 'average_avg_bias:'
+sh collect_HE_wp4_tsp.sh 'average_avg_bias:'
+sh collect_HE_wp6_tsp.sh 'average_avg_bias:'

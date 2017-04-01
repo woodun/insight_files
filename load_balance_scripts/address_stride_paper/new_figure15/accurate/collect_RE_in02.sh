4fa178169bf461d87982484ca3c50b277791a4a8
woodun <hwang07@email.wm.edu>
2017-03-31 21:06:51 -0400

    debug

--- load_balance_scripts/address_stride_paper/new_figure15/accurate/collect_RE_in02.sh
+++ load_balance_scripts/address_stride_paper/new_figure15/accurate/collect_RE_in02.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-sh collect_RE_asap_tsp.sh 'RE5:'
-sh collect_RE_wp0_tsp.sh 'RE5:'
-sh collect_RE_wp2_tsp.sh 'RE5:'
-sh collect_RE_wp4_tsp.sh 'RE5:'
-sh collect_RE_wp6_tsp.sh 'RE5:'
+sh collect_RE_asap_tsp.sh 'RE6:'
+sh collect_RE_wp0_tsp.sh 'RE6:'
+sh collect_RE_wp2_tsp.sh 'RE6:'
+sh collect_RE_wp4_tsp.sh 'RE6:'
+sh collect_RE_wp6_tsp.sh 'RE6:'

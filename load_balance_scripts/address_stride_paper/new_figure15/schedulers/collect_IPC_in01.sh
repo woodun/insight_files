4acf11746977720cd5e2e65c84016935d58edb6d
woodun <hwang07@email.wm.edu>
2017-04-03 09:12:15 -0400

    debug

--- load_balance_scripts/address_stride_paper/new_figure15/schedulers/collect_IPC_in01.sh
+++ load_balance_scripts/address_stride_paper/new_figure15/schedulers/collect_IPC_in01.sh
@@ -2,8 +2,8 @@
 
 #'gpu_tot_ipc =' 'average_avg_bias:' 'Stall:' 'predictor coverage0:'
 
-sh collect_IPC_asap_osp.sh 'predictable lines:' schedulers_asap_osp_Misses.txt
-sh collect_IPC_wp0_osp.sh 'predictable lines:' schedulers_wp0_osp_Misses.txt
-sh collect_IPC_wp2_osp.sh 'predictable lines:' schedulers_wp2_osp_Misses.txt
-#sh collect_IPC_wp4_osp.sh 'predictable lines:' schedulers_wp4_osp_Misses.txt
-sh collect_IPC_wp6_osp.sh 'predictable lines:' schedulers_wp6_osp_Misses.txt
+sh collect_IPC_asap_osp.sh 'average_avg_bias:' schedulers_asap_osp_HE.txt
+sh collect_IPC_wp0_osp.sh 'average_avg_bias:' schedulers_wp0_osp_HE.txt
+sh collect_IPC_wp2_osp.sh 'average_avg_bias:' schedulers_wp2_osp_HE.txt
+#sh collect_IPC_wp4_osp.sh 'average_avg_bias:' schedulers_wp4_osp_HE.txt
+sh collect_IPC_wp6_osp.sh 'average_avg_bias:' schedulers_wp6_osp_HE.txt

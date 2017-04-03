99ae991c4180adcbef1c042864239b55c2d23803
woodun <hwang07@email.wm.edu>
2017-04-02 23:31:54 -0400

    debug

--- load_balance_scripts/address_stride_paper/new_figure15/schedulers/copy_schedulers_in01.sh
+++ load_balance_scripts/address_stride_paper/new_figure15/schedulers/copy_schedulers_in01.sh
@@ -5,7 +5,7 @@ template=/stor1/hwang07/insight_files/insight_template
 #####################################################################################
 mother_dir=/stor1/hwang07/final_schedulers_osp
 
-for configs_stor1 in coverage100_dynamic_osp_nopc_size8_lrr48 coverage100_dynamic_osp_nopc_size8_RR48 coverage20_dynamic_osp_nopc_size8_lrr48 coverage40_dynamic_osp_nopc_size8_lrr48 coverage20_dynamic_osp_nopc_size8_RR48 coverage40_dynamic_osp_nopc_size8_RR48 wp0_osp_coverage20_lrr48 wp0_osp_coverage40_lrr48 wp0_osp_coverage20_RR48 wp0_osp_coverage40_RR48 wp2_osp_coverage20_lrr48 wp2_osp_coverage40_lrr48 wp2_osp_coverage20_RR48 wp2_osp_coverage40_RR48 wp6_osp_coverage20_lrr48 wp6_osp_coverage40_lrr48 wp6_osp_coverage20_RR48 wp6_osp_coverage40_RR48 
+for configs_stor1 in coverage100_dynamic_osp_nopc_size8_lrr48 coverage100_dynamic_osp_nopc_size8_RR48 coverage20_dynamic_osp_nopc_size8_lrr48 coverage20_dynamic_osp_nopc_size8_RR48 wp0_osp_coverage20_lrr48 wp0_osp_coverage20_RR48 wp2_osp_coverage20_lrr48 wp2_osp_coverage20_RR48 wp6_osp_coverage20_lrr48 wp6_osp_coverage20_RR48
 do
 
 cp -r $template $mother_dir/$configs_stor1
--- load_balance_scripts/address_stride_paper/new_figure15/schedulers/copy_schedulers_in02.sh
+++ load_balance_scripts/address_stride_paper/new_figure15/schedulers/copy_schedulers_in02.sh
@@ -5,7 +5,7 @@ template=/stor1/hwang07/insight_files/insight_template
 #####################################################################################
 mother_dir=/stor2/hwang07/final_schedulers_tsp
 
-for configs_stor1 in coverage100_dynamic_tsp_nopc_size8_lrr48 coverage100_dynamic_tsp_nopc_size8_RR48 coverage20_dynamic_tsp_nopc_size8_lrr48 coverage40_dynamic_tsp_nopc_size8_lrr48 coverage20_dynamic_tsp_nopc_size8_RR48 coverage40_dynamic_tsp_nopc_size8_RR48 wp0_tsp_coverage20_lrr48 wp0_tsp_coverage40_lrr48 wp0_tsp_coverage20_RR48 wp0_tsp_coverage40_RR48 wp2_tsp_coverage20_lrr48 wp2_tsp_coverage40_lrr48 wp2_tsp_coverage20_RR48 wp2_tsp_coverage40_RR48 wp6_tsp_coverage20_lrr48 wp6_tsp_coverage40_lrr48 wp6_tsp_coverage20_RR48 wp6_tsp_coverage40_RR48
+for configs_stor1 in coverage100_dynamic_tsp_nopc_size8_lrr48 coverage100_dynamic_tsp_nopc_size8_RR48 coverage20_dynamic_tsp_nopc_size8_lrr48 coverage20_dynamic_tsp_nopc_size8_RR48 wp0_tsp_coverage20_lrr48 wp0_tsp_coverage20_RR48 wp2_tsp_coverage20_lrr48 wp2_tsp_coverage20_RR48 wp6_tsp_coverage20_lrr48 wp6_tsp_coverage20_RR48
 do
 
 cp -r $template $mother_dir/$configs_stor1

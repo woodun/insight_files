f27e4e86bb74844ec15714613e175a3bc260577f
woodun <hwang07@email.wm.edu>
2017-04-28 04:28:35 -0400

    debug

---
+++ hima_scripts/amc_paper/collect_profileinf_gto_hit0.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+#specify your output file
+output=/sciclone/data10/hwang07/GPU_RESEARCH/amc_collection/profileinf_gto_hit0.txt
+mother_dir=/sciclone/data10/hwang07/GPU_RESEARCH/amc/profile
+
+for statistics in 'row_hit_0 all:'
+do
+
+#specify your config path in stor1
+for configs_stor1 in AMC_profile_sizeinf_gto48
+do
+
+#13
+cd $mother_dir
+cd $configs_stor1
+cd polybench
+for benchmark in GESUMMV 2MM 3MM BICG 2DCONV 3DCONV GEMM FDTD-2D
+do
+cd $benchmark
+grep "$statistics[ ]*[-eE\+0-9\.]*" output_* | tail -1 | sed -e "s/$statistics[ ]*\(-$\)*//g" | xargs printf "%s " >> $output
+cd ..
+done
+
+#figures: JPEG RAY srad_v1 histo
+#7
+cd $mother_dir
+cd $configs_stor1
+cd CUDA
+for benchmark in TRA SCP CONS FWT LPS BlackScholes
+do
+cd $benchmark
+grep "$statistics[ ]*[-eE\+0-9\.]*" output_* | tail -1 | sed -e "s/$statistics[ ]*\(-$\)*//g" | xargs printf "%s " >> $output
+cd ..
+done
+
+printf "\r\n" >> $output
+done
+######################################################################################################################################
+
+done

c436db50d1ac431e4d305feecdfdfdee01fd8d7a
woodun <hwang07@email.wm.edu>
2017-04-28 06:24:25 -0400

    debug

--- hima_scripts/amc_paper/collect_profile_gto_removed.sh
+++ hima_scripts/amc_paper/collect_profile_gto_removed.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #specify your output file
-output=/sciclone/data10/hwang07/GPU_RESEARCH/amc_collection/remove16_gto_removed.txt
+output=/sciclone/data10/hwang07/GPU_RESEARCH/amc_collection/profile16_gto_removed.txt
 mother_dir=/sciclone/data10/hwang07/GPU_RESEARCH/amc/profile
 
 for statistics in 'removed all:'

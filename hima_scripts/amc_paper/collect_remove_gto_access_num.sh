ba7e65730a79930c6e61eba7e4a1d51de6fb57e2
woodun <hwang07@email.wm.edu>
2017-04-29 14:41:14 -0400

    debug

---
+++ hima_scripts/amc_paper/collect_remove_gto_access_num.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+#specify your output file
+output=/sciclone/data10/hwang07/GPU_RESEARCH/amc_collection/remove16_gto_access.txt
+mother_dir=/sciclone/data10/hwang07/GPU_RESEARCH/amc/remove
+
+for statistics in 'overall accesses all:'
+do
+
+#specify your config path in stor1
+for configs_stor1 in AMC_remove_size16_gto48
+do
+
+#13
+cd $mother_dir
+cd $configs_stor1
+cd polybench
+for benchmark in GESUMMV 2MM 3MM BICG 2DCONV 3DCONV GEMM FDTD-2D
+do
+cd $benchmark
+grep "$statistics[ ]*[-eE\+0-9\.]*" output_* | tail -1 | sed -e "s/$statistics[ ]*\(-$\)*//g" | xargs printf "%s " >> $output
+cd ..
+done
+
+#figures: JPEG RAY srad_v1 histo
+#7
+cd $mother_dir
+cd $configs_stor1
+cd CUDA
+for benchmark in TRA SCP CONS FWT LPS BlackScholes
+do
+cd $benchmark
+grep "$statistics[ ]*[-eE\+0-9\.]*" output_* | tail -1 | sed -e "s/$statistics[ ]*\(-$\)*//g" | xargs printf "%s " >> $output
+cd ..
+done
+
+printf "\r\n" >> $output
+done
+######################################################################################################################################
+
+done

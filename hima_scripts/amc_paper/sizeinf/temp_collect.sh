0c384005fdc3096df58266f98651c11cbda055e1
woodun <hwang07@email.wm.edu>
2017-05-22 13:42:02 -0400

    debug

--- hima_scripts/amc_paper/sizeinf/temp_collect.sh
+++ hima_scripts/amc_paper/sizeinf/temp_collect.sh
@@ -1,5 +1,3 @@
-sh collect_nocache_noecho_sizeinf_delay_gto_all.sh
-sh collect_nocache_noecho_sizeinf_delayonly_gto_all.sh
+sh collect_noecho_sizeinf_delay_gto_all.sh
+sh collect_noecho_sizeinf_delayonly_gto_all.sh
 sh collect_nocache_profile_all.sh
-sh collect_nocache_sizeinf_delay_gto_all.sh
-sh collect_nocache_sizeinf_delayonly_gto_all.sh

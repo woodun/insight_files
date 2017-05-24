3195908d47e20b6818388f4039b40f19fae19ee8
woodun <hwang07@email.wm.edu>
2017-05-23 23:40:54 -0400

    debug

--- hima_scripts/amc_paper/size512/collect_profile_all.sh
+++ hima_scripts/amc_paper/size512/collect_profile_all.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #specify your output file
-output=/sciclone/data10/hwang07/GPU_RESEARCH/amc_collection/profile_all1.txt
+output=/sciclone/data10/hwang07/GPU_RESEARCH/amc_collection/profile_size512.txt
 mother_dir=/sciclone/data10/hwang07/GPU_RESEARCH/amc/profile
 
 for statistics in 'overall accesses all:' 'overall activations all:' 'bw_util=' 'echo in delay queue all:' 'number of global reads only activations:' 'accesses from global reads only activations:' 'gpu_tot_ipc =' 'L1D_total_cache_miss_rate =' 'L2_total_cache_miss_rate =' 'removed all:' 'delay queue full all:' 'row_hit_0 all:'
@@ -14,7 +14,7 @@ for benchmark in GESUMMV 2MM 3MM BICG 2DCONV 3DCONV GEMM
 do
 
 #specify your config path in stor1
-for configs_stor1 in AMC_profile_sizeinf_gto48
+for configs_stor1 in AMC_profile_size512_gto48
 do
 
 #13
@@ -35,7 +35,7 @@ for benchmark in SCP CONS FWT LPS BlackScholes
 do
 
 #specify your config path in stor1
-for configs_stor1 in AMC_profile_sizeinf_gto48
+for configs_stor1 in AMC_profile_size512_gto48
 do
 
 #figures: JPEG RAY srad_v1 histo

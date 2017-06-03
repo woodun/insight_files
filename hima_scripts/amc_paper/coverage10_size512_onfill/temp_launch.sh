91a7f6e8ca480e332b338b4648e4aeeb7339e4b2
woodun <hwang07@email.wm.edu>
2017-06-03 03:31:19 -0400

    debug

--- hima_scripts/amc_paper/coverage10_size512_onfill/temp_launch.sh
+++ hima_scripts/amc_paper/coverage10_size512_onfill/temp_launch.sh
@@ -1,3 +1,4 @@
+sh launch_profile_gto.sh
 sh launch_coverage10_noecho_size512_delay_gto.sh
 sh launch_coverage10_noecho_size512_delayonly_gto.sh
-sh launch_profile_onmiss_gto.sh
+#sh launch_profile_onmiss_gto.sh

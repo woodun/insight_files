50ab823a5ae56991d8a75ca754b22b7d2d8452ef
woodun <hwang07@email.wm.edu>
2017-06-26 17:55:55 -0400

    debug

---
+++ load_balance_scripts/address_stride_paper/new_figure15/modified/copy_coverages_in01.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+#this one use applications_FEB
+
+template=/stor1/hwang07/insight_files/insight_template
+
+#####################################################################################
+mother_dir=/stor1/hwang07/modified_osp
+
+for configs_stor1 in coverage100_dynamic_osp_nopc_size8_gto48 coverage10_dynamic_osp_nopc_size8_gto48
+do
+
+cp -r $template $mother_dir/$configs_stor1
+cd $mother_dir/$configs_stor1
+sh setup_ALL.sh $configs_stor1
+
+done
+
+
+
+
+
+
+
---
+++ load_balance_scripts/address_stride_paper/new_figure15/modified/launch_coverages_in01.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+#this one works with applications_modified
+
+#####################################################
+mother_dir=/stor1/hwang07/modified_osp
+
+for configs_stor1 in coverage100_dynamic_osp_nopc_size8_gto48 coverage10_dynamic_osp_nopc_size8_gto48
+do
+
+#13
+cd $mother_dir
+cd $configs_stor1
+cd polybench
+for benchmark in 2DCONV_BLUR 2DCONV_EDGE 2DCONV_EMBOSS 2DCONV_ENHANCE 2DCONV_SHARPEN 2DCONV 2MM 3DCONV 3MM ATAX BICG FDTD-2D GEMM GESUMMV GRAMSCHM MVT SYR2K SYRK
+do
+cd $benchmark
+qsub pbs_$benchmark.pbs
+cd ..
+done
+
+#figures: srad_v1 histo JPEG RAY
+#7
+cd $mother_dir
+cd $configs_stor1
+cd CUDA
+for benchmark in JPEG_ENCODE JPEG_DECODE RAY BlackScholes CONS SCP SLA TRA
+do
+cd $benchmark
+qsub pbs_$benchmark.pbs
+cd ..
+done
+
+done

87bb5ed6351bea17bb5908275acd487b61cc9d00
woodun <hwang07@email.wm.edu>
2017-06-30 06:13:47 -0400

    debug

--- load_balance_scripts/address_stride_paper/new_figure15/modified/launch_coverages_in01.sh
+++ load_balance_scripts/address_stride_paper/new_figure15/modified/launch_coverages_in01.sh
@@ -7,11 +7,12 @@ mother_dir=/stor1/hwang07/modified_osp
 for configs_stor1 in coverage100_dynamic_osp_nopc_size8_gto48 coverage10_dynamic_osp_nopc_size8_gto48
 do
 
+#not changed: 
 #13
 cd $mother_dir
 cd $configs_stor1
 cd polybench
-for benchmark in 2DCONV_BLUR 2DCONV_EDGE 2DCONV_EMBOSS 2DCONV_ENHANCE 2DCONV_SHARPEN 2DCONV 2MM 3DCONV 3MM ATAX BICG FDTD-2D GEMM GESUMMV GRAMSCHM MVT SYR2K SYRK
+for benchmark in 2DCONV_BLUR 2DCONV_EMBOSS 2DCONV 2MM 3DCONV 3MM ATAX BICG FDTD-2D GEMM GESUMMV GRAMSCHM MVT SYR2K SYRK
 do
 cd $benchmark
 qsub pbs_$benchmark.pbs
@@ -19,11 +20,12 @@ cd ..
 done
 
 #figures: srad_v1 histo JPEG RAY
+#not changed: SLA TRA
 #7
 cd $mother_dir
 cd $configs_stor1
 cd CUDA
-for benchmark in JPEG_ENCODE JPEG_DECODE RAY BlackScholes CONS SCP SLA TRA
+for benchmark in JPEG_ENCODE BlackScholes CONS SCP
 do
 cd $benchmark
 qsub pbs_$benchmark.pbs

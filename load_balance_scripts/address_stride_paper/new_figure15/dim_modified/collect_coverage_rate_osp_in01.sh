3058044e8c6522ad0e07b0d5229747315e45995d
woodun <hwang07@email.wm.edu>
2017-07-02 06:31:21 -0400

    debug

---
+++ load_balance_scripts/address_stride_paper/new_figure15/dim_modified/collect_coverage_rate_osp_in01.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+#specify your output file
+output=/stor1/hwang07/paper_collection/modified_rate_osp.txt
+mother_dir=/stor1/hwang07/modified_osp
+
+for statistics in 'predictable to miss rate:'
+do
+
+#specify your config path in stor1
+for configs_stor1 in coverage100_dynamic_osp_nopc_size8_gto48
+do
+
+#13
+cd $mother_dir
+cd $configs_stor1
+cd polybench
+for benchmark in 2DCONV_BLUR 2DCONV_EDGE 2DCONV_EMBOSS 2DCONV_ENHANCE 2DCONV_SHARPEN 2DCONV 2MM 3DCONV 3MM ATAX BICG FDTD-2D GEMM GESUMMV GRAMSCHM MVT SYR2K SYRK
+do
+cd $benchmark
+grep -o "$statistics[ ]*[-eE\+0-9\.]*" output_* | tail -1 | sed -e "s/$statistics[ ]*\(-$\)*//g" | xargs printf "0%s " >> $output
+cd ..
+done
+
+#figures: JPEG RAY srad_v1 histo
+#7
+cd $mother_dir
+cd $configs_stor1
+cd CUDA
+for benchmark in JPEG_ENCODE JPEG_DECODE RAY BlackScholes CONS SCP SLA TRA
+do
+cd $benchmark
+grep -o "$statistics[ ]*[-eE\+0-9\.]*" output_* | tail -1 | sed -e "s/$statistics[ ]*\(-$\)*//g" | xargs printf "0%s " >> $output
+cd ..
+done
+
+printf "\r\n" >> $output
+done
+######################################################################################################################################
+
+done
---
+++ load_balance_scripts/address_stride_paper/new_figure15/dim_modified/launch_coverages_in02.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+#this one works with applications_modified
+
+#####################################################
+mother_dir=/stor2/hwang07/dim_modified_tsp
+
+for configs_stor1 in coverage100_dynamic_tsp_nopc_size8_gto48 coverage10_dynamic_tsp_nopc_size8_gto48 wp0_tsp_coverage10_gto48 wp6_tsp_coverage10_gto48
+do
+
+#not changed: 
+#13
+cd $mother_dir
+cd $configs_stor1
+cd polybench
+for benchmark in 2DCONV_BLUR 2DCONV_EMBOSS 2DCONV 2MM 3DCONV 3MM ATAX BICG FDTD-2D GEMM GESUMMV GRAMSCHM MVT SYR2K SYRK
+do
+cd $benchmark
+qsub pbs_$benchmark.pbs
+cd ..
+done
+
+#figures: srad_v1 histo JPEG RAY
+#not changed: SLA TRA
+#7
+cd $mother_dir
+cd $configs_stor1
+cd CUDA
+for benchmark in JPEG_ENCODE BlackScholes CONS SCP
+do
+cd $benchmark
+qsub pbs_$benchmark.pbs
+cd ..
+done
+
+done

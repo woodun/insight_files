72dec152b042369ae4ee66f9d09623b92c039067
woodun <hwang07@email.wm.edu>
2017-07-04 20:36:34 -0400

    debug

--- load_balance_scripts/address_stride_paper/new_figure15/dim_modified/launch_baseline_in02.sh
+++ load_balance_scripts/address_stride_paper/new_figure15/dim_modified/launch_baseline_in02.sh
@@ -8,12 +8,12 @@ for configs_stor1 in tsp_address_exp_gtoswl48_alloff
 do
 
 #13
-#not changed: ATAX BICG GESUMMV GRAMSCHM MVT SYR2K SYRK 2DCONV_EMBOSS 2DCONV_BLUR 2DCONV
-#tsp not good: 
+#not changed: ATAX BICG GESUMMV GRAMSCHM MVT SYR2K SYRK 2DCONV_EMBOSS 2DCONV_BLUR 2DCONV 3DCONV
+#tsp not good: GEMM FDTD-2D
 cd $mother_dir
 cd $configs_stor1
 cd polybench
-for benchmark in 2MM 3DCONV 3MM GEMM FDTD-2D
+for benchmark in 2MM 3MM 
 do
 cd $benchmark
 qsub pbs_$benchmark.pbs
@@ -21,8 +21,8 @@ cd ..
 done
 
 #figures: srad_v1 histo JPEG RAY
-#not changed: SLA TRA CONS
-#tsp not good: BlackScholes JPEG_ENCODE SCP
+#not changed: SLA TRA CONS SCP
+#tsp not good: BlackScholes JPEG_ENCODE
 #7
 cd $mother_dir
 cd $configs_stor1

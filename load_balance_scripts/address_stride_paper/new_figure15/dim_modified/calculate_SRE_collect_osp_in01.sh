deaafff69bcf71f99ac67c6cd45836026dfc498a
woodun <hwang07@email.wm.edu>
2017-07-05 04:39:41 -0400

    debug

--- load_balance_scripts/address_stride_paper/new_figure15/dim_modified/calculate_SRE_collect_osp_in01.sh
+++ load_balance_scripts/address_stride_paper/new_figure15/dim_modified/calculate_SRE_collect_osp_in01.sh
@@ -2,7 +2,7 @@
 
 ########################################input mother dir path
 output=/stor1/hwang07/paper_collection/dim_modified_osp_coverage10_RE.txt
-token='RE:'
+statistics='RE:'
 
 #cd $1
 cd /stor1/hwang07/dim_modified_osp
@@ -18,7 +18,7 @@ cd polybench
 for benchmark in ATAX BICG GESUMMV GRAMSCHM MVT SYR2K SYRK 2DCONV_EMBOSS 2DCONV_BLUR 2DCONV 3DCONV
 do
 cd $benchmark
-grep -o "$token[ ]*[-eE\+0-9\.]*" RE.txt | tail -1 | sed -e "s/$token[ ]*//g" | xargs printf "0%s " >> $output
+grep -o "$statistics[ ]*[-eE\+0-9\.]*" RE.txt | tail -1 | sed -e "s/$statistics[ ]*\(-$\)*//g" | xargs printf "0%s " >> $output
 cd ..
 done
 cd ..
@@ -27,7 +27,7 @@ cd CUDA
 for benchmark in SLA TRA CONS SCP
 do
 cd $benchmark
-grep -o "$token[ ]*[-eE\+0-9\.]*" RE.txt | tail -1 | sed -e "s/$token[ ]*//g" | xargs printf "0%s " >> $output
+grep -o "$statistics[ ]*[-eE\+0-9\.]*" RE.txt | tail -1 | sed -e "s/$statistics[ ]*\(-$\)*//g" | xargs printf "0%s " >> $output
 cd ..
 done
 cd ..

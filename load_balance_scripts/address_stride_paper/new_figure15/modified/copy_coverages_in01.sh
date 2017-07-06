24590e01590dc83954f2a3ed4521fd96642e8cfe
woodun <hwang07@email.wm.edu>
2017-07-05 21:17:37 -0400

    debug

--- load_balance_scripts/address_stride_paper/new_figure15/modified/copy_coverages_in01.sh
+++ load_balance_scripts/address_stride_paper/new_figure15/modified/copy_coverages_in01.sh
@@ -6,7 +6,8 @@ template=/stor1/hwang07/insight_files/insight_template
 #####################################################################################
 mother_dir=/stor1/hwang07/modified_osp
 
-for configs_stor1 in coverage100_dynamic_osp_nopc_size8_gto48 coverage10_dynamic_osp_nopc_size8_gto48
+#coverage100_dynamic_osp_nopc_size8_gto48 coverage10_dynamic_osp_nopc_size8_gto48 
+for configs_stor1 in wp0_osp_coverage10_gto48 wp6_osp_coverage10_gto48
 do
 
 cp -r $template $mother_dir/$configs_stor1
--- load_balance_scripts/address_stride_paper/new_figure15/modified/copy_coverages_in02.sh
+++ load_balance_scripts/address_stride_paper/new_figure15/modified/copy_coverages_in02.sh
@@ -6,7 +6,8 @@ template=/stor1/hwang07/insight_files/insight_template
 #####################################################################################
 mother_dir=/stor2/hwang07/modified_tsp
 
-for configs_stor1 in coverage100_dynamic_tsp_nopc_size8_gto48 coverage10_dynamic_tsp_nopc_size8_gto48
+#coverage100_dynamic_tsp_nopc_size8_gto48 coverage10_dynamic_tsp_nopc_size8_gto48
+for configs_stor1 in wp0_tsp_coverage10_gto48 wp6_tsp_coverage10_gto48
 do
 
 cp -r $template $mother_dir/$configs_stor1

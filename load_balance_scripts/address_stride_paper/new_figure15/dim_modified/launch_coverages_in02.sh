b6d1538641b1e3274daf3f167d0609c3e7f6dc63
woodun <hwang07@email.wm.edu>
2017-07-07 11:50:30 -0400

    debug

--- load_balance_scripts/address_stride_paper/new_figure15/dim_modified/launch_coverages_in02.sh
+++ load_balance_scripts/address_stride_paper/new_figure15/dim_modified/launch_coverages_in02.sh
@@ -8,12 +8,12 @@ for configs_stor1 in coverage100_dynamic_tsp_nopc_size8_gto48 coverage10_dynamic
 do
 
 #13
-#not changed: ATAX BICG GESUMMV GRAMSCHM MVT SYR2K SYRK 2DCONV_EMBOSS 2DCONV_BLUR 2DCONV 3DCONV
+#not changed: BICG GESUMMV GRAMSCHM MVT SYR2K SYRK 2DCONV_EMBOSS 2DCONV_BLUR 2DCONV 3DCONV
 #tsp not good: GEMM FDTD-2D 2MM 3MM
 cd $mother_dir
 cd $configs_stor1
 cd polybench
-for benchmark in 
+for benchmark in ATAX
 do
 cd $benchmark
 qsub pbs_$benchmark.pbs
@@ -21,13 +21,13 @@ cd ..
 done
 
 #figures: srad_v1 histo JPEG RAY
-#not changed: SLA TRA SCP
+#not changed: SLA TRA SCP CONS
 #tsp not good: BlackScholes JPEG_ENCODE
 #7
 cd $mother_dir
 cd $configs_stor1
 cd CUDA
-for benchmark in CONS
+for benchmark in
 do
 cd $benchmark
 qsub pbs_$benchmark.pbs

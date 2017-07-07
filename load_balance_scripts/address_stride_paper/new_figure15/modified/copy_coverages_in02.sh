baa61e0fe8c74483f5f82ec01c2ef8c2852c630d
woodun <hwang07@email.wm.edu>
2017-07-07 05:03:11 -0400

    debug

--- load_balance_scripts/address_stride_paper/new_figure15/modified/copy_coverages_in02.sh
+++ load_balance_scripts/address_stride_paper/new_figure15/modified/copy_coverages_in02.sh
@@ -6,8 +6,8 @@ template=/stor1/hwang07/insight_files/insight_template
 #####################################################################################
 mother_dir=/stor2/hwang07/modified_tsp
 
-#coverage100_dynamic_tsp_nopc_size8_gto48 coverage10_dynamic_tsp_nopc_size8_gto48
-for configs_stor1 in wp0_tsp_coverage10_gto48 wp6_tsp_coverage10_gto48
+#coverage100_dynamic_tsp_nopc_size8_gto48 coverage10_dynamic_tsp_nopc_size8_gto48 wp0_tsp_coverage10_gto48 wp6_tsp_coverage10_gto48
+for configs_stor1 in coverage100_dynamic_tsp_nopc_size8_RR48 coverage20_dynamic_tsp_nopc_size8_RR48 coverage10_dynamic_tsp_nopc_size8_RR48 coverage15_dynamic_tsp_nopc_size8_gto48 wp0_tsp_coverage15_gto48 wp6_tsp_coverage15_gto48 coverage5_dynamic_tsp_nopc_size8_gto48 wp0_tsp_coverage5_gto48 wp6_tsp_coverage5_gto48
 do
 
 cp -r $template $mother_dir/$configs_stor1
--- load_balance_scripts/address_stride_paper/new_figure15/modified/temp_launch.sh
+++ load_balance_scripts/address_stride_paper/new_figure15/modified/temp_launch.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-sh launch_baseline_in02.sh
+#sh launch_baseline_in02.sh
 sh launch_coverages_in01.sh
 sh launch_coverages_in02.sh

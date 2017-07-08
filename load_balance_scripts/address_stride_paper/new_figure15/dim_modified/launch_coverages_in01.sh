b7898811296e8961469eb8a6e7f56402572a71b3
woodun <hwang07@email.wm.edu>
2017-07-07 23:54:03 -0400

    debug

--- load_balance_scripts/address_stride_paper/new_figure15/dim_modified/launch_coverages_in01.sh
+++ load_balance_scripts/address_stride_paper/new_figure15/dim_modified/launch_coverages_in01.sh
@@ -8,12 +8,12 @@ for configs_stor1 in coverage100_dynamic_osp_nopc_size8_gto48 coverage10_dynamic
 do
 
 #13
-#not changed: BICG GESUMMV GRAMSCHM MVT SYR2K SYRK 2DCONV_EMBOSS 2DCONV_BLUR 2DCONV 3DCONV
+#not changed: 2DCONV_EMBOSS 2DCONV_BLUR 2DCONV 3DCONV ATAX
 #tsp not good: GEMM FDTD-2D 2MM 3MM
 cd $mother_dir
 cd $configs_stor1
 cd polybench
-for benchmark in ATAX
+for benchmark in BICG GESUMMV GRAMSCHM MVT SYR2K SYRK
 do
 cd $benchmark
 qsub pbs_$benchmark.pbs

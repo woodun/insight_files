7486b86d7572799b11955de21c2ee52ea5d02cf8
woodun <hwang07@email.wm.edu>
2017-07-09 16:01:11 -0400

    debug

--- load_balance_scripts/address_stride_paper/new_figure15/modified/launch_coverages_in01.sh
+++ load_balance_scripts/address_stride_paper/new_figure15/modified/launch_coverages_in01.sh
@@ -4,18 +4,18 @@
 #####################################################
 mother_dir=/stor1/hwang07/modified_osp
 
-#
+#coverage100_dynamic_osp_nopc_size8_gto48 coverage10_dynamic_osp_nopc_size8_gto48 wp0_osp_coverage10_gto48 wp6_osp_coverage10_gto48
 #coverage100_dynamic_osp_nopc_size8_RR48 coverage20_dynamic_osp_nopc_size8_RR48 coverage10_dynamic_osp_nopc_size8_RR48 coverage15_dynamic_osp_nopc_size8_gto48 wp0_osp_coverage15_gto48 wp6_osp_coverage15_gto48 coverage5_dynamic_osp_nopc_size8_gto48 wp0_osp_coverage5_gto48 wp6_osp_coverage5_gto48
-for configs_stor1 in coverage100_dynamic_osp_nopc_size8_gto48 coverage10_dynamic_osp_nopc_size8_gto48 wp0_osp_coverage10_gto48 wp6_osp_coverage10_gto48
+for configs_stor1 in coverage20_dynamic_osp_nopc_size8_gto48 wp0_osp_coverage20_gto48 wp6_osp_coverage20_gto48
 do
 
 #13
-#not changed: ATAX BICG GESUMMV MVT SYRK 2DCONV_EMBOSS 2DCONV_BLUR 2DCONV 3DCONV
+#not changed: 
 #tsp not good: GEMM FDTD-2D 2MM 3MM GRAMSCHM
 cd $mother_dir
 cd $configs_stor1
 cd polybench
-for benchmark in SYR2K
+for benchmark in ATAX BICG GESUMMV MVT SYR2K SYRK 2DCONV_EMBOSS 2DCONV_BLUR 2DCONV 3DCONV
 do
 cd $benchmark
 qsub pbs_$benchmark.pbs
@@ -23,13 +23,13 @@ cd ..
 done
 
 #figures: srad_v1 histo JPEG RAY
-#not changed: CONS TRA SCP SLA
+#not changed: 
 #tsp not good: BlackScholes JPEG_ENCODE
 #7
 cd $mother_dir
 cd $configs_stor1
 cd CUDA
-for benchmark in 
+for benchmark in CONS TRA SCP SLA
 do
 cd $benchmark
 qsub pbs_$benchmark.pbs

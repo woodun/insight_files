1b886ad1744ff32de5d79bcd014543f8d28c4186
woodun <hwang07@email.wm.edu>
2017-07-10 18:17:52 -0400

    debug

--- load_balance_scripts/address_stride_paper/new_figure15/modified/collect_coverage_IPC_osp_in01.sh
+++ load_balance_scripts/address_stride_paper/new_figure15/modified/collect_coverage_IPC_osp_in01.sh
@@ -8,14 +8,14 @@ for statistics in 'gpu_tot_ipc ='
 do
 
 #specify your config path in stor1
-for configs_stor1 in coverage100_dynamic_osp_nopc_size8_gto48
+for configs_stor1 in coverage5_dynamic_osp_nopc_size8_gto48 wp0_osp_coverage5_gto48 wp6_osp_coverage5_gto48 coverage10_dynamic_osp_nopc_size8_gto48 wp0_osp_coverage10_gto48 wp6_osp_coverage10_gto48 coverage15_dynamic_osp_nopc_size8_gto48 wp0_osp_coverage15_gto48 wp6_osp_coverage15_gto48 coverage20_dynamic_osp_nopc_size8_gto48 wp0_osp_coverage20_gto48 wp6_osp_coverage20_gto48 coverage10_dynamic_osp_nopc_size8_RR48 wp0_osp_coverage10_RR48 wp6_osp_coverage10_RR48 coverage20_dynamic_osp_nopc_size8_RR48 wp0_osp_coverage20_RR48 wp6_osp_coverage20_RR48
 do
 
 #13
 cd $mother_dir
 cd $configs_stor1
 cd polybench
-for benchmark in 2DCONV_BLUR 2DCONV_EDGE 2DCONV_EMBOSS 2DCONV_ENHANCE 2DCONV_SHARPEN 2DCONV 2MM 3DCONV 3MM ATAX BICG FDTD-2D GEMM GESUMMV GRAMSCHM MVT SYR2K SYRK
+for benchmark in ATAX BICG GESUMMV MVT SYR2K SYRK 2DCONV_EMBOSS 2DCONV_BLUR 2DCONV 3DCONV
 do
 cd $benchmark
 grep -o "$statistics[ ]*[-eE\+0-9\.]*" output_* | tail -1 | sed -e "s/$statistics[ ]*\(-$\)*//g" | xargs printf "0%s " >> $output
@@ -27,7 +27,7 @@ done
 cd $mother_dir
 cd $configs_stor1
 cd CUDA
-for benchmark in JPEG_ENCODE JPEG_DECODE RAY BlackScholes CONS SCP SLA TRA
+for benchmark in SLA TRA CONS SCP
 do
 cd $benchmark
 grep -o "$statistics[ ]*[-eE\+0-9\.]*" output_* | tail -1 | sed -e "s/$statistics[ ]*\(-$\)*//g" | xargs printf "0%s " >> $output

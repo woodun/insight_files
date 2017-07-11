d41005ec6ca44929088f813db432bf3dba2cdc88
woodun <hwang07@email.wm.edu>
2017-07-11 15:10:02 -0400

    debug

--- load_balance_scripts/address_stride_paper/new_figure15/modified/temp_collect.sh
+++ load_balance_scripts/address_stride_paper/new_figure15/modified/temp_collect.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 
-sh collect_coverage_coverages_osp_in01.sh
-sh collect_coverage_coverages_tsp_in02.sh
+#sh collect_coverage_coverages_osp_in01.sh
+#sh collect_coverage_coverages_tsp_in02.sh
+#sh collect_coverage_rate_osp_in01.sh
+#sh collect_coverage_rate_tsp_in02.sh
+#sh collect_coverage_l1mr_osp_in01.sh
+#sh collect_coverage_l1mr_tsp_in02.sh
+
+sh collect_nolong_rate_in02.sh
 sh collect_coverage_rate_osp_in01.sh
-sh collect_coverage_rate_tsp_in02.sh
-sh collect_coverage_l1mr_osp_in01.sh
-sh collect_coverage_l1mr_tsp_in02.sh
+sh collect_sizes_in01.sh
+sh collect_fig6_in02.sh
+sh collect_baseline_IPC_in02.sh
+sh collect_coverage_IPC_osp_in01.sh
+sh collect_coverage_IPC_tsp_in02.sh

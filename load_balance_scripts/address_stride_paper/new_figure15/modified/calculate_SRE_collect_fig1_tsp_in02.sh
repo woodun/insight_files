a087b3a503daa59151edd7e495d19ad503c30dce
woodun <hwang07@email.wm.edu>
2017-07-13 13:54:04 -0400

    debug

---
+++ load_balance_scripts/address_stride_paper/new_figure15/modified/calculate_SRE_collect_fig1_tsp_in02.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+########################################input mother dir path
+output=/stor1/hwang07/paper_collection/modified_tsp_fig1_RE.txt
+statistics='RE:'
+
+#cd $1
+cd /stor2/hwang07/modified_tsp
+
+#for i in $(ls -d *)
+for i in coverage5_dynamic_tsp_nopc_size8_gto48 coverage10_dynamic_tsp_nopc_size8_gto48 coverage15_dynamic_tsp_nopc_size8_gto48 coverage20_dynamic_tsp_nopc_size8_gto48
+do
+
+cd $i
+
+
+cd polybench
+for benchmark in GESUMMV SYR2K SYRK 2DCONV_EMBOSS 2DCONV_BLUR 3DCONV
+do
+cd $benchmark
+grep -o "$statistics[ ]*[-eE\+0-9\.]*" RE.txt | tail -1 | sed -e "s/$statistics[ ]*\(-$\)*//g" | xargs printf "0%s " >> $output
+cd ..
+done
+cd ..
+
+cd CUDA
+for benchmark in SLA TRA CONS SCP
+do
+cd $benchmark
+grep -o "$statistics[ ]*[-eE\+0-9\.]*" RE.txt | tail -1 | sed -e "s/$statistics[ ]*\(-$\)*//g" | xargs printf "0%s " >> $output
+cd ..
+done
+cd ..
+
+
+cd ..
+
+printf "\r\n" >> $output
+
+done

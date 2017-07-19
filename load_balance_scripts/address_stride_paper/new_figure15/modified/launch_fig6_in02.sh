a8d9f06682e3e6bc202fe5a6e0e99b2e7edb3334
woodun <hwang07@email.wm.edu>
2017-07-19 06:04:11 -0400

    debug

--- load_balance_scripts/address_stride_paper/new_figure15/modified/launch_fig6_in02.sh
+++ load_balance_scripts/address_stride_paper/new_figure15/modified/launch_fig6_in02.sh
@@ -27,7 +27,7 @@ done
 cd $mother_dir
 cd $configs_stor1
 cd CUDA
-for benchmark in CONS TRA SLA
+for benchmark in SLA
 do
 cd $benchmark
 qsub pbs_$benchmark.pbs
--- load_balance_scripts/address_stride_paper/new_figure15/modified/temp_launch.sh
+++ load_balance_scripts/address_stride_paper/new_figure15/modified/temp_launch.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-#sh launch_baseline_in02.sh
-#sh launch_coverages_in01.sh
+sh launch_baseline_in02.sh
+sh launch_coverages_in01.sh
 sh launch_coverages_in02.sh
 sh launch_fig6_in02.sh
 sh launch_sizes_in01.sh

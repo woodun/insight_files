8f6d4b7c73976146c51676d1bbd341bef92fc940
woodun <hwang07@email.wm.edu>
2017-07-20 12:29:59 -0400

    debug

---
+++ load_balance_scripts/address_stride_paper/new_figure15/modified/collect_baseline_others_in02.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+#specify your output file
+output=/stor1/hwang07/paper_collection/modified_baseline_others.txt
+mother_dir=/stor2/hwang07/modified_baseline
+
+for statistics in 'gpu_tot_ipc =' 'bw_util=' 'Stall:' 'predictable lines:' 'average_avg_distance:' 'average_avg_bias:' 'to access rate:'
+do
+
+printf "%s\r\n" $statistics >> $output
+
+#specify your config path in stor1
+for configs_stor1 in tsp_address_exp_gtoswl48_alloff
+do
+
+#13
+cd $mother_dir
+cd $configs_stor1
+cd polybench
+for benchmark in GESUMMV SYR2K SYRK 2DCONV_EMBOSS 2DCONV_BLUR 3DCONV
+do
+cd $benchmark
+grep -o "$statistics[ ]*[-eE\+0-9\.]*" output_* | tail -1 | sed -e "s/$statistics[ ]*\(-$\)*//g" | xargs printf "0%s " >> $output
+cd ..
+done
+
+#figures: JPEG RAY srad_v1 histo
+#7
+cd $mother_dir
+cd $configs_stor1
+cd CUDA
+for benchmark in SLA TRA CONS SCP
+do
+cd $benchmark
+grep -o "$statistics[ ]*[-eE\+0-9\.]*" output_* | tail -1 | sed -e "s/$statistics[ ]*\(-$\)*//g" | xargs printf "0%s " >> $output
+cd ..
+done
+
+printf "\r\n" >> $output
+done
+######################################################################################################################################
+
+done

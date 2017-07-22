7a74938ff9d82bfbc72c2dd192e80d7ac929b75e
woodun <hwang07@email.wm.edu>
2017-07-22 09:14:25 -0400

    debug

--- hima_scripts/amc_paper/profile_sizes/copy_remove_rs_gto.sh
+++ hima_scripts/amc_paper/profile_sizes/copy_remove_rs_gto.sh
@@ -5,7 +5,8 @@ template=/sciclone/data10/hwang07/GPU_RESEARCH/insight_files/hima_template
 #####################################################################################
 mother_dir=/sciclone/data10/hwang07/GPU_RESEARCH/amc/remove_rs
 
-for configs_stor1 in delay0_remove10_e6_r2_size512_gto48 delay0_remove10_e6_r3_size512_gto48 delay0_remove10_e6_r4_size512_gto48
+#delay0_remove10_e6_r2_size512_gto48 delay0_remove10_e6_r3_size512_gto48 delay0_remove10_e6_r4_size512_gto48
+for configs_stor1 in delay0_remove10_e6_r5_size512_gto48 delay0_remove10_e6_r6_size512_gto48
 do
 
 cp -r $template $mother_dir/$configs_stor1

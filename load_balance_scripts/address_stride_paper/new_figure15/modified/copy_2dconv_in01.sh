2f92b948f425161f6258a6fedfdaeb89bf5f4946
woodun <hwang07@email.wm.edu>
2017-07-23 01:59:44 -0400

    debug

---
+++ load_balance_scripts/address_stride_paper/new_figure15/modified/copy_2dconv_in01.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+#this one use applications_FEB
+
+dest_emboss=/stor1/hwang07/emboss
+dest_blur=/stor1/hwang07/blur
+
+#####################################################################################
+mother_dir1=/stor1/hwang07/modified_osp
+mother_dir2=/stor2/hwang07/modified_tsp
+
+
+for configs_stor1 in coverage5_dynamic_osp_nopc_size8_gto48 wp0_osp_coverage5_gto48 wp6_osp_coverage5_gto48 coverage10_dynamic_osp_nopc_size8_gto48 wp0_osp_coverage10_gto48 wp6_osp_coverage10_gto48 coverage15_dynamic_osp_nopc_size8_gto48 wp0_osp_coverage15_gto48 wp6_osp_coverage15_gto48 coverage20_dynamic_osp_nopc_size8_gto48 wp0_osp_coverage20_gto48 wp6_osp_coverage20_gto48
+do
+
+#######
+cd $mother_dir1
+cd $configs_stor1
+cd polybench
+cd 2DCONV_BLUR
+cp BLUR_GPU.bmp dest_blur/blur_$configs_stor1
+cd ..
+cd 2DCONV_EMBOSS
+cp BLUR_GPU.bmp dest_emboss/emboss_$configs_stor1
+
+done
+
+
+for configs_stor2 in coverage5_dynamic_tsp_nopc_size8_gto48 wp0_tsp_coverage5_gto48 wp6_tsp_coverage5_gto48 coverage10_dynamic_tsp_nopc_size8_gto48 wp0_tsp_coverage10_gto48 wp6_tsp_coverage10_gto48 coverage15_dynamic_tsp_nopc_size8_gto48 wp0_tsp_coverage15_gto48 wp6_tsp_coverage15_gto48 coverage20_dynamic_tsp_nopc_size8_gto48 wp0_tsp_coverage20_gto48 wp6_tsp_coverage20_gto48
+do
+
+#######
+cd $mother_dir2
+cd $configs_stor2
+cd polybench
+cd 2DCONV_BLUR
+cp BLUR_GPU.bmp dest_blur/blur_$configs_stor2
+cd ..
+cd 2DCONV_EMBOSS
+cp BLUR_GPU.bmp dest_emboss/emboss_$configs_stor2
+
+done
+
+
+

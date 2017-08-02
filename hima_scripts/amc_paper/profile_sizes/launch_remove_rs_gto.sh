f086f8430ba1f8d7a81122e4820dd7fa4cdf9829
woodun <hwang07@email.wm.edu>
2017-08-02 16:27:44 -0400

    debug

--- hima_scripts/amc_paper/profile_sizes/launch_remove_rs_gto.sh
+++ hima_scripts/amc_paper/profile_sizes/launch_remove_rs_gto.sh
@@ -3,9 +3,10 @@
 #####################################################
 mother_dir=/sciclone/data10/hwang07/GPU_RESEARCH/amc/remove_rs
 
+
 #specify your config path in stor1
 #
-for configs_stor1 in delay0_remove10_e6_r2_size512_gto48 delay0_remove10_e6_r3_size512_gto48 delay0_remove10_e6_r4_size512_gto48 delay0_remove10_e6_r5_size512_gto48 delay0_remove10_e6_r6_size512_gto48
+for configs_stor1 in delay0_remove10_e6_r7_size512_gto48_p8192_w8192 delay0_remove10_e6_r8_size512_gto48_p8192_w8192 delay0_remove10_e6_r9_size512_gto48_p8192_w8192 delay0_remove10_e6_r10_size512_gto48_p8192_w8192 delay0_remove10_e6_r11_size512_gto48_p8192_w8192 delay0_remove10_e6_r12_size512_gto48_p8192_w8192 delay0_remove10_e6_r7_size512_gto48_p16384_w16384 delay0_remove10_e6_r8_size512_gto48_p16384_w16384 delay0_remove10_e6_r9_size512_gto48_p16384_w16384 delay0_remove10_e6_r10_size512_gto48_p16384_w16384 delay0_remove10_e6_r11_size512_gto48_p16384_w16384 delay0_remove10_e6_r12_size512_gto48_p16384_w16384
 do
 
 #13

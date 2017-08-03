b5932d842be9e75b90062338dc0c5b93ab996661
woodun <hwang07@email.wm.edu>
2017-08-03 18:03:07 -0400

    debug

--- hima_scripts/amc_paper/profile_sizes/detect_profile_sizes.sh
+++ hima_scripts/amc_paper/profile_sizes/detect_profile_sizes.sh
@@ -1,21 +1,26 @@
 #!/bin/sh
 
-#####################################################
+###################################################################################
+output=/sciclone/data10/hwang07/GPU_RESEARCH/relaunch.txt
 mother_dir=/sciclone/data10/hwang07/GPU_RESEARCH/amc/profile_sizes
-
 
 #specify your config path in stor1
 for configs_stor1 in delay0_remove0_e0_r0_size16_gto48_p8192_w8192 delay0_remove0_e0_r0_size32_gto48_p8192_w8192 delay0_remove0_e0_r0_size64_gto48_p8192_w8192 delay0_remove0_e0_r0_size128_gto48_p8192_w8192 delay0_remove0_e0_r0_size256_gto48_p8192_w8192 delay0_remove0_e0_r0_size512_gto48_p8192_w8192 delay0_remove0_e0_r0_size1024_gto48_p8192_w8192 delay0_remove0_e0_r0_sizeinf_gto48_p8192_w8192
 do
 
-#15 3DCONV FDTD-2D GRAMSCHM
+#13
 cd $mother_dir
 cd $configs_stor1
 cd polybench
 for benchmark in GESUMMV MVT 2MM 3MM SYRK ATAX BICG 2DCONV_EMBOSS 2DCONV_BLUR 2DCONV 3DCONV GEMM FDTD-2D GRAMSCHM SYR2K
 do
 cd $benchmark
-qsub pbs_$benchmark.pbs
+if [[ -s $benchmark\_CPU.txt ]]; then
+echo 'ok'
+else
+pwd
+pwd | xargs printf "%s " >> $output
+fi
 cd ..
 done
 
@@ -27,7 +32,12 @@ cd CUDA
 for benchmark in TRA SCP CONS FWT LPS BlackScholes SLA
 do
 cd $benchmark
-qsub pbs_$benchmark.pbs
+if [[ -s $benchmark\_CPU.txt ]]; then
+echo 'ok'
+else
+pwd
+pwd | xargs printf "%s " >> $output
+fi
 cd ..
 done
 

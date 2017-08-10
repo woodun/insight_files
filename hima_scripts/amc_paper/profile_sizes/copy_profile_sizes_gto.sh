a0d596f49d5f17236be9afdbf7e0ffebb242c2dd
woodun <hwang07@email.wm.edu>
2017-08-10 06:43:00 -0400

    debug

--- hima_scripts/amc_paper/profile_sizes/copy_profile_sizes_gto.sh
+++ hima_scripts/amc_paper/profile_sizes/copy_profile_sizes_gto.sh
@@ -5,7 +5,9 @@ template=/sciclone/data10/hwang07/GPU_RESEARCH/insight_files/hima_template
 #####################################################################################
 mother_dir=/sciclone/data10/hwang07/GPU_RESEARCH/amc/profile_sizes
 
-for configs_stor1 in delay0_remove0_e0_r0_size16_gto48_p8192_w8192 delay0_remove0_e0_r0_size32_gto48_p8192_w8192 delay0_remove0_e0_r0_size64_gto48_p8192_w8192 delay0_remove0_e0_r0_size128_gto48_p8192_w8192 delay0_remove0_e0_r0_size256_gto48_p8192_w8192 delay0_remove0_e0_r0_size512_gto48_p8192_w8192 delay0_remove0_e0_r0_size1024_gto48_p8192_w8192 delay0_remove0_e0_r0_sizeinf_gto48_p8192_w8192
+#delay0_remove0_e0_r0_size16_gto48_p8192_w8192 delay0_remove0_e0_r0_size32_gto48_p8192_w8192 delay0_remove0_e0_r0_size64_gto48_p8192_w8192 delay0_remove0_e0_r0_size128_gto48_p8192_w8192 delay0_remove0_e0_r0_size256_gto48_p8192_w8192 delay0_remove0_e0_r0_size512_gto48_p8192_w8192 delay0_remove0_e0_r0_size1024_gto48_p8192_w8192 delay0_remove0_e0_r0_sizeinf_gto48_p8192_w8192
+
+for configs_stor1 in delay0_remove0_e0_r0_size512_gto48_p8192_w8192 delay0_remove0_e0_r0_size512_gto48_p16384_w16384 delay0_remove0_e0_r0_size512_gto48_p32768_w32768
 do
 
 cp -r $template $mother_dir/$configs_stor1

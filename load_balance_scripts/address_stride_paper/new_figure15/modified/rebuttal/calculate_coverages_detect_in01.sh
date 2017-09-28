311351a0ba81aa20fab3ab1dfd77a12516c03f2e
woodun <hwang07@email.wm.edu>
2017-09-28 09:38:35 -0400

    debug

---
+++ load_balance_scripts/address_stride_paper/new_figure15/modified/rebuttal/calculate_coverages_detect_in01.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+
+########################################
+output=/stor2/hwang07/Rebuttal_re.txt
+
+#cd $1
+cd /stor1/hwang07/rebuttal_tsp/
+
+#for i in $(ls -d *)
+for i in coverage5_dynamic_tsp_nopc_size8_gto48_wattch1 wp0_tsp_coverage5_gto48_wattch1 wp6_tsp_coverage5_gto48_wattch1 coverage10_dynamic_tsp_nopc_size8_gto48_wattch1 wp0_tsp_coverage10_gto48_wattch1 wp6_tsp_coverage10_gto48_wattch1 coverage15_dynamic_tsp_nopc_size8_gto48_wattch1 wp0_tsp_coverage15_gto48_wattch1 wp6_tsp_coverage15_gto48_wattch1 coverage20_dynamic_tsp_nopc_size8_gto48_wattch1 wp0_tsp_coverage20_gto48_wattch1 wp6_tsp_coverage20_gto48_wattch1
+do
+
+cd $i
+
+
+cd polybench
+for benchmark in GESUMMV SYR2K SYRK 2DCONV_EMBOSS 2DCONV_BLUR 3DCONV
+do
+cd $benchmark
+if [[ -s RMSE.txt ]]; then
+pwd | xargs printf "%s "
+echo 'ok'
+else
+pwd
+pwd | xargs printf "%s " >> $output
+fi
+cd ..
+done
+cd ..
+
+#TRA CONS SCP
+cd CUDA
+for benchmark in SLA
+do
+cd $benchmark
+if [[ -s RMSE.txt ]]; then
+pwd | xargs printf "%s "
+echo 'ok'
+else
+pwd
+pwd | xargs printf "%s " >> $output
+fi
+cd ..
+done
+cd ..
+
+
+cd ..
+
+done
+
+

92fdc3ffe6894e48bfce2d9a394dd48502923a35
woodun <hwang07@email.wm.edu>
2017-09-28 15:59:29 -0400

    debug

---
+++ load_balance_scripts/address_stride_paper/new_figure15/modified/rebuttal/collect_baseline_cycles_in01.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+#specify your output file
+output=/stor1/hwang07/paper_collection/rebuttal_baseline_cycles_tsp.txt
+mother_dir=/stor1/hwang07/rebuttal_baseline
+
+for statistics in 'gpu_tot_sim_cycle = '
+do
+
+printf "%s\r\n" $statistics >> $output
+
+#specify your config path in stor1
+for configs_stor1 in tsp_address_exp_gtoswl48_alloff_wattch1
+do
+
+#13
+cd $mother_dir
+cd $configs_stor1
+cd polybench
+for benchmark in GESUMMV SYR2K SYRK 2DCONV_EMBOSS 2DCONV_BLUR 3DCONV
+do
+cd $benchmark
+grep -o "$statistics[ ]*[-eE\+0-9\.]*" output_* | tail -1 | sed -e "s/$statistics[ ]*\(-$\)*//g" | xargs printf "0%s " >> $output
+cd ..
+done
+
+#figures: JPEG RAY srad_v1 histo
+#7
+cd $mother_dir
+cd $configs_stor1
+cd CUDA
+for benchmark in SLA TRA CONS SCP
+do
+cd $benchmark
+grep -o "$statistics[ ]*[-eE\+0-9\.]*" output_* | tail -1 | sed -e "s/$statistics[ ]*\(-$\)*//g" | xargs printf "0%s " >> $output
+cd ..
+done
+
+printf "\r\n" >> $output
+done
+######################################################################################################################################
+
+done

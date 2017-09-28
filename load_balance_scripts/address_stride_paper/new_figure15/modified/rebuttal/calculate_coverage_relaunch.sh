b84c540e6044e49fa87c5e4ef4cb6361fd0f03fa
woodun <hwang07@email.wm.edu>
2017-09-28 09:51:04 -0400

    debug

---
+++ load_balance_scripts/address_stride_paper/new_figure15/modified/rebuttal/calculate_coverage_relaunch.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+########################################
+
+for i in /stor2/hwang07/modified_tsp/coverage5_dynamic_tsp_nopc_size8_gto48/polybench/3DCONV /stor2/hwang07/modified_tsp/coverage5_dynamic_tsp_nopc_size8_gto48/CUDA/SLA /stor2/hwang07/modified_tsp/wp0_tsp_coverage5_gto48/CUDA/SLA /stor2/hwang07/modified_tsp/wp6_tsp_coverage5_gto48/polybench/3DCONV /stor2/hwang07/modified_tsp/wp6_tsp_coverage5_gto48/CUDA/SLA /stor2/hwang07/modified_tsp/coverage10_dynamic_tsp_nopc_size8_gto48/polybench/3DCONV /stor2/hwang07/modified_tsp/coverage10_dynamic_tsp_nopc_size8_gto48/CUDA/SLA /stor2/hwang07/modified_tsp/wp0_tsp_coverage10_gto48/CUDA/SLA /stor2/hwang07/modified_tsp/wp6_tsp_coverage10_gto48/polybench/3DCONV /stor2/hwang07/modified_tsp/wp6_tsp_coverage10_gto48/CUDA/SLA /stor2/hwang07/modified_tsp/coverage15_dynamic_tsp_nopc_size8_gto48/polybench/3DCONV /stor2/hwang07/modified_tsp/coverage15_dynamic_tsp_nopc_size8_gto48/CUDA/SLA /stor2/hwang07/modified_tsp/wp0_tsp_coverage15_gto48/CUDA/SLA /stor2/hwang07/modified_tsp/wp6_tsp_coverage15_gto48/CUDA/SLA /stor2/hwang07/modified_tsp/coverage20_dynamic_tsp_nopc_size8_gto48/polybench/3DCONV /stor2/hwang07/modified_tsp/coverage20_dynamic_tsp_nopc_size8_gto48/CUDA/SLA /stor2/hwang07/modified_tsp/wp0_tsp_coverage20_gto48/CUDA/SLA /stor2/hwang07/modified_tsp/wp6_tsp_coverage20_gto48/polybench/3DCONV /stor2/hwang07/modified_tsp/wp6_tsp_coverage20_gto48/CUDA/SLA /stor2/hwang07/modified_tsp/coverage10_dynamic_tsp_nopc_size8_RR48/polybench/3DCONV /stor2/hwang07/modified_tsp/coverage10_dynamic_tsp_nopc_size8_RR48/CUDA/SLA
+do
+
+cd $i
+
+
+qsub appbase_in01.pbs
+
+
+cd ..
+
+done
+
+
+
+
+for i in /stor2/hwang07/modified_tsp/wp0_tsp_coverage10_RR48/CUDA/SLA /stor2/hwang07/modified_tsp/wp6_tsp_coverage10_RR48/polybench/3DCONV /stor2/hwang07/modified_tsp/wp6_tsp_coverage10_RR48/CUDA/SLA /stor2/hwang07/modified_tsp/coverage20_dynamic_tsp_nopc_size8_RR48/polybench/3DCONV /stor2/hwang07/modified_tsp/coverage20_dynamic_tsp_nopc_size8_RR48/CUDA/SLA /stor2/hwang07/modified_tsp/wp0_tsp_coverage20_RR48/CUDA/SLA /stor2/hwang07/modified_tsp/wp6_tsp_coverage20_RR48/CUDA/SLA /stor1/hwang07/modified_osp/coverage5_dynamic_osp_nopc_size8_gto48/CUDA/SLA /stor1/hwang07/modified_osp/coverage10_dynamic_osp_nopc_size8_gto48/polybench/3DCONV /stor1/hwang07/modified_osp/coverage10_dynamic_osp_nopc_size8_gto48/CUDA/SLA /stor1/hwang07/modified_osp/wp0_osp_coverage10_gto48/CUDA/SLA /stor1/hwang07/modified_osp/wp6_osp_coverage10_gto48/CUDA/SLA /stor1/hwang07/modified_osp/coverage15_dynamic_osp_nopc_size8_gto48/CUDA/SLA /stor1/hwang07/modified_osp/coverage20_dynamic_osp_nopc_size8_gto48/polybench/3DCONV /stor1/hwang07/modified_osp/coverage20_dynamic_osp_nopc_size8_gto48/CUDA/SLA /stor1/hwang07/modified_osp/wp0_osp_coverage20_gto48/CUDA/SLA /stor1/hwang07/modified_osp/coverage10_dynamic_osp_nopc_size8_RR48/CUDA/SLA /stor1/hwang07/modified_osp/wp6_osp_coverage10_RR48/CUDA/SLA /stor1/hwang07/modified_osp/coverage20_dynamic_osp_nopc_size8_RR48/CUDA/SLA /stor1/hwang07/modified_osp/wp0_osp_coverage20_RR48/CUDA/SLA
+do
+
+cd $i
+
+
+qsub appbase_in02.pbs
+
+
+cd ..
+
+done
---
+++ load_balance_scripts/address_stride_paper/new_figure15/modified/rebuttal/calculate_coverages_appbase_distribute_in01.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+########################################
+source_dir=/stor1/hwang07/insight_files/load_balance_scripts/address_stride_paper/new_figure15/modified/rebuttal
+
+#cd $1
+cd /stor1/hwang07/rebuttal_tsp/
+
+for i in $(ls -d *)
+#for i in 
+do
+
+cd $i
+
+
+cd polybench
+for benchmark in GESUMMV SYR2K SYRK 2DCONV_EMBOSS 2DCONV_BLUR 3DCONV
+do
+cd $benchmark
+
+rm appbase_in01.pbs
+rm appbase_in02.pbs
+cat $source_dir/script_base_in01.pbs | sed -e "s/JPEG/$benchmark/g" > appbase_in01.pbs
+cat $source_dir/script_base_in02.pbs | sed -e "s/JPEG/$benchmark/g" > appbase_in02.pbs
+chmod +x appbase_in01.pbs
+chmod +x appbase_in02.pbs
+
+cd ..
+done
+cd ..
+
+###########
+
+cd CUDA
+for benchmark in SLA TRA CONS SCP
+do
+cd $benchmark
+
+rm appbase_in01.pbs
+rm appbase_in02.pbs
+cat $source_dir/script_base_in01.pbs | sed -e "s/JPEG/$benchmark/g" > appbase_in01.pbs
+cat $source_dir/script_base_in02.pbs | sed -e "s/JPEG/$benchmark/g" > appbase_in02.pbs
+chmod +x appbase_in01.pbs
+chmod +x appbase_in02.pbs
+
+cd ..
+done
+cd ..
+
+
+cd ..
+
+done

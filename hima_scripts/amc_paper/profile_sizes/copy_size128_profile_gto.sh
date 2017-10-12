8a6c4ec491cc48d4b3cb0f8352be58aea86a8606
woodun <hwang07@email.wm.edu>
2017-10-12 06:58:10 -0400

    debug

--- hima_scripts/amc_paper/profile_sizes/copy_size128_profile_gto.sh
+++ hima_scripts/amc_paper/profile_sizes/copy_size128_profile_gto.sh
@@ -3,16 +3,9 @@
 template=/sciclone/data10/hwang07/GPU_RESEARCH/insight_files/hima_template
 
 #####################################################################################
-mother_dir=/sciclone/data10/hwang07/GPU_RESEARCH/amc/profile_sizes
+mother_dir=/sciclone/data10/hwang07/GPU_RESEARCH/amc/size128_profile
 
-#delay0_remove0_e0_r0_size16_gto48_p8192_w8192 delay0_remove0_e0_r0_size32_gto48_p8192_w8192 delay0_remove0_e0_r0_size64_gto48_p8192_w8192 delay0_remove0_e0_r0_size128_gto48_p8192_w8192 delay0_remove0_e0_r0_size256_gto48_p8192_w8192 delay0_remove0_e0_r0_size512_gto48_p8192_w8192 delay0_remove0_e0_r0_size1024_gto48_p8192_w8192 delay0_remove0_e0_r0_sizeinf_gto48_p8192_w8192
-
-#delay0_remove0_e0_r0_size512_gto48_p8192_w8192
-
-#delay0_remove0_e0_r0_size512_gto48_p16384_w16384 delay0_remove0_e0_r0_size512_gto48_p32768_w32768
-#delay0_remove0_e0_r0_size512_gto48_p4096_w4096
-
-for configs_stor1 in 
+for configs_stor1 in delay0_remove0_e8_r0_size128_gto48_pw8192_ww32768_c64_bw95_aw1000_rw5000 delay0_remove0_e8_r0_size128_gto48_pw16384_ww32768_c64_bw95_aw2000_rw10000 delay0_remove0_e8_r0_size128_gto48_pw32768_ww32768_c64_bw95_aw4000_rw20000 delay0_remove0_e8_r0_size128_gto48_pw65536_ww32768_c64_bw95_aw8000_rw40000 delay0_remove0_e8_r0_size128_gto48_pw131072_ww32768_c64_bw95_aw16000_rw80000 delay0_remove0_e8_r0_size128_gto48_pw262144_ww32768_c64_bw95_aw32000_rw160000 delay0_remove0_e8_r0_size128_gto48_pw524288_ww32768_c64_bw95_aw64000_rw320000 delay0_remove0_e8_r0_size128_gto48_pw1048576_ww32768_c64_bw95_aw128000_rw640000
 do
 
 cp -r $template $mother_dir/$configs_stor1
--- hima_scripts/amc_paper/profile_sizes/launch_size128_profile_gto.sh
+++ hima_scripts/amc_paper/profile_sizes/launch_size128_profile_gto.sh
@@ -1,15 +1,9 @@
 #!/bin/sh
 
 #####################################################
-mother_dir=/sciclone/data10/hwang07/GPU_RESEARCH/amc/profile_sizes
+mother_dir=/sciclone/data10/hwang07/GPU_RESEARCH/amc/size128_profile
 
-#delay0_remove0_e0_r0_size512_gto48_p8192_w8192 delay0_remove0_e0_r0_size512_gto48_p16384_w16384 delay0_remove0_e0_r0_size512_gto48_p32768_w32768
-
-#delay0_remove0_e0_r0_size512_gto48_p4096_w4096
-#delay0_remove0_e0_r0_size16_gto48_p8192_w8192 delay0_remove0_e0_r0_size32_gto48_p8192_w8192 delay0_remove0_e0_r0_size64_gto48_p8192_w8192 delay0_remove0_e0_r0_size128_gto48_p8192_w8192 delay0_remove0_e0_r0_size256_gto48_p8192_w8192 delay0_remove0_e0_r0_size512_gto48_p8192_w8192 delay0_remove0_e0_r0_size1024_gto48_p8192_w8192 delay0_remove0_e0_r0_sizeinf_gto48_p8192_w8192 
-
-for configs_stor1 in delay0_remove0_e0_r0_size128_gto48_p8192_w8192 delay0_remove0_e0_r0_size128_gto48_p4096_w4096 delay0_remove0_e0_r0_size128_gto48_p16384_w16384 delay0_remove0_e0_r0_size128_gto48_p32768_w32768
-#p&w lengths: delay0_remove0_e0_r0_size512_gto48_p16384_w16384 delay0_remove0_e0_r0_size512_gto48_p32768_w32768 delay0_remove0_e0_r0_size512_gto48_p4096_w4096
+for configs_stor1 in delay0_remove0_e8_r0_size128_gto48_pw8192_ww32768_c64_bw95_aw1000_rw5000 delay0_remove0_e8_r0_size128_gto48_pw16384_ww32768_c64_bw95_aw2000_rw10000 delay0_remove0_e8_r0_size128_gto48_pw32768_ww32768_c64_bw95_aw4000_rw20000 delay0_remove0_e8_r0_size128_gto48_pw65536_ww32768_c64_bw95_aw8000_rw40000 delay0_remove0_e8_r0_size128_gto48_pw131072_ww32768_c64_bw95_aw16000_rw80000 delay0_remove0_e8_r0_size128_gto48_pw262144_ww32768_c64_bw95_aw32000_rw160000 delay0_remove0_e8_r0_size128_gto48_pw524288_ww32768_c64_bw95_aw64000_rw320000 delay0_remove0_e8_r0_size128_gto48_pw1048576_ww32768_c64_bw95_aw128000_rw640000
 do
 
 #15
@@ -17,7 +11,7 @@ do
 cd $mother_dir
 cd $configs_stor1
 cd polybench
-for benchmark in 
+for benchmark in GESUMMV MVT 2MM 3MM SYRK ATAX BICG 2DCONV_EMBOSS 2DCONV_BLUR 2DCONV 3DCONV GEMM FDTD-2D GRAMSCHM SYR2K
 do
 cd $benchmark
 qsub pbs_$benchmark.pbs
@@ -30,8 +24,7 @@ done
 cd $mother_dir
 cd $configs_stor1
 cd CUDA
-for benchmark in BlackScholes
-#relaunch ray, do not discard size16 when delaying (since it's there in motivation), adjust input size with size 16 for TRA, CONS and relaunch.
+for benchmark in SCP FWT LPS BlackScholes SLA TRA CONS RAY
 do
 cd $benchmark
 qsub pbs_$benchmark.pbs

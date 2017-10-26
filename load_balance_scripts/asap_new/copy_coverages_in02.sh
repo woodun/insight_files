e65ebc5654a15c0a004d50e93232d924d53666ce
woodun <hwang07@email.wm.edu>
2017-10-26 10:08:43 -0400

    debug

--- load_balance_scripts/asap_new/copy_coverages_in02.sh
+++ load_balance_scripts/asap_new/copy_coverages_in02.sh
@@ -4,11 +4,12 @@
 template=/stor1/hwang07/insight_files/insight_template
 
 #####################################################################################
-mother_dir=/stor2/hwang07/rebuttal_osp
+mother_dir=/stor2/hwang07/asap/coverages
 
 #coverage100_dynamic_tsp_nopc_size8_gto48 coverage10_dynamic_tsp_nopc_size8_gto48 wp0_tsp_coverage10_gto48 wp6_tsp_coverage10_gto48
 #coverage100_dynamic_tsp_nopc_size8_RR48 coverage20_dynamic_tsp_nopc_size8_RR48 coverage10_dynamic_tsp_nopc_size8_RR48 coverage15_dynamic_tsp_nopc_size8_gto48 wp0_tsp_coverage15_gto48 wp6_tsp_coverage15_gto48 coverage5_dynamic_tsp_nopc_size8_gto48 wp0_tsp_coverage5_gto48 wp6_tsp_coverage5_gto48
 #coverage20_dynamic_tsp_nopc_size8_gto48 wp0_tsp_coverage20_gto48 wp6_tsp_coverage20_gto48
+
 for configs_stor1 in coverage5_dynamic_osp_nopc_size8_gto48_wattch1 wp0_osp_coverage5_gto48_wattch1 wp6_osp_coverage5_gto48_wattch1 coverage10_dynamic_osp_nopc_size8_gto48_wattch1 wp0_osp_coverage10_gto48_wattch1 wp6_osp_coverage10_gto48_wattch1 coverage15_dynamic_osp_nopc_size8_gto48_wattch1 wp0_osp_coverage15_gto48_wattch1 wp6_osp_coverage15_gto48_wattch1 coverage20_dynamic_osp_nopc_size8_gto48_wattch1 wp0_osp_coverage20_gto48_wattch1 wp6_osp_coverage20_gto48_wattch1
 do
 
--- load_balance_scripts/asap_new/launch_coverages_in01.sh
+++ load_balance_scripts/asap_new/launch_coverages_in01.sh
@@ -2,21 +2,34 @@
 #this one works with applications_modified
 
 #####################################################
-mother_dir=/stor1/hwang07/rebuttal_tsp
+mother_dir=/stor1/hwang07/asap/coverages
 
 for configs_stor1 in coverage5_dynamic_tsp_nopc_size8_gto48_wattch1 wp0_tsp_coverage5_gto48_wattch1 wp6_tsp_coverage5_gto48_wattch1 coverage10_dynamic_tsp_nopc_size8_gto48_wattch1 wp0_tsp_coverage10_gto48_wattch1 wp6_tsp_coverage10_gto48_wattch1 coverage15_dynamic_tsp_nopc_size8_gto48_wattch1 wp0_tsp_coverage15_gto48_wattch1 wp6_tsp_coverage15_gto48_wattch1 coverage20_dynamic_tsp_nopc_size8_gto48_wattch1 wp0_tsp_coverage20_gto48_wattch1 wp6_tsp_coverage20_gto48_wattch1
 do
 
+#10
+#binarization blackscholes convolution inversek2j jmeint laplacian meanfilter newton-raph sobel srad
+cd $mother_dir
+cd $configs_stor1
+cd axbench
+for benchmark in binarization blackscholes convolution inversek2j jmeint laplacian meanfilter newton-raph sobel srad
+do
+cd $benchmark
+qsub in01_pbs_$benchmark.pbs
+cd ..
+done
+
 #13
 #not changed: ATAX BICG GESUMMV MVT SYR2K SYRK 2DCONV 3DCONV
 #tsp not good: GEMM FDTD-2D 2MM 3MM GRAMSCHM
+#GESUMMV SYR2K SYRK 2DCONV_EMBOSS 2DCONV_BLUR 3DCONV
 cd $mother_dir
 cd $configs_stor1
 cd polybench
-for benchmark in GESUMMV SYR2K SYRK 2DCONV_EMBOSS 2DCONV_BLUR 3DCONV
+for benchmark in 
 do
 cd $benchmark
-qsub pbs_$benchmark.pbs
+qsub in01_pbs_$benchmark.pbs
 cd ..
 done
 
@@ -24,13 +37,14 @@ done
 #not changed: CONS TRA SCP SLA
 #tsp not good: BlackScholes JPEG_ENCODE
 #7
+#SLA TRA SCP CONS
 cd $mother_dir
 cd $configs_stor1
 cd CUDA
-for benchmark in SLA TRA SCP CONS
+for benchmark in 
 do
 cd $benchmark
-qsub pbs_$benchmark.pbs
+qsub in01_pbs_$benchmark.pbs
 cd ..
 done
 

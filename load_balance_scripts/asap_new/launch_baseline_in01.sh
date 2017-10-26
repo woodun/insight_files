a4c57d92d1be091a044466597254b97059d15c14
woodun <hwang07@email.wm.edu>
2017-10-26 09:22:37 -0400

    debug

--- load_balance_scripts/asap_new/launch_baseline_in01.sh
+++ load_balance_scripts/asap_new/launch_baseline_in01.sh
@@ -7,17 +7,30 @@ mother_dir=/stor1/hwang07/rebuttal_baseline
 for configs_stor1 in tsp_address_exp_gtoswl48_alloff_wattch1
 do
 
+#10
+#binarization blackscholes convolution inversek2j jmeint laplacian meanfilter newton-raph sobel srad
+cd $mother_dir
+cd $configs_stor1
+cd axbench
+for benchmark in binarization blackscholes convolution inversek2j jmeint laplacian meanfilter newton-raph sobel srad
+do
+cd $benchmark
+qsub in01_pbs_$benchmark.pbs
+cd ..
+done
+
 #13
 #not changed: 2DCONV 3DCONV ATAX BICG GESUMMV MVT SYR2K SYRK
 #tsp not good: GEMM FDTD-2D 2MM 3MM GRAMSCHM
 #2DCONV 3DCONV 2MM 3MM ATAX BICG FDTD-2D GEMM GESUMMV GRAMSCHM MVT SYR2K SYRK 2DCONV_BLUR 2DCONV_EDGE 2DCONV_EMBOSS 2DCONV_ENHANCE 2DCONV_SHARPEN
+#GESUMMV SYR2K SYRK 2DCONV_EMBOSS 2DCONV_BLUR 3DCONV
 cd $mother_dir
 cd $configs_stor1
 cd polybench
-for benchmark in GESUMMV SYR2K SYRK 2DCONV_EMBOSS 2DCONV_BLUR 3DCONV
+for benchmark in 
 do
 cd $benchmark
-qsub pbs_$benchmark.pbs
+qsub in01_pbs_$benchmark.pbs
 cd ..
 done
 
@@ -26,13 +39,14 @@ done
 #tsp not good: BlackScholes JPEG_ENCODE
 #7 
 #BlackScholes LPS RAY SCP SLA CONS TRA JPEG_ENCODE JPEG_DECODE
+#SLA TRA SCP CONS
 cd $mother_dir
 cd $configs_stor1
 cd CUDA
-for benchmark in SLA TRA SCP CONS
+for benchmark in 
 do
 cd $benchmark
-qsub pbs_$benchmark.pbs
+qsub in01_pbs_$benchmark.pbs
 cd ..
 done
 

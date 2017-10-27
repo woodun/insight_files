cc04a1840d5acb02fb8027b9df22d6557650a22c
woodun <hwang07@email.wm.edu>
2017-10-27 03:09:33 -0400

    debug

--- load_balance_scripts/asap_new/launch_baseline_in01.sh
+++ load_balance_scripts/asap_new/launch_baseline_in01.sh
@@ -12,7 +12,7 @@ do
 cd $mother_dir
 cd $configs_stor1
 cd axbench
-for benchmark in binarization blackscholes convolution inversek2j jmeint laplacian meanfilter newton-raph sobel srad
+for benchmark in 
 do
 cd $benchmark
 qsub in01_pbs_$benchmark.pbs
@@ -44,6 +44,61 @@ cd $mother_dir
 cd $configs_stor1
 cd CUDA
 for benchmark in 
+do
+cd $benchmark
+qsub in01_pbs_$benchmark.pbs
+cd ..
+done
+
+#bfs bh dmr mst sp sssp
+cd $mother_dir
+cd $configs_stor1
+cd lonestar
+for benchmark in bfs bh dmr mst sp sssp
+do
+cd $benchmark
+qsub in01_pbs_$benchmark.pbs
+cd ..
+done
+
+#SimilarityScore Kmeans MatrixMul InvertedIndex PageViewCount PageViewRank StringMatch WordCount
+cd $mother_dir
+cd $configs_stor1
+cd Mars
+for benchmark in SimilarityScore Kmeans MatrixMul InvertedIndex PageViewCount PageViewRank StringMatch WordCount
+do
+cd $benchmark
+qsub in01_pbs_$benchmark.pbs
+cd ..
+done
+
+#cutcp histo mm sad spmv lbm tpacf
+cd $mother_dir
+cd $configs_stor1
+cd parboil
+for benchmark in cutcp histo mm sad spmv lbm tpacf
+do
+cd $benchmark
+qsub in01_pbs_$benchmark.pbs
+cd ..
+done
+
+#backprop bfs hotspot heartwall cfd streamcluster nw pathfinder lud leukocyte srad_v1 srad_v2 pf_float
+cd $mother_dir
+cd $configs_stor1
+cd rodinia
+for benchmark in backprop bfs hotspot heartwall cfd streamcluster nw pathfinder lud leukocyte srad_v1 srad_v2 pf_float
+do
+cd $benchmark
+qsub in01_pbs_$benchmark.pbs
+cd ..
+done
+
+#MD QTC Reduction Scan Spmv Stencil2D Triad BFS
+cd $mother_dir
+cd $configs_stor1
+cd shoc
+for benchmark in MD QTC Reduction Scan Spmv Stencil2D Triad BFS
 do
 cd $benchmark
 qsub in01_pbs_$benchmark.pbs

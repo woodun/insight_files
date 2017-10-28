73572df2581a026af62601392cdb3e51699cbf0c
woodun <hwang07@email.wm.edu>
2017-10-27 23:18:04 -0400

    debug

--- hima_scripts/amc_paper/axbench_added/copy_queue_sizes_gto.sh
+++ hima_scripts/amc_paper/axbench_added/copy_queue_sizes_gto.sh
@@ -5,7 +5,7 @@ template=/sciclone/data10/hwang07/GPU_RESEARCH/insight_files/hima_template
 #####################################################################################
 mother_dir=/sciclone/data10/hwang07/GPU_RESEARCH/amc/queue_sizes
 
-for configs_stor1 in delay4096_remove0_e8_r0_sizeinf_gto48_pb0_pe0_ww0_c128_bw95_aw0_rw0_rp64 delay2048_remove0_e8_r0_sizeinf_gto48_pb0_pe0_ww0_c128_bw95_aw0_rw0_rp64 delay1024_remove0_e8_r0_sizeinf_gto48_pb0_pe0_ww0_c128_bw95_aw0_rw0_rp64 delay512_remove0_e8_r0_sizeinf_gto48_pb0_pe0_ww0_c128_bw95_aw0_rw0_rp64 delay256_remove0_e8_r0_sizeinf_gto48_pb0_pe0_ww0_c128_bw95_aw0_rw0_rp64 delay128_remove0_e8_r0_sizeinf_gto48_pb0_pe0_ww0_c128_bw95_aw0_rw0_rp64 delay64_remove0_e8_r0_sizeinf_gto48_pb0_pe0_ww0_c128_bw95_aw0_rw0_rp64
+for configs_stor1 in delay0_remove0_e8_r0_size16_gto48_pb0_pe0_ww0_c128_bw95_aw0_rw0_rp64 delay0_remove0_e8_r0_size32_gto48_pb0_pe0_ww0_c128_bw95_aw0_rw0_rp64 delay0_remove0_e8_r0_size64_gto48_pb0_pe0_ww0_c128_bw95_aw0_rw0_rp64 delay0_remove0_e8_r0_size128_gto48_pb0_pe0_ww0_c128_bw95_aw0_rw0_rp64 delay0_remove0_e8_r0_size256_gto48_pb0_pe0_ww0_c128_bw95_aw0_rw0_rp64 delay0_remove0_e8_r0_size512_gto48_pb0_pe0_ww0_c128_bw95_aw0_rw0_rp64 delay0_remove0_e8_r0_size1024_gto48_pb0_pe0_ww0_c128_bw95_aw0_rw0_rp64 delay0_remove0_e8_r0_sizeinf_gto48_pb0_pe0_ww0_c128_bw95_aw0_rw0_rp64
 do
 
 cp -r $template $mother_dir/$configs_stor1

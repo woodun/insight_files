97abd3f1f18d8809c6c3a357883fe5a0574c3246
woodun <hwang07@email.wm.edu>
2017-10-29 18:16:20 -0400

    debug

--- hima_scripts/amc_paper/axbench_added/launch_size128_delayauto_remove0_gto.sh
+++ hima_scripts/amc_paper/axbench_added/launch_size128_delayauto_remove0_gto.sh
@@ -8,11 +8,12 @@ do
 
 #15
 #all: GESUMMV MVT 2MM 3MM SYRK ATAX BICG 2DCONV_EMBOSS 2DCONV_BLUR 2DCONV 3DCONV GEMM FDTD-2D GRAMSCHM SYR2K
-#removed: 2DCONV FDTD-2D GRAMSCHM
+#removed: 2DCONV FDTD-2D GRAMSCHM SYR2K
+#GESUMMV MVT 2MM 3MM SYRK ATAX BICG 2DCONV_EMBOSS 2DCONV_BLUR 3DCONV GEMM
 cd $mother_dir
 cd $configs_stor1
 cd polybench
-for benchmark in GESUMMV MVT 2MM 3MM SYRK ATAX BICG 2DCONV_EMBOSS 2DCONV_BLUR 3DCONV GEMM SYR2K
+for benchmark in GESUMMV MVT 2MM 3MM SYRK ATAX BICG 2DCONV_EMBOSS 2DCONV_BLUR 3DCONV GEMM
 do
 cd $benchmark
 qsub hi_pbs_$benchmark.pbs
@@ -23,10 +24,11 @@ done
 #figures: srad_v1 histo JPEG RAY
 #all: SCP FWT LPS BlackScholes SLA TRA CONS RAY
 #removed:
+#TRA SCP CONS FWT LPS BlackScholes SLA RAY
 cd $mother_dir
 cd $configs_stor1
 cd CUDA
-for benchmark in SCP FWT LPS BlackScholes SLA TRA CONS RAY
+for benchmark in TRA SCP CONS FWT LPS BlackScholes SLA RAY
 do
 cd $benchmark
 qsub hi_pbs_$benchmark.pbs
@@ -34,8 +36,9 @@ cd ..
 done
 
 #10
-#binarization blackscholes convolution inversek2j jmeint laplacian meanfilter newton-raph sobel srad
+#all: binarization blackscholes convolution inversek2j jmeint laplacian meanfilter newton-raph sobel srad
 #removed: binarization
+#blackscholes convolution inversek2j jmeint laplacian meanfilter newton-raph sobel srad
 cd $mother_dir
 cd $configs_stor1
 cd axbench

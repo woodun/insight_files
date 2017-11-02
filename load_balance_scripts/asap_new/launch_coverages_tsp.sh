e9a766f34daecd6c25421284558195ce1ef3e90d
woodun <hwang07@email.wm.edu>
2017-11-01 20:13:07 -0400

    debug

--- load_balance_scripts/asap_new/launch_coverages_tsp.sh
+++ load_balance_scripts/asap_new/launch_coverages_tsp.sh
@@ -8,58 +8,6 @@ mother_dir=/stor1/hwang07/asap/coverages
 for configs_stor1 in coverage20_dynamic_tsp_nopc_size8_gto48_wattch1 wp0_tsp_coverage20_gto48_wattch1 wp6_tsp_coverage20_gto48_wattch1
 do
 
-#10
-#binarization blackscholes convolution inversek2j jmeint laplacian meanfilter newton-raph sobel srad
-cd $mother_dir
-cd $configs_stor1
-cd axbench
-for benchmark in 
-do
-cd $benchmark
-qsub in01_pbs_$benchmark.pbs
-cd ..
-done
-
-#13
-#not changed: ATAX BICG GESUMMV MVT SYR2K SYRK 2DCONV 3DCONV
-#tsp not good: GEMM FDTD-2D 2MM 3MM GRAMSCHM
-#GESUMMV SYR2K SYRK 2DCONV_EMBOSS 2DCONV_BLUR 3DCONV
-cd $mother_dir
-cd $configs_stor1
-cd polybench
-for benchmark in 
-do
-cd $benchmark
-qsub in01_pbs_$benchmark.pbs
-cd ..
-done
-
-#figures: srad_v1 histo JPEG RAY
-#not changed: CONS TRA SCP SLA
-#tsp not good: BlackScholes JPEG_ENCODE
-#7
-#SLA TRA SCP CONS
-cd $mother_dir
-cd $configs_stor1
-cd CUDA
-for benchmark in 
-do
-cd $benchmark
-qsub in01_pbs_$benchmark.pbs
-cd ..
-done
-
-#bfs bh dmr mst sp sssp
-cd $mother_dir
-cd $configs_stor1
-cd lonestar
-for benchmark in bfs bh dmr mst sp sssp
-do
-cd $benchmark
-qsub in01_pbs_$benchmark.pbs
-cd ..
-done
-
 #SimilarityScore Kmeans MatrixMul InvertedIndex PageViewCount PageViewRank StringMatch WordCount
 cd $mother_dir
 cd $configs_stor1
@@ -98,6 +46,58 @@ cd $mother_dir
 cd $configs_stor1
 cd shoc
 for benchmark in MD QTC Reduction Scan Spmv Stencil2D Triad BFS
+do
+cd $benchmark
+qsub in01_pbs_$benchmark.pbs
+cd ..
+done
+
+#bfs bh dmr mst sp sssp
+cd $mother_dir
+cd $configs_stor1
+cd lonestar
+for benchmark in 
+do
+cd $benchmark
+qsub in01_pbs_$benchmark.pbs
+cd ..
+done 
+
+#10
+#binarization blackscholes convolution inversek2j jmeint laplacian meanfilter newton-raph sobel srad
+cd $mother_dir
+cd $configs_stor1
+cd axbench
+for benchmark in 
+do
+cd $benchmark
+qsub in01_pbs_$benchmark.pbs
+cd ..
+done
+
+#13
+#not changed: ATAX BICG GESUMMV MVT SYR2K SYRK 2DCONV 3DCONV
+#tsp not good: GEMM FDTD-2D 2MM 3MM GRAMSCHM
+#GESUMMV SYR2K SYRK 2DCONV_EMBOSS 2DCONV_BLUR 3DCONV
+cd $mother_dir
+cd $configs_stor1
+cd polybench
+for benchmark in 
+do
+cd $benchmark
+qsub in01_pbs_$benchmark.pbs
+cd ..
+done
+
+#figures: srad_v1 histo JPEG RAY
+#not changed: CONS TRA SCP SLA
+#tsp not good: BlackScholes JPEG_ENCODE
+#7
+#SLA TRA SCP CONS
+cd $mother_dir
+cd $configs_stor1
+cd CUDA
+for benchmark in 
 do
 cd $benchmark
 qsub in01_pbs_$benchmark.pbs

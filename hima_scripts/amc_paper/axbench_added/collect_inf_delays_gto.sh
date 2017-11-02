86abf25459bc56cb263237ed86db64e0f5961a80
woodun <hwang07@email.wm.edu>
2017-11-02 15:16:17 -0400

    debug

--- hima_scripts/amc_paper/axbench_added/collect_inf_delays_gto.sh
+++ hima_scripts/amc_paper/axbench_added/collect_inf_delays_gto.sh
@@ -4,7 +4,7 @@
 output=/sciclone/data10/hwang07/GPU_RESEARCH/amc_collection/inf_delays_new.txt
 mother_dir=/sciclone/data10/hwang07/GPU_RESEARCH/amc/inf_delays
 
-for statistics in 'overall accesses all:' 'overall activations all:' 'bw_util=' 'dram_eff=' 'gpu_tot_ipc =' 'readonly accesses all:' 'readonly activations all:' 'total global read access:' 'L1D_total_cache_miss_rate =' 'L2_total_cache_miss_rate =' 'removed all:' 'delay queue full all:' 'row_hit_0 all:' 'actual coverage all:' 'used row all:' 'actual delay:' 'profiling done cycle:'
+for statistics in 'overall accesses all:' 'overall activations all:' 'bw_util=' 'dram_eff=' 'gpu_tot_ipc =' 'readonly accesses all:' 'readonly activations all:' 'total global read access:' 'L1D_total_cache_miss_rate =' 'L2_total_cache_miss_rate =' 'removed all:' 'delay queue full all:' 'row_hit_0 all:' 'actual coverage all:' 'used row all:' 'actual delay:' 'profiling done cycle:' 'reprofiling count:'
 do
 
 printf "%s\r\n" $statistics >> $output

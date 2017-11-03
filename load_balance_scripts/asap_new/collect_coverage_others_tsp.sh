c5a81fb7a2e89a28fa792efa2bdd09c15781bb87
woodun <hwang07@email.wm.edu>
2017-11-03 05:57:11 -0400

    debug

---
+++ load_balance_scripts/asap_new/collect_coverage_others_tsp.sh
@@ -0,0 +1,75 @@
+#!/bin/sh
+
+#specify your output file
+output=/stor1/hwang07/paper_collection/asap_more_apps_osp.txt
+mother_dir=/stor1/hwang07/asap/coverages
+
+for statistics in 'gpu_tot_ipc =' 'to access rate:' 'predictable to miss rate:' 'predictable lines:' 'bw_util=' 'bwutil_global_read=' 'bwutil_global_write=' 'L1D_total_cache_miss_rate =' 'L1D_total_cache_misses =' 'L2_total_cache_miss_rate =' 'L2_total_cache_misses =' 'average_avg_distance:' 'average_avg_bias:' 'Stall:' 'W0_Idle:' 'W0_Scoreboard:' 'averagemflatency ='
+do
+
+printf "%s\r\n" $statistics >> $output
+
+#specify your config path in stor1
+for configs_stor1 in coverage20_dynamic_tsp_nopc_size8_gto48_wattch1 wp0_tsp_coverage20_gto48_wattch1 wp6_tsp_coverage20_gto48_wattch1
+do
+
+#bfs bh dmr mst sp sssp
+cd $mother_dir
+cd $configs_stor1
+cd lonestar
+for benchmark in bfs bh dmr mst sp sssp
+do
+cd $benchmark
+grep -o "$statistics[ ]*[-eE\+0-9\.]*" output_* | tail -1 | sed -e "s/$statistics[ ]*\(-$\)*//g" | xargs printf "0%s " >> $output
+cd ..
+done
+
+#SimilarityScore Kmeans MatrixMul InvertedIndex PageViewCount PageViewRank StringMatch WordCount
+cd $mother_dir
+cd $configs_stor1
+cd Mars
+for benchmark in SimilarityScore Kmeans MatrixMul InvertedIndex PageViewCount PageViewRank StringMatch WordCount
+do
+cd $benchmark
+grep -o "$statistics[ ]*[-eE\+0-9\.]*" output_* | tail -1 | sed -e "s/$statistics[ ]*\(-$\)*//g" | xargs printf "0%s " >> $output
+cd ..
+done
+
+#cutcp histo mm sad spmv lbm tpacf
+cd $mother_dir
+cd $configs_stor1
+cd parboil
+for benchmark in cutcp histo mm sad spmv lbm tpacf
+do
+cd $benchmark
+grep -o "$statistics[ ]*[-eE\+0-9\.]*" output_* | tail -1 | sed -e "s/$statistics[ ]*\(-$\)*//g" | xargs printf "0%s " >> $output
+cd ..
+done
+
+#backprop bfs hotspot heartwall cfd streamcluster nw pathfinder lud leukocyte srad_v1 srad_v2 pf_float
+cd $mother_dir
+cd $configs_stor1
+cd rodinia
+for benchmark in backprop bfs hotspot heartwall cfd streamcluster nw pathfinder lud leukocyte srad_v1 srad_v2 pf_float
+do
+cd $benchmark
+grep -o "$statistics[ ]*[-eE\+0-9\.]*" output_* | tail -1 | sed -e "s/$statistics[ ]*\(-$\)*//g" | xargs printf "0%s " >> $output
+cd ..
+done
+
+#MD QTC Reduction Scan Spmv Stencil2D Triad BFS
+cd $mother_dir
+cd $configs_stor1
+cd shoc
+for benchmark in MD QTC Reduction Scan Spmv Stencil2D Triad BFS
+do
+cd $benchmark
+grep -o "$statistics[ ]*[-eE\+0-9\.]*" output_* | tail -1 | sed -e "s/$statistics[ ]*\(-$\)*//g" | xargs printf "0%s " >> $output
+cd ..
+done
+
+printf "\r\n" >> $output
+done
+######################################################################################################################################
+
+done

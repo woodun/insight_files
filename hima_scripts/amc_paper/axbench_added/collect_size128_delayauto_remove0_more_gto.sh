e9a2e902aa0c1d444aebafa82bb9a96ea52254d5
woodun <hwang07@email.wm.edu>
2017-11-13 14:59:32 -0500

    debug

--- hima_scripts/amc_paper/axbench_added/collect_size128_delayauto_remove0_more_gto.sh
+++ hima_scripts/amc_paper/axbench_added/collect_size128_delayauto_remove0_more_gto.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #specify your output file
-output=/sciclone/data10/hwang07/GPU_RESEARCH/amc_collection/size128_delayauto_remove0_more.txt
+output=/sciclone/data10/hwang07/GPU_RESEARCH/amc_collection/size128_delayauto_remove0_more2.txt
 mother_dir=/sciclone/data10/hwang07/GPU_RESEARCH/amc/size128_delayauto_all
 
 for statistics in 'overall accesses all:' 'overall activations all:' 'bw_util=' 'dram_eff=' 'gpu_tot_ipc =' 'readonly accesses all:' 'readonly activations all:' 'total global read access:' 'L1D_total_cache_miss_rate =' 'L2_total_cache_miss_rate =' 'removed all:' 'delay queue full all:' 'row_hit_0 all:' 'actual coverage all:' 'used row all:' 'actual delay:' 'profiling done cycle:' 'reprofiling count:'
@@ -10,7 +10,7 @@ do
 printf "%s\r\n" $statistics >> $output
 
 #specify your config path in stor1
-for configs_stor1 in delayA_remove0_e8_r0_size128_gto48_pb4096_pe0_ww65536_c128_bw95_aw0_rw0_rp32 delayX_remove0_e8_r0_size128_gto48_pb4096_pe0_ww65536_c128_bw95_aw0_rw0_rp32
+for configs_stor1 in copy_starting0_delayA_remove0_e8_r0_size128_gto48_pb4096_pe0_ww65536_c128_bw95_aw0_rw0_rp32
 do
 
 #15

e9603d589778f63de6c05fe1d0ffe9e5c1952d46
woodun <hwang07@email.wm.edu>
2018-03-22 03:22:28 -0400

    debug

--- load_balance_scripts/address_stride_paper/new_figure15/modified/restricted/launch_coverages_in02.sh
+++ load_balance_scripts/address_stride_paper/new_figure15/modified/restricted/launch_coverages_in02.sh
@@ -2,24 +2,27 @@
 #this one works with applications_modified
 
 #####################################################launch in in01?
-mother_dir=/stor2/hwang07/restricted_asap
+mother_dir=/stor2/hwang07/
 
-for configs_stor1 in coverage100_dynamic_osp_nopc_size8_gto48 coverage100_dynamic_tsp_nopc_size8_gto48 dynamic_nopc_profile_gto48 osp_profile_gto48
+#dynamic_nopc_profile_gto48 osp_profile_gto48
+for configs_stor1 in coverage100_dynamic_osp_nopc_size8_gto48 coverage100_dynamic_tsp_nopc_size8_gto48
 do
 
+#ATAX BICG GESUMMV SYR2K SYRK 2DCONV_EMBOSS 2DCONV_BLUR 3DCONV
 #13
 #not changed: ATAX BICG GESUMMV MVT SYR2K SYRK 2DCONV 3DCONV
 #tsp not good: GEMM FDTD-2D 2MM 3MM GRAMSCHM
 cd $mother_dir
 cd $configs_stor1
 cd polybench
-for benchmark in ATAX BICG GESUMMV SYR2K SYRK 2DCONV_EMBOSS 2DCONV_BLUR 3DCONV
+for benchmark in ATAX BICG GESUMMV SYR2K SYRK
 do
 cd $benchmark
 qsub in02_pbs_$benchmark.pbs
 cd ..
 done
 
+#LPS CONS SCP SLA
 #figures: srad_v1 histo JPEG RAY
 #not changed: CONS TRA SCP SLA
 #tsp not good: BlackScholes JPEG_ENCODE
@@ -27,7 +30,7 @@ done
 cd $mother_dir
 cd $configs_stor1
 cd CUDA
-for benchmark in LPS CONS SCP SLA
+for benchmark in LPS
 do
 cd $benchmark
 qsub in02_pbs_$benchmark.pbs

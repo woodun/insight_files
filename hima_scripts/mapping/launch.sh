e0aae7518267ae679ec4d21a82dd1529a25c31ef
woodun <hwang07@email.wm.edu>
2019-01-23 17:31:49 -0500

    debug

--- hima_scripts/mapping/launch.sh
+++ hima_scripts/mapping/launch.sh
@@ -2,37 +2,104 @@
 #this one works with applications_modified
 
 #####################################################
-mother_dir=/sciclone/data10/hwang07/GPU_RESEARCH/asap_rebuttal
+mother_dir=/sciclone/pscr/hwang07/mapping_exp
 
-#coverage5_dynamic_tsp_nopc_size8_gto48_wattch1 wp0_tsp_coverage5_gto48_wattch1 wp6_tsp_coverage5_gto48_wattch1 coverage10_dynamic_tsp_nopc_size8_gto48_wattch1 wp0_tsp_coverage10_gto48_wattch1 wp6_tsp_coverage10_gto48_wattch1 coverage15_dynamic_tsp_nopc_size8_gto48_wattch1 wp0_tsp_coverage15_gto48_wattch1 wp6_tsp_coverage15_gto48_wattch1 coverage20_dynamic_tsp_nopc_size8_gto48_wattch1 wp0_tsp_coverage20_gto48_wattch1 wp6_tsp_coverage20_gto48_wattch1
-for configs_stor1 in coverage5_dynamic_osp_nopc_size8_gto48_wattch1 wp0_osp_coverage5_gto48_wattch1 wp6_osp_coverage5_gto48_wattch1 coverage10_dynamic_osp_nopc_size8_gto48_wattch1 wp0_osp_coverage10_gto48_wattch1 wp6_osp_coverage10_gto48_wattch1 coverage15_dynamic_osp_nopc_size8_gto48_wattch1 wp0_osp_coverage15_gto48_wattch1 wp6_osp_coverage15_gto48_wattch1 coverage20_dynamic_osp_nopc_size8_gto48_wattch1 wp0_osp_coverage20_gto48_wattch1 wp6_osp_coverage20_gto48_wattch1
+for configs_stor1 in mapping0_GTX480 mapping1_GTX480 mapping2_GTX480 mapping3_GTX480
 do
 
-#13
-#not changed: ATAX BICG GESUMMV MVT SYR2K SYRK 2DCONV 3DCONV
-#tsp not good: GEMM FDTD-2D 2MM 3MM GRAMSCHM
-cd $mother_dir
-cd $configs_stor1
-cd polybench
-for benchmark in GESUMMV SYR2K SYRK 2DCONV_EMBOSS 2DCONV_BLUR 3DCONV
-do
-cd $benchmark
-qsub pbs_$benchmark.pbs
-cd ..
-done
-
-#figures: srad_v1 histo JPEG RAY
-#not changed: CONS TRA SCP SLA
-#tsp not good: BlackScholes JPEG_ENCODE
-#7
 cd $mother_dir
 cd $configs_stor1
 cd CUDA
-for benchmark in SLA TRA SCP CONS
+for benchmark in BlackScholes CP LIB LPS NQU RAY STO SCP SLA CONS FWT TRA lulesh AES BFS MUM NN kmeans JPEG BFS2 KMN
 do
 cd $benchmark
-qsub pbs_$benchmark.pbs
+qsub hi_pbs_$benchmark.pbs
+cd ..
+done
+
+cd $mother_dir
+cd $configs_stor1
+cd polybench
+for benchmark in 2DCONV 3DCONV 2MM 3MM ATAX BICG CORR COVAR FDTD-2D GEMM GESUMMV GRAMSCHM MVT SYR2K SYRK
+do
+cd $benchmark
+qsub hi_pbs_$benchmark.pbs
+cd ..
+done
+
+cd $mother_dir
+cd $configs_stor1
+cd axbench
+for benchmark in binarization blackscholes convolution inversek2j jmeint laplacian meanfilter newton-raph sobel srad
+do
+cd $benchmark
+qsub hi_pbs_$benchmark.pbs
+cd ..
+done
+
+cd $mother_dir
+cd $configs_stor1
+cd lonestar
+for benchmark in bfs bh dmr mst sp sssp
+do
+cd $benchmark
+qsub hi_pbs_$benchmark.pbs
+cd ..
+done
+
+cd $mother_dir
+cd $configs_stor1
+cd Mars
+for benchmark in SimilarityScore Kmeans MatrixMul InvertedIndex PageViewCount PageViewRank StringMatch WordCount
+do
+cd $benchmark
+qsub hi_pbs_$benchmark.pbs
+cd ..
+done
+
+cd $mother_dir
+cd $configs_stor1
+cd parboil
+for benchmark in cutcp histo mm sad spmv lbm tpacf
+do
+cd $benchmark
+qsub hi_pbs_$benchmark.pbs
+cd ..
+done
+
+cd $mother_dir
+cd $configs_stor1
+cd rodinia
+for benchmark in backprop bfs hotspot heartwall cfd streamcluster nw pathfinder lud leukocyte srad_v1 srad_v2 pf_float
+do
+cd $benchmark
+qsub hi_pbs_$benchmark.pbs
+cd ..
+done
+
+cd $mother_dir
+cd $configs_stor1
+cd shoc
+for benchmark in MD QTC Reduction Scan Spmv Stencil2D Triad BFS
+do
+cd $benchmark
+qsub hi_pbs_$benchmark.pbs
 cd ..
 done
 
 done
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

188079514990983383848232a333504ea4e7612d
woodun <hwang07@email.wm.edu>
2019-01-28 18:18:05 -0500

    debug

---
+++ hima_scripts/mapping/launch_sci_mapping_8_1.sh
@@ -0,0 +1,105 @@
+#!/bin/sh
+#this one works with applications_modified
+
+#####################################################
+mother_dir=/sciclone/pscr/hwang07/mapping_exp
+
+for configs_stor1 in mapping0_GTX480_8_1 mapping1_GTX480_8_1 mapping2_GTX480_8_1 mapping3_GTX480_8_1
+do
+
+cd $mother_dir
+cd $configs_stor1
+cd CUDA
+for benchmark in BlackScholes CP LIB LPS NQU RAY STO SCP SLA CONS FWT TRA lulesh AES BFS MUM NN kmeans JPEG BFS2 KMN
+do
+cd $benchmark
+qsub sci_pbs_$benchmark.pbs
+cd ..
+done
+
+cd $mother_dir
+cd $configs_stor1
+cd polybench
+for benchmark in 2DCONV 3DCONV 2MM 3MM ATAX BICG CORR COVAR FDTD-2D GEMM GESUMMV GRAMSCHM MVT SYR2K SYRK
+do
+cd $benchmark
+qsub sci_pbs_$benchmark.pbs
+cd ..
+done
+
+cd $mother_dir
+cd $configs_stor1
+cd axbench
+for benchmark in binarization blackscholes convolution inversek2j jmeint laplacian meanfilter newton-raph sobel srad
+do
+cd $benchmark
+qsub sci_pbs_$benchmark.pbs
+cd ..
+done
+
+cd $mother_dir
+cd $configs_stor1
+cd lonestar
+for benchmark in bfs bh dmr mst sp sssp
+do
+cd $benchmark
+qsub sci_pbs_$benchmark.pbs
+cd ..
+done
+
+cd $mother_dir
+cd $configs_stor1
+cd Mars
+for benchmark in SimilarityScore Kmeans MatrixMul InvertedIndex PageViewCount PageViewRank StringMatch WordCount
+do
+cd $benchmark
+qsub sci_pbs_$benchmark.pbs
+cd ..
+done
+
+cd $mother_dir
+cd $configs_stor1
+cd parboil
+for benchmark in cutcp histo mm sad spmv lbm tpacf
+do
+cd $benchmark
+qsub sci_pbs_$benchmark.pbs
+cd ..
+done
+
+cd $mother_dir
+cd $configs_stor1
+cd rodinia
+for benchmark in backprop bfs hotspot heartwall cfd streamcluster nw pathfinder lud leukocyte srad_v1 srad_v2 pf_float
+do
+cd $benchmark
+qsub sci_pbs_$benchmark.pbs
+cd ..
+done
+
+cd $mother_dir
+cd $configs_stor1
+cd shoc
+for benchmark in MD QTC Reduction Scan Spmv Stencil2D Triad BFS
+do
+cd $benchmark
+qsub sci_pbs_$benchmark.pbs
+cd ..
+done
+
+done
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

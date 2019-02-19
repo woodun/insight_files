4526ba0292ef52c588c1e8688e60a5b574f81a2f
woodun <hwang07@email.wm.edu>
2019-02-19 15:23:01 -0500

    debug

--- hima_scripts/mapping/collect_mapping_scope2.sh
+++ hima_scripts/mapping/collect_mapping_scope2.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #specify your output file
-output=/sciclone/pscr/hwang07/mapping_collection/hima_mapping_scope_metrics.txt
+output=/sciclone/pscr/hwang07/mapping_collection/hima_mapping_scope2_metrics.txt
 mother_dir=/sciclone/pscr/hwang07/mapping_exp
 #
 for statistics in 'gpu_tot_ipc =     ' 'average rbl = ' 'r_blp_all=' 'r_eblp_all=' 'r_blc_all=' 'r_eblc_all=' 'r_rblc_all=' 'r_clp=' 'r_eclp=' 'r_clc=' 'r_eclc=' 'r_rclc=' 'r_waste_all=' 'dram_eff_all=' 'n_waste_all=' 'bw_util_all='

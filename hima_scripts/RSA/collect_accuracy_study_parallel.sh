c6dfd2cacf14f57a9c0c9e6e4db167fe609e5775
woodun <hwang07@email.wm.edu>
2019-03-01 16:03:52 -0500

    debug

---
+++ hima_scripts/RSA/collect_accuracy_study_parallel.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+#specify your output file
+output=/sciclone/pscr/hwang07/RSA_CUDA/accuracy_study_parallel/summary.txt
+mother_dir=/sciclone/pscr/hwang07/RSA_CUDA/accuracy_study_parallel
+
+cd $mother_dir
+
+for statistics in 'vote_only: total count:' 'filter_only: total count:' 'filter_and_vote: total count:' 'no_means: total count:' 'always passed: total count:'
+do
+printf "%s\r\n" $statistics >> $output
+
+for configs_stor1 in `ls 2048_volta_16384*`
+do
+printf "%s\r\n" $configs_stor1
+
+grep "$statistics[ ]*[-eE\+0-9\.]*" output_* | tail -1 | sed -e "s/$statistics[ ]*\(-$\)*//g" | xargs printf "0%s " >> $output
+
+done
+######################################################################################################################################
+printf "\r\n" >> $output
+printf "\r\n" >> $output
+printf "\r\n" >> $output
+done
+
+
+

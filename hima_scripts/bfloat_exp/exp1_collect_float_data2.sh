e683f4f27cac8681cfabde880a6f4df9a1a05b2e
woodun <hwang07@email.wm.edu>
2019-04-26 16:02:49 -0400

    debug

---
+++ hima_scripts/bfloat_exp/exp1_collect_float_data2.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+#####################################################
+output=/sciclone/pscr/hwang07/bfloat_analysis/collection/all_results.txt
+mother_dir=/sciclone/pscr/hwang07/bfloat_analysis/results
+
+cd $mother_dir
+
+
+
+for number in -126 -119 -112 -105 -98 -91 -84 -77 -70 -63 -56 -49 -42 -35 -28 -21 -14 -7 0 7 14 21 28 35 42 49 56 63 70 77 84 91 98 105 112 119 126
+do
+
+printf "range%s:\r\n" $number >> $output
+
+printf "uniform_float_one_side:\r\n" >> $output
+for statistics in 'actual coverage all:' 
+do
+grep "$statistics[ ]*[-eE\+0-9\.]*" uniform_float_one_side_result$number.txt | sed -e "s/$statistics[ ]*\(-$\)*//g" | xargs printf "%s " >> $output
+printf "\r\n" >> $output
+done #for statistics in
+
+printf "uniform_float_one_side:\r\n" >> $output
+for statistics in 'actual coverage all:' 
+do
+grep "$statistics[ ]*[-eE\+0-9\.]*" uniform_float_two_sides_result$number.txt | sed -e "s/$statistics[ ]*\(-$\)*//g" | xargs printf "%s " >> $output
+printf "\r\n" >> $output
+done #for statistics in
+
+printf "uniform_float_one_side:\r\n" >> $output
+for statistics in 'actual coverage all:' 
+do
+grep "$statistics[ ]*[-eE\+0-9\.]*" normal_float_one_side_result$number.txt | sed -e "s/$statistics[ ]*\(-$\)*//g" | xargs printf "%s " >> $output
+printf "\r\n" >> $output
+done #for statistics in
+
+printf "uniform_float_one_side:\r\n" >> $output
+for statistics in 'actual coverage all:' 
+do
+grep "$statistics[ ]*[-eE\+0-9\.]*" normal_float_two_sides_result$number.txt | sed -e "s/$statistics[ ]*\(-$\)*//g" | xargs printf "%s " >> $output
+printf "\r\n" >> $output
+done #for statistics in
+
+printf "\r\n" >> $output
+
+done #for number in
+
+
+
+
---
+++ hima_scripts/bfloat_exp/exp1_collect_float_data3.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+#####################################################
+output=/sciclone/pscr/hwang07/bfloat_analysis/collection/all_results.txt
+mother_dir=/sciclone/pscr/hwang07/bfloat_analysis/results
+
+cd $mother_dir
+
+for statistics in 'actual coverage all:' 
+do
+
+printf "%s\r\n" $statistics >> $output
+
+for number in -126 -119 -112 -105 -98 -91 -84 -77 -70 -63 -56 -49 -42 -35 -28 -21 -14 -7 0 7 14 21 28 35 42 49 56 63 70 77 84 91 98 105 112 119 126
+do
+
+printf "%s:\r\n" $number >> $output
+
+grep "$statistics[ ]*[-eE\+0-9\.]*" uniform_float_one_side_result$number.txt | sed -e "s/$statistics[ ]*\(-$\)*//g" | xargs printf "%s " >> $output
+grep "$statistics[ ]*[-eE\+0-9\.]*" uniform_float_two_sides_result$number.txt | sed -e "s/$statistics[ ]*\(-$\)*//g" | xargs printf "%s " >> $output
+grep "$statistics[ ]*[-eE\+0-9\.]*" normal_float_one_side_result$number.txt | sed -e "s/$statistics[ ]*\(-$\)*//g" | xargs printf "%s " >> $output
+grep "$statistics[ ]*[-eE\+0-9\.]*" normal_float_two_sides_result$number.txt | sed -e "s/$statistics[ ]*\(-$\)*//g" | xargs printf "%s " >> $output
+
+printf "\r\n" >> $output
+
+done #for number in
+
+printf "\r\n" >> $output
+printf "\r\n" >> $output
+
+done #for statistics in
+

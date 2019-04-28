91053825fe4cbfce3b0111be12c1eae1a753d39f
woodun <hwang07@email.wm.edu>
2019-04-27 23:07:20 -0400

    debug

--- hima_scripts/bfloat_exp/exp1_collect_int_data2.sh
+++ hima_scripts/bfloat_exp/exp1_collect_int_data2.sh
@@ -21,21 +21,21 @@ printf "\r\n" >> $output
 done #for statistics in
 
 printf "normal_int_one_side:\r\n" >> $output
-for statistics in 'bytes_ones_int:' 'bytes_ones_new:' 'bytes_flips_int:'  'bytes_flips_new:'
+for statistics in 'bytes_ones_float:' 'bytes_ones_new:' 'bytes_ones_int:' 'bytes_flips_float:'  'bytes_flips_new:' 'bytes_ones_int:'
 do
 grep "$statistics[ ]*[-eE\+0-9\.]*" normal_int_one_side_result$number.txt | xargs printf "%s " >> $output
 printf "\r\n" >> $output
 done #for statistics in
 
 printf "uniform_int_two_sides:\r\n" >> $output
-for statistics in 'bytes_ones_int:' 'bytes_ones_new:' 'bytes_flips_int:'  'bytes_flips_new:'
+for statistics in 'bytes_ones_float:' 'bytes_ones_new:' 'bytes_ones_int:' 'bytes_flips_float:'  'bytes_flips_new:' 'bytes_ones_int:'
 do
 grep "$statistics[ ]*[-eE\+0-9\.]*" uniform_int_two_sides_result$number.txt | xargs printf "%s " >> $output
 printf "\r\n" >> $output
 done #for statistics in
 
 printf "normal_int_two_sides:\r\n" >> $output
-for statistics in 'bytes_ones_int:' 'bytes_ones_new:' 'bytes_flips_int:'  'bytes_flips_new:'
+for statistics in 'bytes_ones_float:' 'bytes_ones_new:' 'bytes_ones_int:' 'bytes_flips_float:'  'bytes_flips_new:' 'bytes_ones_int:'
 do
 grep "$statistics[ ]*[-eE\+0-9\.]*" normal_int_two_sides_result$number.txt | xargs printf "%s " >> $output
 printf "\r\n" >> $output

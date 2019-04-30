f3035467b97be882f3336eb10a08a12a02c2f67e
woodun <hwang07@email.wm.edu>
2019-04-30 00:59:15 -0400

    debug

---
+++ hima_scripts/bfloat_exp/collect_all_exp1.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+#####################################################
+exp1_collect_int_data_visual.sh
+exp1_collect_float_data_visual.sh
+exp1_collect_int_data_visual_dbi.sh
+exp1_collect_float_data_visual_dbi.sh

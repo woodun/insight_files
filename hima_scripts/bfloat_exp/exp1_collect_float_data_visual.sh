cd30c64b8bc55287091aff3c9c5caf72facd5235
woodun <hwang07@email.wm.edu>
2019-04-30 00:55:48 -0400

    debug

--- hima_scripts/bfloat_exp/exp1_collect_float_data_visual.sh
+++ hima_scripts/bfloat_exp/exp1_collect_float_data_visual.sh
@@ -4,6 +4,7 @@
 output=/sciclone/pscr/hwang07/bfloat_analysis/collection/all_results_float.txt
 mother_dir=/sciclone/pscr/hwang07/bfloat_analysis/results_float
 
+rm $output
 cd $mother_dir
 
 
@@ -11,33 +12,37 @@ cd $mother_dir
 for number in -126 -119 -112 -105 -98 -91 -84 -77 -70 -63 -56 -49 -42 -35 -28 -21 -14 -7 0 7 14 21 28 35 42 49 56 63 70 77 84 91 98 105 112 119 126
 do
 
-printf "range%s:\r\n" $number >> $output
+printf "#############################################range%s:\r\n" $number >> $output
 
 printf "uniform_float_one_side:\r\n" >> $output
-for statistics in 'bytes_ones_float:' 'bytes_ones_new:' 'bytes_flips_float:'  'bytes_flips_new:'
+for statistics in 'bytes_ones_float:' 'bytes_ones_new:' 'bytes_flips_float:'  'bytes_flips_new:' 'distribution4_ones_float:' 'distribution4_ones_new:' 'distribution4_flips_float:' 'distribution4_flips_new:'
 do
 grep "$statistics[ ]*[-eE\+0-9\.]*" uniform_float_one_side_result$number.txt | xargs printf "%s " >> $output
+printf "\r\n" >> $output
 printf "\r\n" >> $output
 done #for statistics in
 
 printf "normal_float_one_side:\r\n" >> $output
-for statistics in 'bytes_ones_float:' 'bytes_ones_new:' 'bytes_flips_float:'  'bytes_flips_new:'
+for statistics in 'bytes_ones_float:' 'bytes_ones_new:' 'bytes_flips_float:'  'bytes_flips_new:' 'distribution4_ones_float:' 'distribution4_ones_new:' 'distribution4_flips_float:' 'distribution4_flips_new:'
 do
 grep "$statistics[ ]*[-eE\+0-9\.]*" normal_float_one_side_result$number.txt | xargs printf "%s " >> $output
+printf "\r\n" >> $output
 printf "\r\n" >> $output
 done #for statistics in
 
 printf "uniform_float_two_sides:\r\n" >> $output
-for statistics in 'bytes_ones_float:' 'bytes_ones_new:' 'bytes_flips_float:'  'bytes_flips_new:'
+for statistics in 'bytes_ones_float:' 'bytes_ones_new:' 'bytes_flips_float:'  'bytes_flips_new:' 'distribution4_ones_float:' 'distribution4_ones_new:' 'distribution4_flips_float:' 'distribution4_flips_new:'
 do
 grep "$statistics[ ]*[-eE\+0-9\.]*" uniform_float_two_sides_result$number.txt | xargs printf "%s " >> $output
+printf "\r\n" >> $output
 printf "\r\n" >> $output
 done #for statistics in
 
 printf "normal_float_two_sides:\r\n" >> $output
-for statistics in 'bytes_ones_float:' 'bytes_ones_new:' 'bytes_flips_float:'  'bytes_flips_new:'
+for statistics in 'bytes_ones_float:' 'bytes_ones_new:' 'bytes_flips_float:'  'bytes_flips_new:' 'distribution4_ones_float:' 'distribution4_ones_new:' 'distribution4_flips_float:' 'distribution4_flips_new:'
 do
 grep "$statistics[ ]*[-eE\+0-9\.]*" normal_float_two_sides_result$number.txt | xargs printf "%s " >> $output
+printf "\r\n" >> $output
 printf "\r\n" >> $output
 done #for statistics in
 
--- hima_scripts/bfloat_exp/exp1_collect_int_data_visual_dbi.sh
+++ hima_scripts/bfloat_exp/exp1_collect_int_data_visual_dbi.sh
@@ -4,6 +4,7 @@
 output=/sciclone/pscr/hwang07/bfloat_analysis/collection/all_results_int_dbi.txt
 mother_dir=/sciclone/pscr/hwang07/bfloat_analysis/results_int_dbi
 
+rm $output
 cd $mother_dir
 
 
@@ -11,33 +12,37 @@ cd $mother_dir
 for number in 1 4 7 10 13 16 19 22 25 28 31
 do
 
-printf "range%s:\r\n" $number >> $output
+printf "#############################################range%s:\r\n" $number >> $output
 
-printf "uniform_int_one_side:\r\n" >> $output
-for statistics in 'bytes_ones_float:' 'bytes_ones_new:' 'bytes_ones_int:' 'bytes_flips_float:'  'bytes_flips_new:' 'bytes_flips_int:'
+printf "uniform_float_one_side:\r\n" >> $output
+for statistics in 'bytes_ones_int:' 'bytes_ones_float:' 'bytes_ones_new:' 'bytes_flips_int:' 'bytes_flips_float:'  'bytes_flips_new:' 'distribution4_ones_float:' 'distribution4_ones_int:' 'distribution4_ones_new:' 'distribution4_flips_int:' 'distribution4_flips_float:' 'distribution4_flips_new:'
 do
-grep "$statistics[ ]*[-eE\+0-9\.]*" uniform_int_one_side_result$number.txt | xargs printf "%s " >> $output
+grep "$statistics[ ]*[-eE\+0-9\.]*" uniform_float_one_side_result$number.txt | xargs printf "%s " >> $output
+printf "\r\n" >> $output
 printf "\r\n" >> $output
 done #for statistics in
 
-printf "normal_int_one_side:\r\n" >> $output
-for statistics in 'bytes_ones_float:' 'bytes_ones_new:' 'bytes_ones_int:' 'bytes_flips_float:'  'bytes_flips_new:' 'bytes_flips_int:'
+printf "normal_float_one_side:\r\n" >> $output
+for statistics in 'bytes_ones_int:' 'bytes_ones_float:' 'bytes_ones_new:' 'bytes_flips_int:' 'bytes_flips_float:'  'bytes_flips_new:' 'distribution4_ones_float:' 'distribution4_ones_int:' 'distribution4_ones_new:' 'distribution4_flips_int:' 'distribution4_flips_float:' 'distribution4_flips_new:'
 do
-grep "$statistics[ ]*[-eE\+0-9\.]*" normal_int_one_side_result$number.txt | xargs printf "%s " >> $output
+grep "$statistics[ ]*[-eE\+0-9\.]*" normal_float_one_side_result$number.txt | xargs printf "%s " >> $output
+printf "\r\n" >> $output
 printf "\r\n" >> $output
 done #for statistics in
 
-printf "uniform_int_two_sides:\r\n" >> $output
-for statistics in 'bytes_ones_float:' 'bytes_ones_new:' 'bytes_ones_int:' 'bytes_flips_float:'  'bytes_flips_new:' 'bytes_flips_int:'
+printf "uniform_float_two_sides:\r\n" >> $output
+for statistics in 'bytes_ones_int:' 'bytes_ones_float:' 'bytes_ones_new:' 'bytes_flips_int:' 'bytes_flips_float:'  'bytes_flips_new:' 'distribution4_ones_float:' 'distribution4_ones_int:' 'distribution4_ones_new:' 'distribution4_flips_int:' 'distribution4_flips_float:' 'distribution4_flips_new:'
 do
-grep "$statistics[ ]*[-eE\+0-9\.]*" uniform_int_two_sides_result$number.txt | xargs printf "%s " >> $output
+grep "$statistics[ ]*[-eE\+0-9\.]*" uniform_float_two_sides_result$number.txt | xargs printf "%s " >> $output
+printf "\r\n" >> $output
 printf "\r\n" >> $output
 done #for statistics in
 
-printf "normal_int_two_sides:\r\n" >> $output
-for statistics in 'bytes_ones_float:' 'bytes_ones_new:' 'bytes_ones_int:' 'bytes_flips_float:'  'bytes_flips_new:' 'bytes_flips_int:'
+printf "normal_float_two_sides:\r\n" >> $output
+for statistics in 'bytes_ones_int:' 'bytes_ones_float:' 'bytes_ones_new:' 'bytes_flips_int:' 'bytes_flips_float:'  'bytes_flips_new:' 'distribution4_ones_float:' 'distribution4_ones_int:' 'distribution4_ones_new:' 'distribution4_flips_int:' 'distribution4_flips_float:' 'distribution4_flips_new:'
 do
-grep "$statistics[ ]*[-eE\+0-9\.]*" normal_int_two_sides_result$number.txt | xargs printf "%s " >> $output
+grep "$statistics[ ]*[-eE\+0-9\.]*" normal_float_two_sides_result$number.txt | xargs printf "%s " >> $output
+printf "\r\n" >> $output
 printf "\r\n" >> $output
 done #for statistics in
 

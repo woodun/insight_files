342a225772e65c61ff9ea0dc1e77bdfe5131a79c
woodun <hwang07@email.wm.edu>
2019-06-16 19:47:34 -0400

    debug

---
+++ hima_scripts/bfloat_exp/full_range/clear_all_scripts_exp1.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+#####################################################
+cd /sciclone/pscr/hwang07/bfloat_analysis/scripts_int
+rm *
+
+cd /sciclone/pscr/hwang07/bfloat_analysis/scripts_float
+rm *
+
+cd /sciclone/pscr/hwang07/bfloat_analysis/scripts_int_dbi
+rm *
+
+cd /sciclone/pscr/hwang07/bfloat_analysis/scripts_float_dbi
+rm *

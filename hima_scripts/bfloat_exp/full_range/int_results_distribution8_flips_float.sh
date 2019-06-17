292289b5525be00bb4eb0aa29051e1418b7d7d47
woodun <hwang07@email.wm.edu>
2019-06-17 04:26:10 -0400

    debug

--- hima_scripts/bfloat_exp/full_range/int_results_distribution8_flips_float.sh
+++ hima_scripts/bfloat_exp/full_range/int_results_distribution8_flips_float.sh
@@ -11,8 +11,6 @@ cd $mother_dir
 for number in 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31
 do
 
-printf "#############################################range%s:\r\n" $number >> $output
-
 for statistics in 'distribution8_flips_float:'
 do
 

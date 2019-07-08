949f0b36839927404686a642354ddb0a59537100
woodun <hwang07@email.wm.edu>
2019-07-08 11:49:08 -0400

    debug

--- hima_scripts/bfloat_exp/full_range/collect_all_exp1_matlab.sh
+++ hima_scripts/bfloat_exp/full_range/collect_all_exp1_matlab.sh
@@ -5,12 +5,10 @@ sh float_results_bytes_flips_float.sh
 sh float_results_bytes_flips_new.sh
 sh float_results_bytes_ones_float.sh
 sh float_results_bytes_ones_new.sh
-sh float_results_distribution8_flips_float.sh
-sh float_results_distribution8_flips_new.sh
-sh float_results_distribution8_ones_float.sh
-sh float_results_distribution8_ones_new.sh
-
-
+#sh float_results_distribution8_flips_float.sh
+#sh float_results_distribution8_flips_new.sh
+#sh float_results_distribution8_ones_float.sh
+#sh float_results_distribution8_ones_new.sh
 
 
 sh int_results_bytes_ones_int.sh
@@ -19,12 +17,64 @@ sh int_results_bytes_ones_new.sh
 sh int_results_bytes_flips_int.sh
 sh int_results_bytes_flips_float.sh
 sh int_results_bytes_flips_new.sh
-sh int_results_distribution8_ones_float.sh
-sh int_results_distribution8_ones_int.sh
-sh int_results_distribution8_ones_new.sh
-sh int_results_distribution8_flips_int.sh
-sh int_results_distribution8_flips_float.sh
-sh int_results_distribution8_flips_new.sh
+#sh int_results_distribution8_ones_float.sh
+#sh int_results_distribution8_ones_int.sh
+#sh int_results_distribution8_ones_new.sh
+#sh int_results_distribution8_flips_int.sh
+#sh int_results_distribution8_flips_float.sh
+#sh int_results_distribution8_flips_new.sh
+
+sh float_results_RE_float_F16.sh
+sh float_results_RE_float_F8.sh
+sh float_results_RE_float_B16.sh
+sh float_results_RE_float_B8.sh
+
+sh float_results_RE_float_NF16.sh
+sh float_results_RE_float_NF8.sh
+sh float_results_RE_float_NB16.sh
+sh float_results_RE_float_NB8.sh
+
+sh int_results_RE_float_F16.sh
+sh int_results_RE_float_F8.sh
+sh int_results_RE_float_B16.sh
+sh int_results_RE_float_B8.sh
+
+sh int_results_RE_float_NF16.sh
+sh int_results_RE_float_NF8.sh
+sh int_results_RE_float_NB16.sh
+sh int_results_RE_float_NB8.sh
+
+sh int_results_RE_float_IL16.sh
+sh int_results_RE_float_IL8.sh
+sh int_results_RE_float_IR16.sh
+sh int_results_RE_float_IR8.sh
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ hima_scripts/bfloat_exp/full_range/int_results_RE_float_B16.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+#####################################################better for visual comparison
+output=/sciclone/pscr/hwang07/bfloat_analysis/collection/int_results_RE_float_B16.txt
+mother_dir=/sciclone/pscr/hwang07/bfloat_analysis/results_int_dbi
+
+rm $output
+cd $mother_dir
+
+
+for number in 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31
+do
+
+for statistics in 'sum_per_line_RE_float_B16:'
+do
+
+grep "$statistics[ ]*[-eE\+0-9\.]*" uniform_float_two_sides_result$number.txt | sed -e "s/$statistics[ ]*\(-$\)*//g" | xargs printf "%s " >> $output
+printf "\r\n" >> $output
+
+done #for statistics in
+
+done #for number in
+
+
+
+
